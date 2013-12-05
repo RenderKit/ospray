0a171df3b2f691948ba22b36f5290412b2798b5d
Ingo Wald <ingowald@gmail.com>
2013-12-04 20:03:37 -0600

    first dummy version compiles with (still dummy) objview app.

---
+++ apps/objView/objView.cpp
@@ -0,0 +1,9 @@
+/*! \file objView.cpp A GLUT-based viewer for Wavefront OBJ files */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, const char **av)
+{
+  printf("objView - Hello World\n");
+}
