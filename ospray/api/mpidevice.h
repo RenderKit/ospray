17ab5217cc3c5d605918621d6a4455bb3961a76f
Ingo Wald <ingowald@gmail.com>
2014-01-03 21:18:13 -0600

    cmake infrastructure for MPI compilation working, MPI device not yet doing anything useful

---
+++ ospray/api/mpidevice.h
@@ -0,0 +1,62 @@
+#include "device.h"
+
+/*! \file mpidevice.h Implements the "mpi" device for mpi rendering */
+
+namespace ospray {
+  namespace api {
+    struct MPIDevice : public Device {
+
+      /*! constructor */
+      MPIDevice(int *_ac=NULL, const char **_av=NULL);
+
+      /*! create a new frame buffer */
+      virtual OSPFrameBuffer frameBufferCreate(const vec2i &size, 
+                                               const OSPFrameBufferMode mode,
+                                               const size_t swapChainDepth);
+
+      /*! map frame buffer */
+      virtual const void *frameBufferMap(OSPFrameBuffer fb);
+
+      /*! unmap previously mapped frame buffer */
+      virtual void frameBufferUnmap(const void *mapped,
+                                    OSPFrameBuffer fb);
+
+      /*! create a new model */
+      virtual OSPModel newModel();
+
+      // /*! finalize a newly specified model */
+      // virtual void finalizeModel(OSPModel _model);
+
+      /*! commit the given object's outstanding changes */
+      virtual void commit(OSPObject object);
+
+      /*! add a new geometry to a model */
+      virtual void addGeometry(OSPModel _model, OSPGeometry _geometry);
+
+      /*! create a new data buffer */
+      virtual OSPData newData(size_t nitems, OSPDataType format, void *init, int flags);
+
+      /*! assign (named) data item as a parameter to an object */
+      virtual void setObject(OSPObject target, const char *bufName, OSPObject value);
+      /*! assign (named) float parameter to an object */
+      virtual void setFloat(OSPObject object, const char *bufName, const float f);
+      /*! assign (named) vec3f parameter to an object */
+      virtual void setVec3f(OSPObject object, const char *bufName, const vec3f &v);
+
+      /*! create a new triangle mesh geometry */
+      virtual OSPTriangleMesh newTriangleMesh();
+
+      /*! create a new renderer object (out of list of registered renderers) */
+      virtual OSPRenderer newRenderer(const char *type);
+
+      /*! create a new camera object (out of list of registered cameras) */
+      virtual OSPCamera newCamera(const char *type);
+
+      /*! call a renderer to render a frame buffer */
+      virtual void renderFrame(OSPFrameBuffer _sc, 
+                               OSPRenderer _renderer);
+    };
+  }
+}
+
+
