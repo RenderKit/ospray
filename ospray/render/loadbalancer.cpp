22886e934848db25aad44def9c086ea4f6a32ecc
Ingo Wald <ingowald@gmail.com>
2014-01-28 21:53:46 -0600

    raycaster now working with (local) tiled load balancer

--- ospray/render/loadbalancer.cpp
+++ ospray/render/loadbalancer.cpp
@@ -4,11 +4,48 @@
 namespace ospray {
   TiledLoadBalancer *TiledLoadBalancer::instance = NULL;
 
+#define divRoundUp(X,Y) ((X)+(Y)-1)/(Y)
+  
+  LocalTiledLoadBalancer::RenderTileTask::RenderTileTask(size_t numTiles, 
+                                                         LocalTiledLoadBalancer *loadBalancer,
+                                                         TileRenderer *tiledRenderer,
+                                                         FrameBuffer *fb)
+    : Task(&eventSync,_run,this,numTiles,NULL,NULL,
+           "LocalTiledLoadBalancer::RenderTileTask"),
+      loadBalancer(loadBalancer), fb(fb), tiledRenderer(tiledRenderer)
+  {
+  }
+
+  void LocalTiledLoadBalancer::RenderTileTask::run(size_t threadIndex, 
+                                                   size_t threadCount, 
+                                                   size_t taskIndex, 
+                                                   size_t taskCount, 
+                                                   TaskScheduler::Event* event) 
+  {
+    Tile tile;
+    tile.fbSize = fb->size;
+    const size_t numTiles_x = divRoundUp(fb->size.x,TILE_SIZE);
+    const size_t tile_y = taskIndex / numTiles_x;
+    const size_t tile_x = taskIndex % numTiles_x;
+    tile.region.lower.x = tile_x * TILE_SIZE; //x0;
+    tile.region.lower.y = tile_y * TILE_SIZE; //y0;
+    tile.region.upper.x = std::min(tile.region.lower.x+TILE_SIZE,fb->size.x);
+    tile.region.upper.y = std::min(tile.region.lower.y+TILE_SIZE,fb->size.y);
+    tiledRenderer->renderTile(tile);
+    fb->setTile(tile);
+  }
+
+  /*! render a frame via the tiled load balancer */
   void LocalTiledLoadBalancer::renderFrame(TileRenderer *tiledRenderer,
                                            FrameBuffer *fb)
   {
-    // PING;
     Assert(tiledRenderer);
+#if 1
+    size_t numTiles = divRoundUp(fb->size.x,TILE_SIZE)*divRoundUp(fb->size.y,TILE_SIZE);
+    RenderTileTask *renderTask = new RenderTileTask(numTiles,this,tiledRenderer,fb);
+    TaskScheduler::addTask(-1, TaskScheduler::GLOBAL_BACK, renderTask); //&task_renderTiles)
+    renderTask->eventSync.sync();
+#else
     Tile tile;
     tile.fbSize = fb->size;
     for (int y0=0;y0<fb->size.y;y0+=TILE_SIZE) {
@@ -20,8 +57,7 @@ namespace ospray {
         tiledRenderer->renderTile(tile);
         fb->setTile(tile);
       }
-      // PING;
     }
-    // PING;
+#endif
   }
 }
--- ospray/render/loadbalancer.h
+++ ospray/render/loadbalancer.h
@@ -1,9 +1,13 @@
 #pragma once
 
+// ospray
 #include "../common/ospcommon.h"
 #include "../fb/framebuffer.h"
+// embree
+#include "common/sys/taskscheduler.h"
 
 namespace ospray {
+  using embree::TaskScheduler;
 
   struct TileRenderer;
 
@@ -16,6 +20,24 @@ namespace ospray {
 
   struct LocalTiledLoadBalancer 
   {
+    struct RenderTileTask : public embree::TaskScheduler::Task {
+      RenderTileTask(size_t numTiles, 
+                     LocalTiledLoadBalancer *loadBalancer,
+                     TileRenderer *tiledRenderer,
+                     FrameBuffer *fb);
+
+      LocalTiledLoadBalancer  *loadBalancer;
+      TaskScheduler::EventSync eventSync;
+      TileRenderer            *tiledRenderer;
+      FrameBuffer             *fb;
+      // void run(size_t threadIndex, size_t threadCount, 
+      //          size_t taskIndex, size_t taskCount, 
+      //          embree::TaskScheduler::Event* event);
+      TASK_RUN_FUNCTION(RenderTileTask,run);
+      // TASK_COMPLETE_FUNCTION(ISPCTask,finish);
+    };
+  
+
     virtual void renderFrame(TileRenderer *tiledRenderer,
                              FrameBuffer *fb);
   };
