869050369197a250442d3dee6180a5398ecac2cf
Brad Rathke <brad.w.rathke@intel.com>
2014-06-13 11:05:21 -0500

    Texture2D::createTexture now creates the ispc side of the texture as well

--- ospray/texture/texture2d.cpp
+++ ospray/texture/texture2d.cpp
@@ -1,4 +1,8 @@
 #include "texture2d.h"
+#include "image3c_ispc.h"
+#include "image3ca_ispc.h"
+#include "image3f_ispc.h"
+#include "image3fa_ispc.h"
 
 namespace ospray {
 
@@ -8,10 +12,22 @@ namespace ospray {
       //NOTE: For now, we just copy the data and rely on the viewer to have logic for buffer sharing.
       int bpp = 0;
       switch (type) {
-        case OSP_vec4uc: bpp = 4; break;
-        case OSP_vec3uc: bpp = 3; break;
-        case OSP_vec3f: bpp = 3 * sizeof(float); break;
-        case OSP_vec3fa: bpp = 4 * sizeof(float); break;
+        case OSP_vec4uc:
+          bpp = 4;
+          tx->ispcEquivalent = ispc::Image3ca__new(width, height, (uint32_t*)data, true);
+          break;
+        case OSP_vec3uc:
+          bpp = 3;
+          tx->ispcEquivalent = ispc::Image3c__new(width, height, (ispc::vec3uc*)data, true);
+          break;
+        case OSP_vec3f:
+          bpp = 3 * sizeof(float);
+          tx->ispcEquivalent = ispc::Image3f__new(width, height, (ispc::vec3f*)data, true);
+          break;
+        case OSP_vec3fa:
+          bpp = 4 * sizeof(float);
+          tx->ispcEquivalent = ispc::Image3fa__new(width, height, (ispc::vec3fa*)data, true);
+          break;
         default: throw std::runtime_error("Could not determine bytes per pixel when creating a Texture2D");
       };
 
@@ -21,6 +37,7 @@ namespace ospray {
       tx->type = type;
       tx->data = new unsigned char[bytes];
 
+      //Do we really need to make this copy?
       if (data) memcpy(tx->data, data, bytes);
     }
 
