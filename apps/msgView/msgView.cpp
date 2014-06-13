ab10e214fd8641a0af63509711155a0490b7baf6
Brad Rathke <brad.w.rathke@intel.com>
2014-06-13 13:39:54 -0500

    Added if check to ensure we don't try to create a data array of size 0

--- apps/msgView/msgView.cpp
+++ apps/msgView/msgView.cpp
@@ -248,8 +248,11 @@ namespace ospray {
     for (size_t i = 0; i < mat->textures.size(); i++) {
       textures.push_back(createTexture2D(mat->textures[i].ptr));
     }
-    OSPData textureArray = ospNewData(textures.size(), OSP_OBJECT, &textures[0], OSP_DATA_SHARED_BUFFER);
-    ospSetData(ospMat, "textures", textureArray);
+
+    if (!textures.empty()) {
+      OSPData textureArray = ospNewData(textures.size(), OSP_OBJECT, &textures[0], 0);
+      ospSetData(ospMat, "textures", textureArray);
+    }
     
     ospCommit(ospMat);
     return ospMat;
