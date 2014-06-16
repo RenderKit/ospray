4915c464717b21d93354310d78500e9ac116a6a9
Brad Rathke <brad.w.rathke@intel.com>
2014-06-16 15:45:42 -0500

    Began using 'nearestneighbor for textures. note that texture2d.cpp will cause leaks through the img pointer.

--- ospray/texture/texture2d.cpp
+++ ospray/texture/texture2d.cpp
@@ -3,6 +3,7 @@
 #include "image3ca_ispc.h"
 #include "image3f_ispc.h"
 #include "image3fa_ispc.h"
+#include "nearestneighbor_ispc.h"
 
 namespace ospray {
 
@@ -11,25 +12,30 @@ namespace ospray {
     if (tx) {
       //NOTE: For now, we just copy the data and rely on the viewer to have logic for buffer sharing.
       int bpp = 0;
+      //TODO:ISPC side memory for images isn't currently managed, this will leak!
+      void *img = NULL;
       switch (type) {
         case OSP_vec4uc:
           bpp = 4;
-          tx->ispcEquivalent = ispc::Image3ca__new(width, height, (uint32_t*)data, true);
+          img = ispc::Image3ca__new(width, height, (uint32_t*)data, true);
           break;
         case OSP_vec3uc:
           bpp = 3;
-          tx->ispcEquivalent = ispc::Image3c__new(width, height, (ispc::vec3uc*)data, true);
+          img = ispc::Image3c__new(width, height, (ispc::vec3uc*)data, true);
           break;
         case OSP_vec3f:
           bpp = 3 * sizeof(float);
-          tx->ispcEquivalent = ispc::Image3f__new(width, height, (ispc::vec3f*)data, true);
+          img = ispc::Image3f__new(width, height, (ispc::vec3f*)data, true);
           break;
         case OSP_vec3fa:
           bpp = 4 * sizeof(float);
-          tx->ispcEquivalent = ispc::Image3fa__new(width, height, (ispc::vec3fa*)data, true);
+          img = ispc::Image3fa__new(width, height, (ispc::vec3fa*)data, true);
           break;
-        default: throw std::runtime_error("Could not determine bytes per pixel when creating a Texture2D");
+        default: throw std::runtime_error("Could not determine bytes per pixel in " __FILE__);
       };
+
+      assert(img && "Image pointer must not be null in " __FILE__);
+      tx->ispcEquivalent = ispc::NearestNeighbor__new(img);
 
       const int bytes = width * height * bpp;
       tx->width = width;
