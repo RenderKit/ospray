75f746d6f667b47013a76034f1413744eb7a174a
Brad Rathke <brad.w.rathke@intel.com>
2014-06-20 15:29:32 -0500

    Added stubs for coidevice

--- ospray/api/coidevice_host.cpp
+++ ospray/api/coidevice_host.cpp
@@ -49,7 +49,9 @@ namespace ospray {
       OSPCOI_NEW_FRAMEBUFFER,
       OSPCOI_RENDER_FRAME,
       OSPCOI_RENDER_FRAME_SYNC,
-      OSPCOI_NUM_FUNCTIONS
+      OSPCOI_NEW_TEXTURE2D,
+      OSPCOI_NEW_LIGHT,
+      OSPCOI_NUM_FUNCTIONS,
     } RemoteFctID;
 
     const char *coiFctName[] = {
@@ -69,6 +71,8 @@ namespace ospray {
       "ospray_coi_new_framebuffer",
       "ospray_coi_render_frame",
       "ospray_coi_render_frame_sync",
+      "ospray_coi_new_texture2d",
+      "ospray_coi_new_light",
       NULL
     };
     
@@ -164,6 +168,12 @@ namespace ospray {
       /*! create a new volume object (out of list of registered volume types) with data from a file */
       virtual OSPVolume newVolumeFromFile(const char *filename, const char *type);
 
+      /*! have given renderer create a new Light */
+      virtual OSPLight newLight(OSPRenderer _renderer, const char *type);
+
+      /*! create a new Texture2D object */
+      virtual OSPTexture2D newTexture2D(int width, int height, OSPDataType type, void *data, int flags);
+      
       /*! call a renderer to render a frame buffer */
       virtual void renderFrame(OSPFrameBuffer _sc, 
                                OSPRenderer _renderer);
@@ -496,6 +506,19 @@ namespace ospray {
       args.write(type);
       callFunction(OSPCOI_NEW_MATERIAL,args);
       return (OSPMaterial)(int64)handle;
+    }
+
+    /*! create a new texture2D */
+    OSPTexture2D COIDevice::newTexture2D(int width, int height, OSPDataType type, void *data, int flags)
+    {
+      assert(false && __func__ " not yet implemented for COIDevice");
+      return NULL;
+    }
+
+    /*! have given renderer create a new light */
+    OSPLight COIDevice::newLight(OSPRenderer _renderer, const char *type)
+    {
+      assert(false && __func__ " not yet implemented for COIDevice");
     }
 
     /*! create a new geometry object (out of list of registered geometrys) */
