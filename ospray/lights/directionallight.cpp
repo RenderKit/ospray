33d34e8df7e7e53671fb3d4b276166ccb7cab041
Brad Rathke <brad.w.rathke@intel.com>
2014-06-23 11:08:39 -0500

    Added allocation of ispc side of object to directionallight.cpp since for now it will be used directly and not subclassed (by e.g. OBJDirectionalLight)

--- ospray/lights/directionallight.cpp
+++ ospray/lights/directionallight.cpp
@@ -1,15 +1,20 @@
 #include "directionallight.h"
+#include "directionallight_ispc.h"
 
 namespace ospray {
 
   DirectionalLight::DirectionalLight()
     : direction(0.f, -1.f, 0.f)
     , color(1.f, 1.f, 1.f)
-  {}
+  {
+    ispcEquivalent = ispc::DirectionalLight_create(this);
+  }
 
   void DirectionalLight::commit() {
     direction = getParam3f("direction", vec3f(0.f, -1.f, 0.f));
     color     = getParam3f("color", vec3f(1.f, 1.f, 1.f));
+
+    ispc::DirectionalLight_set(getIE(), (ispc::vec3f&)color, (ispc::vec3f&)direction);
   }
 
   OSP_REGISTER_LIGHT(DirectionalLight, DirectionalLight);
