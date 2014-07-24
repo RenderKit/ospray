263f0a5c7ae4c47d6f5a5bb036c67d87ac6937a2
Ingo Wald <ingowald@gmail.com>
2014-07-24 12:11:27 -0500

    added subsampled variants of the ao16 renderer - there's now also ao1, ao2, ao4, and ao8

--- ospray/render/ao16/ao16.h
+++ ospray/render/ao16/ao16.h
@@ -41,7 +41,24 @@ namespace ospray {
     Ref<Texture> map_Kd;
   };
   
-  /*! \brief Simple 16-sample Ambient Occlusion Renderer */
+  /*! \brief Simple 16-sample Ambient Occlusion Renderer
+    
+    This renderer uses a set of 16 precomputed AO directions to shoot
+    shadow rays; for accumulation these 16 directions are
+    (semi-)randomly rotated to give different directions every frame
+    (this is far from optimal regarding how well the samples are
+    distributed, but seems OK for accumulation).
+
+    To further improve frame rate there's a template parameter
+    NUM_SAMPLES_PER_FRAME that allows for selecting subsets of these
+    samples (only 1,2,4,8, and 16 area allowed). I.e., whereas AO16
+    would usually trace the orginal 16 samples in the first frame, and
+    a rotated version of that in the second, with
+    NUM_SAMPLES_PER_FRAME set to 8 it would trace the original 8
+    samples in the first frame, the next 8 in the second, then
+    rotation of the first 8 in the third, etc.
+   */
+  template<int NUM_SAMPLES_PER_FRAME>
   struct AO16Renderer : public Renderer {
     AO16Renderer();
 
