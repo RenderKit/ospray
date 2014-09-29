ac59070332c5aec990ec85af416729efced2368e
Ingo Wald <ingowald@gmail.com>
2014-09-29 10:54:42 -0500

    - lots of doxygen fixes - removed deprecated texture.cpp (apparently all working code is in texture2d.cpp

--- ospray/common/managed.cpp
+++ ospray/common/managed.cpp
@@ -71,15 +71,6 @@ namespace ospray {
     return paramList[paramList.size()-1];
   }
 
-  /*! @{ */
-  /*! \brief find the named parameter, and return its object value if
-    available; else return 'default' value 
-    
-    \detailed The returned managed object will *not* automatically
-    have its refcount increased; it is up to the callee to
-    properly do that (typically by assigning to a proper 'ref'
-    instance */
-  
 #define define_getparam(T,ABB,TARGETTYPE,FIELD)                       \
   T ManagedObject::getParam##ABB(const char *name, T valIfNotFound) { \
   Param *param = findParam(name);                                     \
@@ -98,9 +89,6 @@ namespace ospray {
   define_getparam(const char *, String, OSP_STRING, ptr);
 
 #undef define_getparam
-
-  /*! @} */
-
   
   void   ManagedObject::setParam(const char *name, ManagedObject *data)
   {
