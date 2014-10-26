01c254606c1830347d485ef3be12253b2da8ee10
Ingo Wald <ingowald@gmail.com>
2014-10-26 16:59:13 -0500

    missing files

--- apps/qtModelViewer/OSPRayRenderer.h
+++ apps/qtModelViewer/OSPRayRenderer.h
@@ -1,22 +1,22 @@
-/********************************************************************* *\
- * INTEL CORPORATION PROPRIETARY INFORMATION                            
- * This software is supplied under the terms of a license agreement or  
- * nondisclosure agreement with Intel Corporation and may not be copied 
- * or disclosed except in accordance with the terms of that agreement.  
- * Copyright (C) 2014 Intel Corporation. All Rights Reserved.           
- ********************************************************************* */
+// /********************************************************************* *\
+//  * INTEL CORPORATION PROPRIETARY INFORMATION                            
+//  * This software is supplied under the terms of a license agreement or  
+//  * nondisclosure agreement with Intel Corporation and may not be copied 
+//  * or disclosed except in accordance with the terms of that agreement.  
+//  * Copyright (C) 2014 Intel Corporation. All Rights Reserved.           
+//  ********************************************************************* */
 
-#pragma once
+// #pragma once
 
-namespace ospray {
-  namespace viewer {
-    //! \brief abstracts all rendering activities
-    struct OSPRayRenderer : public Renderer {
-      virtual void setResolution(const vec2i &imageSize);
-      virtual void setCamera(
-      virtual void render();
-    };
+// namespace ospray {
+//   namespace viewer {
+//     //! \brief abstracts all rendering activities
+//     struct OSPRayRenderer : public Renderer {
+//       virtual void setResolution(const vec2i &imageSize);
+//       virtual void setCamera(
+//       virtual void render();
+//     };
 
-  }
+//   }
 
-}
+// }
