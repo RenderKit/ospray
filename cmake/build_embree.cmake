e5322fe34434e57936699a675710025e8152f8a4
Ingo Wald <Ingo.Wald@intel.com>
2014-12-05 08:05:52 -0600

    embree makefile fix for avx2

--- cmake/build_embree.cmake
+++ cmake/build_embree.cmake
@@ -14,38 +14,38 @@ CONFIGURE_OSPRAY()
 # source for everything embree keeps in its 'sys' library
 # =======================================================
 IF (THIS_IS_MIC)
-	SET(EMBREE_COMMON_SYS_SOURCES
-		platform.cpp
-		sysinfo.cpp
-		filename.cpp
-		library.cpp
-		thread.cpp
-		network.cpp
-		tasklogger.cpp
-		taskscheduler.cpp
-		taskscheduler_sys.cpp
-		taskscheduler_mic.cpp
-		sync/mutex.cpp
-		sync/condition.cpp
-		sync/barrier.cpp
-		stl/string.cpp
-		)
+  SET(EMBREE_COMMON_SYS_SOURCES
+    platform.cpp
+    sysinfo.cpp
+    filename.cpp
+    library.cpp
+    thread.cpp
+    network.cpp
+    tasklogger.cpp
+    taskscheduler.cpp
+    taskscheduler_sys.cpp
+    taskscheduler_mic.cpp
+    sync/mutex.cpp
+    sync/condition.cpp
+    sync/barrier.cpp
+    stl/string.cpp
+    )
 ELSE()
-	SET(EMBREE_COMMON_SYS_SOURCES
-		platform.cpp
-		sysinfo.cpp
-		filename.cpp
-		library.cpp
-		thread.cpp
-		network.cpp
-		tasklogger.cpp
-		taskscheduler.cpp
-		taskscheduler_sys.cpp
-		sync/mutex.cpp
-		sync/condition.cpp
-		sync/barrier.cpp
-		stl/string.cpp
-		)
+  SET(EMBREE_COMMON_SYS_SOURCES
+    platform.cpp
+    sysinfo.cpp
+    filename.cpp
+    library.cpp
+    thread.cpp
+    network.cpp
+    tasklogger.cpp
+    taskscheduler.cpp
+    taskscheduler_sys.cpp
+    sync/mutex.cpp
+    sync/condition.cpp
+    sync/barrier.cpp
+    stl/string.cpp
+    )
 ENDIF()
 
 # =======================================================
@@ -53,11 +53,11 @@ ENDIF()
 # =======================================================
 IF(THIS_IS_MIC)
   SET(EMBREE_COMMON_SIMD_SOURCES 
-		mic_i.cpp mic_f.cpp mic_m.cpp
+    mic_i.cpp mic_f.cpp mic_m.cpp
     )
 ELSE()
   SET(EMBREE_COMMON_SIMD_SOURCES 
-		sse.cpp
+    sse.cpp
     )
 ENDIF()
 
@@ -72,69 +72,69 @@ IF (THIS_IS_MIC)
   # embree kernel components ONLY for xeon phi
   # =======================================================
   SET(EMBREE_KERNELS_XEONPHI_SOURCES
-		../common/stat.cpp 
-		../common/globals.cpp 
-		../common/alloc.cpp 
-		../common/tasksys.cpp 
-		../common/acceln.cpp
-		../common/rtcore.cpp 
-		../common/rtcore_ispc.cpp 
-		../common/rtcore_ispc.ispc 
-		../common/buffer.cpp
-		../common/scene.cpp
-		../common/geometry.cpp
-		../common/scene_user_geometry.cpp
-		../common/scene_triangle_mesh.cpp
-		../common/scene_bezier_curves.cpp
-		../common/scene_subdiv_mesh.cpp
-		../common/raystream_log.cpp
-		
-		geometry/triangle1.cpp
-		geometry/instance_intersector1.cpp
-		geometry/instance_intersector16.cpp
-		geometry/subdiv_intersector16.cpp
+    ../common/stat.cpp 
+    ../common/globals.cpp 
+    ../common/alloc.cpp 
+    ../common/tasksys.cpp 
+    ../common/acceln.cpp
+    ../common/rtcore.cpp 
+    ../common/rtcore_ispc.cpp 
+    ../common/rtcore_ispc.ispc 
+    ../common/buffer.cpp
+    ../common/scene.cpp
+    ../common/geometry.cpp
+    ../common/scene_user_geometry.cpp
+    ../common/scene_triangle_mesh.cpp
+    ../common/scene_bezier_curves.cpp
+    ../common/scene_subdiv_mesh.cpp
+    ../common/raystream_log.cpp
+    
+    geometry/triangle1.cpp
+    geometry/instance_intersector1.cpp
+    geometry/instance_intersector16.cpp
+    geometry/subdiv_intersector16.cpp
 
-		builders/parallel_builder.cpp
+    builders/parallel_builder.cpp
 
-		bvh4i/bvh4i.cpp
-		bvh4i/bvh4i_builder.cpp
-		bvh4i/bvh4i_builder_ext.cpp
-		bvh4i/bvh4i_builder_morton.cpp
-		bvh4i/bvh4i_builder_morton_64bit.cpp
-		bvh4i/bvh4i_intersector16_chunk.cpp
-		bvh4i/bvh4i_intersector16_single.cpp
-		bvh4i/bvh4i_intersector16_hybrid.cpp
-		bvh4i/bvh4i_intersector16_test.cpp
-		bvh4i/bvh4i_intersector1.cpp
-		bvh4i/bvh4i_statistics.cpp
-		bvh4i/bvh4i_rotate.cpp
+    bvh4i/bvh4i.cpp
+    bvh4i/bvh4i_builder.cpp
+    bvh4i/bvh4i_builder_ext.cpp
+    bvh4i/bvh4i_builder_morton.cpp
+    bvh4i/bvh4i_builder_morton_64bit.cpp
+    bvh4i/bvh4i_intersector16_chunk.cpp
+    bvh4i/bvh4i_intersector16_single.cpp
+    bvh4i/bvh4i_intersector16_hybrid.cpp
+    bvh4i/bvh4i_intersector16_test.cpp
+    bvh4i/bvh4i_intersector1.cpp
+    bvh4i/bvh4i_statistics.cpp
+    bvh4i/bvh4i_rotate.cpp
 
 
-		bvh4mb/bvh4mb.cpp
-		bvh4mb/bvh4mb_builder.cpp
-		bvh4mb/bvh4mb_intersector16_chunk.cpp
-		bvh4mb/bvh4mb_intersector16_single.cpp  
-		bvh4mb/bvh4mb_intersector16_hybrid.cpp
-		bvh4mb/bvh4mb_intersector1.cpp
+    bvh4mb/bvh4mb.cpp
+    bvh4mb/bvh4mb_builder.cpp
+    bvh4mb/bvh4mb_intersector16_chunk.cpp
+    bvh4mb/bvh4mb_intersector16_single.cpp  
+    bvh4mb/bvh4mb_intersector16_hybrid.cpp
+    bvh4mb/bvh4mb_intersector1.cpp
 
-		bvh4hair/bvh4hair_builder.cpp
-		bvh4hair/bvh4hair.cpp	
-		bvh4hair/bvh4hair_intersector16_single.cpp
+    bvh4hair/bvh4hair_builder.cpp
+    bvh4hair/bvh4hair.cpp	
+    bvh4hair/bvh4hair_intersector16_single.cpp
     )
 
-	SET (OSPRAY_EMBREE_SOURCES "")
-	FOREACH (src ${EMBREE_COMMON_SYS_SOURCES})
-		SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/sys/${src})
-	ENDFOREACH()
-	
-	FOREACH (src ${EMBREE_COMMON_SIMD_SOURCES})
-		SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/simd/${src})
-	ENDFOREACH()
+  SET (OSPRAY_EMBREE_SOURCES "")
+  FOREACH (src ${EMBREE_COMMON_SYS_SOURCES})
+    SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/sys/${src})
+  ENDFOREACH()
+  
+  FOREACH (src ${EMBREE_COMMON_SIMD_SOURCES})
+    SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/simd/${src})
+  ENDFOREACH()
 
-	FOREACH (src ${EMBREE_KERNELS_XEONPHI_SOURCES})
-		SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeonphi/${src})
-	ENDFOREACH()
-	
+  FOREACH (src ${EMBREE_KERNELS_XEONPHI_SOURCES})
+    SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeonphi/${src})
+  ENDFOREACH()
+  
   OSPRAY_ADD_LIBRARY(ospray_embree${OSPRAY_LIB_SUFFIX} SHARED ${OSPRAY_EMBREE_SOURCES})
   TARGET_LINK_LIBRARIES(ospray_embree${OSPRAY_LIB_SUFFIX} pthread dl)
 ELSE()
@@ -142,81 +142,81 @@ ELSE()
   # embree kernel components ONLY for xeon
   # =======================================================
   SET(EMBREE_KERNELS_XEON_SOURCES
-		../common/stat.cpp 
-		../common/globals.cpp 
-		../common/alloc.cpp 
-		../common/tasksys.cpp 
-		../common/acceln.cpp
-		../common/rtcore.cpp 
-		../common/rtcore_ispc.cpp 
-		../common/rtcore_ispc.ispc 
-		../common/buffer.cpp
-		../common/scene.cpp
-		../common/geometry.cpp
-		../common/scene_user_geometry.cpp
-		../common/scene_triangle_mesh.cpp
-		../common/scene_bezier_curves.cpp
-		../common/scene_subdiv_mesh.cpp
-		../common/raystream_log.cpp
+    ../common/stat.cpp 
+    ../common/globals.cpp 
+    ../common/alloc.cpp 
+    ../common/tasksys.cpp 
+    ../common/acceln.cpp
+    ../common/rtcore.cpp 
+    ../common/rtcore_ispc.cpp 
+    ../common/rtcore_ispc.ispc 
+    ../common/buffer.cpp
+    ../common/scene.cpp
+    ../common/geometry.cpp
+    ../common/scene_user_geometry.cpp
+    ../common/scene_triangle_mesh.cpp
+    ../common/scene_bezier_curves.cpp
+    ../common/scene_subdiv_mesh.cpp
+    ../common/raystream_log.cpp
 
-		builders/bezierrefgen.cpp 
-		builders/primrefgen.cpp
-		builders/heuristic_object_partition_unaligned.cpp
-		builders/heuristic_object_partition.cpp
-		builders/heuristic_spatial_split.cpp
-		builders/heuristic_strand_partition.cpp
-		builders/heuristic_fallback.cpp
+    builders/bezierrefgen.cpp 
+    builders/primrefgen.cpp
+    builders/heuristic_object_partition_unaligned.cpp
+    builders/heuristic_object_partition.cpp
+    builders/heuristic_spatial_split.cpp
+    builders/heuristic_strand_partition.cpp
+    builders/heuristic_fallback.cpp
     
-		geometry/bezier1v.cpp
-		geometry/bezier1i.cpp
-		geometry/triangle1.cpp
-		geometry/triangle4.cpp
-		geometry/triangle1v.cpp
-		geometry/triangle4v.cpp
-		geometry/triangle4v_mb.cpp
-		geometry/triangle4i.cpp
-		geometry/subdivpatch1.cpp
-		geometry/subdivpatchdispl1.cpp
-		geometry/virtual_accel.cpp
-		geometry/instance_intersector1.cpp
-		geometry/instance_intersector4.cpp
-		geometry/subdivpatch1_intersector1.cpp
-		
-		bvh4/bvh4.cpp
-		bvh4/bvh4_rotate.cpp
-		bvh4/bvh4_refit.cpp
-		bvh4/bvh4_builder.cpp
-		bvh4/bvh4_builder_mb.cpp
-		bvh4/bvh4_builder_hair.cpp
-		bvh4/bvh4_builder_hair_mb.cpp
-		bvh4/bvh4_builder_fast.cpp
-		bvh4/bvh4_builder_morton.cpp
-		bvh4/bvh4_builder_toplevel.cpp
-		bvh4/bvh4_intersector1.cpp   
-		bvh4/bvh4_intersector4_single.cpp
-		bvh4/bvh4_intersector4_chunk.cpp
-		bvh4/bvh4_statistics.cpp
+    geometry/bezier1v.cpp
+    geometry/bezier1i.cpp
+    geometry/triangle1.cpp
+    geometry/triangle4.cpp
+    geometry/triangle1v.cpp
+    geometry/triangle4v.cpp
+    geometry/triangle4v_mb.cpp
+    geometry/triangle4i.cpp
+    geometry/subdivpatch1.cpp
+    geometry/subdivpatchdispl1.cpp
+    geometry/virtual_accel.cpp
+    geometry/instance_intersector1.cpp
+    geometry/instance_intersector4.cpp
+    geometry/subdivpatch1_intersector1.cpp
+    
+    bvh4/bvh4.cpp
+    bvh4/bvh4_rotate.cpp
+    bvh4/bvh4_refit.cpp
+    bvh4/bvh4_builder.cpp
+    bvh4/bvh4_builder_mb.cpp
+    bvh4/bvh4_builder_hair.cpp
+    bvh4/bvh4_builder_hair_mb.cpp
+    bvh4/bvh4_builder_fast.cpp
+    bvh4/bvh4_builder_morton.cpp
+    bvh4/bvh4_builder_toplevel.cpp
+    bvh4/bvh4_intersector1.cpp   
+    bvh4/bvh4_intersector4_single.cpp
+    bvh4/bvh4_intersector4_chunk.cpp
+    bvh4/bvh4_statistics.cpp
     )
 
   # ------------------------------------------------------------------
   # create the *BASE* embree library that's available on all ISAs  
   # ------------------------------------------------------------------
-	SET (OSPRAY_EMBREE_SOURCES "")
-	FOREACH (src ${EMBREE_COMMON_SYS_SOURCES})
-		SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/sys/${src})
-	ENDFOREACH()
-	
-	FOREACH (src ${EMBREE_COMMON_SIMD_SOURCES})
-		SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/simd/${src})
-	ENDFOREACH()
+  SET (OSPRAY_EMBREE_SOURCES "")
+  FOREACH (src ${EMBREE_COMMON_SYS_SOURCES})
+    SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/sys/${src})
+  ENDFOREACH()
+  
+  FOREACH (src ${EMBREE_COMMON_SIMD_SOURCES})
+    SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/common/simd/${src})
+  ENDFOREACH()
 
-	FOREACH (src ${EMBREE_KERNELS_XEON_SOURCES})
-		SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/${src})
-	ENDFOREACH()
-	
+  FOREACH (src ${EMBREE_KERNELS_XEON_SOURCES})
+    SET (OSPRAY_EMBREE_SOURCES ${OSPRAY_EMBREE_SOURCES} ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/${src})
+  ENDFOREACH()
+  
   OSPRAY_ADD_LIBRARY(ospray_embree${OSPRAY_LIB_SUFFIX} SHARED ${OSPRAY_EMBREE_SOURCES})
   TARGET_LINK_LIBRARIES(ospray_embree${OSPRAY_LIB_SUFFIX} pthread dl)
-	
+  
   # ------------------------------------------------------------------
   # now, build and link in SSE41 support (for anything more than SSE)
   # note: "SSE" is a shortcut for SSE42
@@ -230,8 +230,8 @@ ELSE()
       ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/bvh4/bvh4_builder_toplevel.cpp
       ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/bvh4/bvh4_intersector1.cpp   
       ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/bvh4/bvh4_intersector4_chunk.cpp
-			# remove for embree 2.3.3:
-			#      ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/bvh4/bvh4_builder_binner.cpp
+      # remove for embree 2.3.3:
+      #      ${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/bvh4/bvh4_builder_binner.cpp
       )
     SET_TARGET_PROPERTIES(ospray_embree_sse41 PROPERTIES COMPILE_FLAGS "${OSPRAY_ARCH_SSE41}")
     TARGET_LINK_LIBRARIES(ospray_embree ospray_embree_sse41)
@@ -257,54 +257,54 @@ ELSE()
   # ------------------------------------------------------------------
   IF ((${OSPRAY_XEON_TARGET} STREQUAL "AVX2") OR
       (${OSPRAY_XEON_TARGET} STREQUAL "AVX"))
-		SET(EMBREE_KERNELS_AVX_SOURCES
-			builders/bezierrefgen.cpp 
-			builders/primrefgen.cpp
-			builders/heuristic_object_partition_unaligned.cpp
-			builders/heuristic_object_partition.cpp
-			builders/heuristic_spatial_split.cpp
-			builders/heuristic_strand_partition.cpp
-			builders/heuristic_fallback.cpp
+    SET(EMBREE_KERNELS_AVX_SOURCES
+      builders/bezierrefgen.cpp 
+      builders/primrefgen.cpp
+      builders/heuristic_object_partition_unaligned.cpp
+      builders/heuristic_object_partition.cpp
+      builders/heuristic_spatial_split.cpp
+      builders/heuristic_strand_partition.cpp
+      builders/heuristic_fallback.cpp
 
-			geometry/triangle8.cpp
+      geometry/triangle8.cpp
 
-			geometry/instance_intersector1.cpp
-			geometry/instance_intersector4.cpp
-			geometry/instance_intersector8.cpp
+      geometry/instance_intersector1.cpp
+      geometry/instance_intersector4.cpp
+      geometry/instance_intersector8.cpp
 
-			bvh4/bvh4_rotate.cpp
-			bvh4/bvh4_builder_toplevel.cpp
-			bvh4/bvh4_builder_morton.cpp
-			bvh4/bvh4_builder_fast.cpp
-			bvh4/bvh4_builder.cpp
-			bvh4/bvh4_builder_mb.cpp
-			bvh4/bvh4_builder_hair.cpp
-			bvh4/bvh4_builder_hair_mb.cpp
-			bvh4/bvh4_refit.cpp
+      bvh4/bvh4_rotate.cpp
+      bvh4/bvh4_builder_toplevel.cpp
+      bvh4/bvh4_builder_morton.cpp
+      bvh4/bvh4_builder_fast.cpp
+      bvh4/bvh4_builder.cpp
+      bvh4/bvh4_builder_mb.cpp
+      bvh4/bvh4_builder_hair.cpp
+      bvh4/bvh4_builder_hair_mb.cpp
+      bvh4/bvh4_refit.cpp
 
-			bvh4/bvh4_intersector1.cpp   
-			bvh4/bvh4_intersector4_single.cpp
-			bvh4/bvh4_intersector4_chunk.cpp
-			bvh4/bvh4_intersector4_hybrid.cpp
-			bvh4/bvh4_intersector8_single.cpp
-			bvh4/bvh4_intersector8_chunk.cpp
-			bvh4/bvh4_intersector8_hybrid.cpp
+      bvh4/bvh4_intersector1.cpp   
+      bvh4/bvh4_intersector4_single.cpp
+      bvh4/bvh4_intersector4_chunk.cpp
+      bvh4/bvh4_intersector4_hybrid.cpp
+      bvh4/bvh4_intersector8_single.cpp
+      bvh4/bvh4_intersector8_chunk.cpp
+      bvh4/bvh4_intersector8_hybrid.cpp
 
-			bvh8/bvh8.cpp
-			bvh8/bvh8_builder.cpp
-			bvh8/bvh8_statistics.cpp
-			bvh8/bvh8_intersector1.cpp  
-			bvh8/bvh8_intersector4_hybrid.cpp   
-			bvh8/bvh8_intersector8_chunk.cpp   
-			bvh8/bvh8_intersector8_hybrid.cpp   
-			)
-		SET(OSPRAY_EMBREE_AVX_SOURCES "")
-		FOREACH(src ${EMBREE_KERNELS_AVX_SOURCES})
-			SET(OSPRAY_EMBREE_AVX_SOURCES ${OSPRAY_EMBREE_AVX_SOURCES} 		
-				${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/${src})
-		ENDFOREACH()
-		OSPRAY_ADD_LIBRARY(ospray_embree_avx STATIC
-			${OSPRAY_EMBREE_AVX_SOURCES})
+      bvh8/bvh8.cpp
+      bvh8/bvh8_builder.cpp
+      bvh8/bvh8_statistics.cpp
+      bvh8/bvh8_intersector1.cpp  
+      bvh8/bvh8_intersector4_hybrid.cpp   
+      bvh8/bvh8_intersector8_chunk.cpp   
+      bvh8/bvh8_intersector8_hybrid.cpp   
+      )
+    SET(OSPRAY_EMBREE_AVX_SOURCES "")
+    FOREACH(src ${EMBREE_KERNELS_AVX_SOURCES})
+      SET(OSPRAY_EMBREE_AVX_SOURCES ${OSPRAY_EMBREE_AVX_SOURCES} 		
+	${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/${src})
+    ENDFOREACH()
+    OSPRAY_ADD_LIBRARY(ospray_embree_avx STATIC
+      ${OSPRAY_EMBREE_AVX_SOURCES})
     SET_TARGET_PROPERTIES(ospray_embree_avx PROPERTIES COMPILE_FLAGS "${OSPRAY_ARCH_AVX}")
     TARGET_LINK_LIBRARIES(ospray_embree ospray_embree_avx)
   ENDIF()
@@ -312,11 +312,11 @@ ELSE()
   # ------------------------------------------------------------------
   # now, build and link in AVX2 support (for AVX2 only)
   # ------------------------------------------------------------------
-	SET(OSPRAY_EMBREE_AVX2_SOURCES
+  SET(EMBREE_KERNELS_AVX2_SOURCES
     geometry/instance_intersector1.cpp
     geometry/instance_intersector4.cpp
     geometry/instance_intersector8.cpp
-		
+    
     bvh4/bvh4_intersector1.cpp
     bvh4/bvh4_intersector4_single.cpp
     bvh4/bvh4_intersector4_chunk.cpp
@@ -329,15 +329,15 @@ ELSE()
     bvh8/bvh8_intersector4_hybrid.cpp 
     bvh8/bvh8_intersector8_chunk.cpp   
     bvh8/bvh8_intersector8_hybrid.cpp   
-		)
+    )
   IF ((${OSPRAY_XEON_TARGET} STREQUAL "AVX2"))
-		SET(OSPRAY_EMBREE_AVX_SOURCES "")
-		FOREACH(src ${EMBREE_KERNELS_AVX_SOURCES})
-			SET(OSPRAY_EMBREE_AVX_SOURCES ${OSPRAY_EMBREE_AVX_SOURCES} 		
-				${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/${src})
-		ENDFOREACH()
-		OSPRAY_ADD_LIBRARY(ospray_embree_avx2 STATIC
-			${OSPRAY_EMBREE_AVX2_SOURCES})
+    SET(OSPRAY_EMBREE_AVX2_SOURCES "")
+    FOREACH(src ${EMBREE_KERNELS_AVX2_SOURCES})
+      SET(OSPRAY_EMBREE_AVX2_SOURCES ${OSPRAY_EMBREE_AVX2_SOURCES} 		
+	${OSPRAY_EMBREE_SOURCE_DIR}/kernels/xeon/${src})
+    ENDFOREACH()
+    OSPRAY_ADD_LIBRARY(ospray_embree_avx2 STATIC
+      ${OSPRAY_EMBREE_AVX2_SOURCES})
     SET_TARGET_PROPERTIES(ospray_embree_avx2 PROPERTIES COMPILE_FLAGS "${OSPRAY_ARCH_AVX2}")
     TARGET_LINK_LIBRARIES(ospray_embree ospray_embree_avx2)
   ENDIF()
