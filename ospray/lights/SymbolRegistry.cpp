4283848571e48453b2de33b462803a3bda85a431
Ingo Wald <ingowald@gmail.com>
2014-12-09 14:22:50 -0600

    removing old headers, fixing #pragmas, etc

--- ospray/lights/SymbolRegistry.cpp
+++ ospray/lights/SymbolRegistry.cpp
@@ -1,26 +1,17 @@
-//
-//                 INTEL CORPORATION PROPRIETARY INFORMATION
-//
-//    This software is supplied under the terms of a license agreement or
-//    nondisclosure agreement with Intel Corporation and may not be copied
-//    or disclosed except in accordance with the terms of that agreement.
-//    Copyright (C) 2014 Intel Corporation. All Rights Reserved.
-//
-
 #include "ospray/lights/DirectionalLight.h"
 #include "ospray/lights/PointLight.h"
 #include "ospray/lights/SpotLight.h"
 
 namespace ospray {
 
-    //! A directional light.
-    OSP_REGISTER_LIGHT(DirectionalLight, DirectionalLight);
+  //! A directional light.
+  OSP_REGISTER_LIGHT(DirectionalLight, DirectionalLight);
 
-    //! A point light with bounded range.
-    OSP_REGISTER_LIGHT(PointLight, PointLight);
+  //! A point light with bounded range.
+  OSP_REGISTER_LIGHT(PointLight, PointLight);
 
-    //! A spot light with bounded angle and range.
-    OSP_REGISTER_LIGHT(SpotLight, SpotLight);
+  //! A spot light with bounded angle and range.
+  OSP_REGISTER_LIGHT(SpotLight, SpotLight);
 
 } // namespace ospray
 
