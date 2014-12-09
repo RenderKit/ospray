976794566af8832ef687a867549328382d2779af
Ingo Wald <ingowald@gmail.com>
2014-12-09 14:02:03 -0600

    fixed all includes and renames in path tracer

--- modules/pathtracer/PathTracer.cpp
+++ modules/pathtracer/PathTracer.cpp
@@ -1,16 +1,8 @@
-/********************************************************************* *\
- * INTEL CORPORATION PROPRIETARY INFORMATION                            
- * This software is supplied under the terms of a license agreement or  
- * nondisclosure agreement with Intel Corporation and may not be copied 
- * or disclosed except in accordance with the terms of that agreement.  
- * Copyright (C) 2014 Intel Corporation. All Rights Reserved.           
- ********************************************************************* */
-
 #undef NDEBUG
 
 // ospray
-#include "pathtracer.h"
-#include "pathtracer_ispc.h"
+#include "PathTracer.h"
+#include "PathTracer_ispc.h"
 // std
 #include <map>
 
