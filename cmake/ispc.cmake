73d4d66a5e9895f59a4e7f6aacd9a41053f5c56f
Ingo Wald <ingowald@gmail.com>
2014-12-09 14:05:44 -0600

    proper capitalization for knc.h

--- cmake/ispc.cmake
+++ cmake/ispc.cmake
@@ -75,7 +75,7 @@ MACRO (ispc_compile)
   ENDIF()
 
   IF (THIS_IS_MIC)
-    SET(CMAKE_ISPC_FLAGS --opt=force-aligned-memory --target generic-16 --emit-c++ --c++-include-file=${PROJECT_SOURCE_DIR}/ospray/common/IspcKncBackend.h  --addressing=${ISPC_ADDRESSING})
+    SET(CMAKE_ISPC_FLAGS --opt=force-aligned-memory --target generic-16 --emit-c++ --c++-include-file=${PROJECT_SOURCE_DIR}/ospray/common/ISPC_KNC_Backend.h  --addressing=${ISPC_ADDRESSING})
     #${ISPC_DIR}/examples/intrinsics/knc.h)
     SET(ISPC_TARGET_EXT ".dev.cpp")
   ELSE()
