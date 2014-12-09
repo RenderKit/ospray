12078e817b2bd10bb8a65e72d39413b4cb819268
Ingo Wald <ingowald@gmail.com>
2014-12-09 17:51:53 -0600

    done manual cleanup pass. can't find a single un-touched file any more. pushing to create a restore-point prior to slapping on new license

--- apps/particleViewer/Model.h
+++ apps/particleViewer/Model.h
@@ -1,10 +1,3 @@
-/********************************************************************* *\
- * INTEL CORPORATION PROPRIETARY INFORMATION                            
- * This software is supplied under the terms of a license agreement or  
- * nondisclosure agreement with Intel Corporation and may not be copied 
- * or disclosed except in accordance with the terms of that agreement.  
- * Copyright (C) 2014 Intel Corporation. All Rights Reserved.           
- ********************************************************************* */
 #pragma once
 
 // ospray
@@ -107,5 +100,5 @@ namespace ospray {
       }
     };
 
-  }
-}
+  } // ::ospray::particle
+} // ::ospray
