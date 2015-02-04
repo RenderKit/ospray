2d8933f1dc899a284aeb0b702ea5db7d9c6f2896
Gregory P. Johnson <gregory.p.johnson@intel.com>
2015-02-03 20:32:45 -0600

    MPI build changes for mvapich2.

--- cmake/mpi.cmake
+++ cmake/mpi.cmake
@@ -15,55 +15,25 @@
 ## ======================================================================== ##
 
 IF (OSPRAY_MPI)
-  if (OSPRAY_COMPILER STREQUAL "ICC")
-    find_program(MPI_COMPILER 
-      NAMES mpiicpc
-      DOC "MPI compiler.")
-    SET(OSPRAY_MPI_MULTI_THREADING_FLAG "-mt_mpi")
-  else()
-    find_package(MPI)
-    if (${MPI_COMPILER} STREQUAL "MPI_COMPILER-NOTFOUND")
-      find_program(MPI_COMPILER
-        NAMES mpicxx
-        PATHS /usr/lib64/openmpi/bin
-        DOC "MPI compiler.")
-      find_library(MPI_LIBRARY
-        NAMES mpi
-        PATHS /usr/lib64/openmpi/lib
-        DOC "MPI library.")
-      SET(MPI_LIBRARIES ${MPI_LIBRARY})
-    endif()
-    SET(OSPRAY_MPI_MULTI_THREADING_FLAG "") #???
-  endif()
-  if (${MPI_COMPILER} STREQUAL "MPI_COMPILER-NOTFOUND")
-    message("could not find mpi compiler")
-  endif()
-  mark_as_advanced(MPI_COMPILER)
+  FIND_PACKAGE(MPI)
 
-  exec_program(${MPI_COMPILER} 
-    ARGS -show
-    OUTPUT_VARIABLE MPI_COMPILE_CMDLINE
-    RETURN_VALUE MPI_COMPILER_RETURN)
+  IF (NOT MPI_CXX_FOUND)
+    MESSAGE(FATAL_ERROR "MPI C++ not found")
+  ENDIF()
 
-  # Extract include paths from compile command line
-  string(REGEX MATCHALL "-I([^\" ]+|\"[^\"]+\")" MPI_ALL_INCLUDE_PATHS "${MPI_COMPILE_CMDLINE}")
-  set(MPI_INCLUDE_PATH_WORK)
-  foreach(IPATH ${MPI_ALL_INCLUDE_PATHS})
-    string(REGEX REPLACE "^-I" "" IPATH ${IPATH})
-    string(REGEX REPLACE "//" "/" IPATH ${IPATH})
-    list(APPEND MPI_INCLUDE_PATH_WORK ${IPATH})
-  endforeach(IPATH)
+  IF (${MPI_CXX_COMPILER} STREQUAL "MPI_CXX_COMPILER-NOTFOUND")
+    message(FATAL_ERROR "MPI C++ compiler not found")
+  ENDIF()
 
-  set(MPI_INCLUDE_PATH ${MPI_INCLUDE_PATH_WORK} )
+  # need to find a way to set this as needed for different MPI versions.
+  # -mt_mpi needed for Intel MPI.
+  SET(OSPRAY_MPI_MULTI_THREADING_FLAG "")
 
   MACRO(CONFIGURE_MPI)
-    INCLUDE_DIRECTORIES(${MPI_INCLUDE_PATH})
-    SET(CMAKE_CXX_COMPILER ${MPI_COMPILER})
-    SET(APP_CMAKE_CXX_FLAGS "${OSPRAY_MPI_MULTI_THREADING_FLAG}")
-    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE}")
-    SET(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG}")
-    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${OSPRAY_MPI_MULTI_THREADING_FLAG}")
-    SET(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} ${OSPRAY_MPI_MULTI_THREADING_FLAG}")
+    INCLUDE_DIRECTORIES(${MPI_CXX_INCLUDE_PATH})
+    SET(CMAKE_CXX_COMPILER ${MPI_CXX_COMPILER})
+    SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${MPI_CXX_COMPILE_FLAGS} ${OSPRAY_MPI_MULTI_THREADING_FLAG}")
+    SET(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} ${MPI_CXX_COMPILE_FLAGS} ${OSPRAY_MPI_MULTI_THREADING_FLAG}")
 
     IF (THIS_IS_MIC)
       SET( CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -mmic" )
