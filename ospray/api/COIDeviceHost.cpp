a19685e3957249dbec59a59dde87553a227767fe
Gregory P. Johnson <gregory.p.johnson@intel.com>
2015-02-23 16:40:34 -0600

    COI build fix.

--- ospray/api/COIDeviceHost.cpp
+++ ospray/api/COIDeviceHost.cpp
@@ -1065,7 +1065,7 @@ namespace ospray {
       getString(object, "voxelType", &typeString);
       OSPDataType type = typeForString(typeString);
       Assert(type != OSP_UNKNOWN && "unknown volume element type");
-      OSPData data = newData(count.x * count.y * count.z, type, source, OSP_DATA_SHARED_BUFFER);
+      OSPData data = newData(count.x * count.y * count.z, type, (void *)source, OSP_DATA_SHARED_BUFFER);
 
       int result;
       DataStream stream;
