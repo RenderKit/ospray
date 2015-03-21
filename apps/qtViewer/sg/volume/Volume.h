0577d9056eb8714456f8e0516a1d6e3020efb09a
Ingo Wald <ingo.wald@intel.com>
2015-03-21 11:50:40 -0500

    volume now has transferfct rather than material, and uses proper set/get fcts

--- apps/qtViewer/sg/volume/Volume.h
+++ apps/qtViewer/sg/volume/Volume.h
@@ -16,14 +16,14 @@
 
 #pragma once
 
-#include "sg/common/Material.h"
+#include "sg/common/TransferFunction.h"
 
 namespace ospray {
   namespace sg {
 
     /*! a geometry node - the generic geometry node */
     struct Volume : public sg::Node {
-      Volume(const std::string &type) : type(type), material(NULL) {};
+      Volume() {};
 
       /*! \brief returns a std::string with the c++ name of this class */
       virtual    std::string toString() const { return "ospray::sg::Volume"; }
@@ -31,11 +31,7 @@ namespace ospray {
       //! return bounding box of all primitives
       virtual box3f getBounds() = 0;
 
-      /*! geometry type, i.e., 'spheres', 'cylinders', 'trianglemesh', ... */
-      const std::string type; 
-      
-      /*! material for this geometry */
-      Ref<Material> material;
+      SG_NODE_DECLARE_MEMBER(Ref<TransferFunction>,transferFunctionfovy,TransferFunction);    
     };
     
   } // ::ospray::sg
