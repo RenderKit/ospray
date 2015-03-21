6fcf105e820c213c5ea136a2bf2b375d3e7f1d07
Ingo Wald <ingo.wald@intel.com>
2015-03-21 11:45:25 -0500

    updated all license headers to MIT/2015. fixes #9

--- apps/qtViewer/sg/importer/Importer.h
+++ apps/qtViewer/sg/importer/Importer.h
@@ -1,5 +1,5 @@
 // ======================================================================== //
-// Copyright 2009-2014 Intel Corporation                                    //
+// Copyright 2009-2015 Intel Corporation                                    //
 //                                                                          //
 // Licensed under the Apache License, Version 2.0 (the "License");          //
 // you may not use this file except in compliance with the License.         //
@@ -24,6 +24,7 @@
 
 namespace ospray {
   namespace sg {
+
     using embree::FileName;
 
     struct ImportState {
