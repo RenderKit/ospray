68df14d6396c2cdd24d09e701c0a34ae1a8112c8
Ingo Wald <ingo.wald@intel.com>
2015-08-20 13:47:04 -0500

    fixed parentheses warning

--- apps/qtViewer/main.cpp
+++ apps/qtViewer/main.cpp
@@ -127,8 +127,8 @@ namespace ospray {
             world = sg::loadOSP(fn.str());
             // } else if (fn.ext() == "atom") {
             //   world = sg::AlphaSpheres::importOspAtomFile(fn.str());
-          } else if (fn.ext() == "ply" || 
-                     fn.ext() == "gz" && fn.dropExt().ext() == "ply") {
+          } else if ((fn.ext() == "ply") || 
+                     ((fn.ext() == "gz") && (fn.dropExt().ext() == "ply"))) {
             sg::importPLY(world,fn);
           } else if (fn.ext() == "obj") {
             sg::importOBJ(world,fn);
