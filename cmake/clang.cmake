fb0a5f12775efc24e1fd681a0c11506de5042982
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2015-09-25 18:57:32 -0400

    remove hard override of compiler variables, we now know the compiler used

--- cmake/clang.cmake
+++ cmake/clang.cmake
@@ -14,9 +14,6 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-SET(CMAKE_CXX_COMPILER "clang++")
-SET(CMAKE_C_COMPILER "clang")
-
 IF (OSPRAY_ALLOW_EXTERNAL_EMBREE)
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC -fno-strict-aliasing -Wno-narrowing -std=c++11 -stdlib=libc++")
 ELSE()
