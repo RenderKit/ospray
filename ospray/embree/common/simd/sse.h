1e7fcdd56ba7184d8a436cda324fec53f953cac9
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2015-10-28 11:19:38 -0400

    fix for building with TBB and icpc

--- ospray/embree/common/simd/sse.h
+++ ospray/embree/common/simd/sse.h
@@ -21,10 +21,11 @@
 #include "sse_special.h"
 
 /* Workaround for Compiler bug in VS2008 */
-#if !defined(__SSE4_1__) || defined(_MSC_VER) && (_MSC_VER < 1600) && !defined(__INTEL_COMPILER)
+#if (!defined(OSPRAY_USE_TBB) && defined(__INTEL_COMPILER)) && \
+    !defined(__SSE4_1__) || defined(_MSC_VER) && (_MSC_VER < 1600) && !defined(__INTEL_COMPILER)
   #define _mm_blendv_ps __emu_mm_blendv_ps
-  __forceinline __m128 _mm_blendv_ps( __m128 f, __m128 t, __m128 mask ) { 
-    return _mm_or_ps(_mm_and_ps(mask, t), _mm_andnot_ps(mask, f)); 
+  __forceinline __m128 _mm_blendv_ps( __m128 f, __m128 t, __m128 mask ) {
+    return _mm_or_ps(_mm_and_ps(mask, t), _mm_andnot_ps(mask, f));
   }
 #endif
 
@@ -36,7 +37,7 @@
   }
 #endif
 
-namespace embree 
+namespace embree
 {
   extern const __m128 _mm_lookupmask_ps[16];
 
