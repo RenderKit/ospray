7c4327d393c1ec931fd72433487c06462ddcabdc
Ingo Wald <ingo.wald@intel.com>
2015-10-30 09:57:29 -0500

    added support for 'Spheres' nodes in qtviewer ospxml input files

--- apps/qtViewer/sg/geometry/Spheres.h
+++ apps/qtViewer/sg/geometry/Spheres.h
@@ -48,105 +48,28 @@ namespace ospray {
       /*! 'render' the nodes */
       virtual void render(RenderContext &ctx);
 
+      //! \brief Initialize this node's value from given XML node 
+      /*!
+        \detailed This allows a plug-and-play concept where a XML
+        file can specify all kind of nodes wihout needing to know
+        their actual types: The XML parser only needs to be able to
+        create a proper C++ instance of the given node type (the
+        OSP_REGISTER_SG_NODE() macro will allow it to do so), and can
+        tell the node to parse itself from the given XML content and
+        XML children 
+        
+        \param node The XML node specifying this node's fields
+
+        \param binBasePtr A pointer to an accompanying binary file (if
+        existant) that contains additional binary data that the xml
+        node fields may point into
+      */
+      void setFromXML(const xml::Node *const node, const unsigned char *binBasePtr);
+
       OSPGeometry         ospGeometry;
       std::vector<Sphere> sphere;
     };
 
-
-    // // =======================================================
-    // /*! \brief Realizes a set of spheres with associated attributes
-    //     and transfer function for color- and alaha-mapping.  */
-    // // =======================================================
-    // struct AlphaSpheres : public sg::Geometry {
-
-    //   //! constructor
-    //   AlphaSpheres() 
-    //     : Geometry("alpha_spheres"), 
-    //       transferFunction(NULL),
-    //       ospPositionData(NULL),
-    //       ospGeometry(NULL)
-    //   {};
-
-    //   //! destructor
-    //   virtual ~AlphaSpheres() { throw std::runtime_error("alpha-spheres destructed!?"); }
-      
-    //   //! set of attributes. must have one attribute value per valid
-    //   //! ID in the spheres
-    //   struct Attribute : public embree::RefCount, public std::vector<float> {
-
-    //     //! constructor
-    //     Attribute(const std::string &name) : name(name), ospData(NULL) {};
-
-    //   protected:
-    //     const std::string name;
-    //     //! the data we use to store this attribute in
-    //     OSPData ospData;
-
-    //     friend class AlphaSpheres;
-    //   };
-
-    //   //! set radius to use for the spheres
-    //   void setRadius(const float radius);
-
-    //   //! set transferFunction to use for the spheres
-    //   void setTransferFunction(Ref<sg::TransferFunction> transferFunction);
-
-    //   //! \brief set radius to use for the spheres
-    //   /*! \detailed This function may NOT get called once the geometry
-    //       got 'render'ed for the first time */
-    //   void setPositions(const std::vector<vec3f> &positions);
-
-    //   //! return a list of the names of all attributes we have in this geometry
-    //   std::vector<std::string> getAttributeNames() const;
-
-    //   //! add a new set of attributes to the geometry
-    //   void addAttribute(Attribute *attribute);
-
-    //   //! add a new set of attributes to the geometry
-    //   void setActiveAttribute(Attribute *attribute);
-
-    //   //! select a new attribute
-    //   void setActiveAttribute(const std::string &attrName) 
-    //   { setActiveAttribute(attribute[attrName].ptr); }
-
-
-    //   //! \brief Initialize this node's value from given XML node 
-    //   virtual void setFromXML(const xml::Node *const node);
-      
-    //   //! return bounding box of this node (in local space)
-    //   virtual box3f getBounds();
-
-    //   /*! 'render' the nodes - all geometries, materials, etc will
-    //       create their ospray counterparts, and store them in the
-    //       node  */
-    //   virtual void render(RenderContext &ctx);
-
-    // protected:
-    //   /*! single radius applied to all spheres in this geometry */
-    //   float radius;
-      
-    //   //! the ospray geometry we use for this set of alpha-spheres
-    //   OSPGeometry ospGeometry;
-
-    //   //! vector os sphere positions; one per sphere
-    //   std::vector<vec3f> position;
-
-    //   //! data array that keeps the sphere positions
-    //   OSPData ospPositionData;
-
-    //   //! the different sets of attributes that can be used for this sphere
-    //   std::map<std::string,Ref<Attribute> > attribute;
-
-    //   //! the attribute currently used for color mapping
-    //   Ref<Attribute> activeAttribute;
-
-    //   //! the transfer function we use for color-mapping the given attribute
-    //   Ref<TransferFunction>           transferFunction;
-      
-    // public:
-    //   static sg::World *importOspAtomFile(const std::string &fileName);
-    // };
-    
   } // ::ospray::sg
 } // ::ospray
 
--- apps/qtViewer/sg/geometry/TriangleMesh.cpp
+++ apps/qtViewer/sg/geometry/TriangleMesh.cpp
@@ -39,22 +39,22 @@ namespace ospray {
       return bounds;
     }
 
-      //! \brief Initialize this node's value from given XML node 
-      /*!
-        \detailed This allows a plug-and-play concept where a XML
-        file can specify all kind of nodes wihout needing to know
-        their actual types: The XML parser only needs to be able to
-        create a proper C++ instance of the given node type (the
-        OSP_REGISTER_SG_NODE() macro will allow it to do so), and can
-        tell the node to parse itself from the given XML content and
-        XML children 
+    //! \brief Initialize this node's value from given XML node 
+    /*!
+      \detailed This allows a plug-and-play concept where a XML
+      file can specify all kind of nodes wihout needing to know
+      their actual types: The XML parser only needs to be able to
+      create a proper C++ instance of the given node type (the
+      OSP_REGISTER_SG_NODE() macro will allow it to do so), and can
+      tell the node to parse itself from the given XML content and
+      XML children 
         
-        \param node The XML node specifying this node's fields
+      \param node The XML node specifying this node's fields
 
-        \param binBasePtr A pointer to an accompanying binary file (if
-        existant) that contains additional binary data that the xml
-        node fields may point into
-      */
+      \param binBasePtr A pointer to an accompanying binary file (if
+      existant) that contains additional binary data that the xml
+      node fields may point into
+    */
     void TriangleMesh::setFromXML(const xml::Node *const node, const unsigned char *binBasePtr) 
     {
       for (size_t childID=0;childID<node->child.size();childID++) {
