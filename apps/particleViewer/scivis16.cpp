d9c8c67025cf2b37c97b82264b5ae79fd5d9ea1f
Pavol Klacansky <pavol@klacansky.com>
2015-12-28 14:25:19 +0000

    Read attributes for filtering
    How to use them in particle viewer?

--- apps/particleViewer/scivis16.cpp
+++ apps/particleViewer/scivis16.cpp
@@ -1,3 +1,18 @@
+/*
+Data Format
+-----------
+- position - float x, float y, float z
+- velocity - float x, float y, float z
+- concentration - float x
+
+The `magic_offset` points to the start of the binary raw data.
+
+
+Notes
+-----
+- always 4 byte padding before each array
+*/
+
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -23,33 +38,60 @@ Model *parse_scivis16(char const *filepath)
 
         /* process header */
         struct {
-                uint32_t _pad3;
+                uint32_t pad_size;
                 uint32_t size;
-                uint32_t _pad1;
+                uint32_t pad_step;
                 uint32_t step;
-                uint32_t _pad2;
+                uint32_t pad_time;
                 float time;
         } header;
 
-        fread(&header, sizeof (header), 1, fp);
+        fread(&header, sizeof header, 1, fp);
 
 
         Model *model = new Model;
 
-        /* read particles */
+
+        /* read particles position */
+        fseek(fp, 4, SEEK_CUR);
+
         Model::AtomType *atom_type = new Model::AtomType("position");
         atom_type->color = vec3f(1.0f, 0.0f, 0.0f);
         model->atomType.push_back(atom_type);
 
         for (uint32_t i = 0; i < header.size; ++i) {
                 float co[3];
-                fread(co, sizeof (co), 1, fp);
+                fread(co, sizeof co, 1, fp);
 
                 Model::Atom atom;
                 atom.position = vec3f(co[0], co[1], co[2]);
                 atom.radius = 0.05f;
                 atom.type = 0;
                 model->atom.push_back(atom);
+        }
+
+
+        /* read particles velocity */
+        fseek(fp, 4, SEEK_CUR);
+
+        for (uint32_t i = 0; i < header.size; ++i) {
+                float co[3];
+                fread(co, sizeof co, 1, fp);
+
+                model->addAttribute("velocity_x", co[0]);
+                model->addAttribute("velocity_y", co[1]);
+                model->addAttribute("velocity_z", co[2]);
+        }
+
+
+        /* read particles concentration */
+        fseek(fp, 4, SEEK_CUR);
+
+        for (uint32_t i = 0; i < header.size; ++i) {
+                float concentration;
+                assert(fread(concentration, sizeof concentration, 1, fp));
+
+                model->addAttribute("concentration", concentration);
         }
 
 
