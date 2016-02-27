5bc668e08af55b3a22506b8672dba612c6771db5
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2016-02-26 20:22:56 -0600

    make similar simplification to InterleavedTiledLoadBalancer

--- ospray/render/LoadBalancer.cpp
+++ ospray/render/LoadBalancer.cpp
@@ -51,6 +51,7 @@ namespace ospray {
     int numTiles_y = divRoundUp(fb->size.y,TILE_SIZE);
 
     const int NTASKS = numTiles_x * numTiles_y;
+
     parallel_for(NTASKS, [&](int taskIndex){
       Tile tile;
       const size_t tile_y = taskIndex / numTiles_x;
@@ -85,72 +86,57 @@ namespace ospray {
     return "ospray::LocalTiledLoadBalancer";
   }
 
-  void InterleavedTiledLoadBalancer::RenderTask::run(size_t taskIndex) const
-  {
-    int tileIndex = deviceID + numDevices * taskIndex;
-
-    Tile tile;
-    const size_t tile_y = tileIndex / numTiles_x;
-    const size_t tile_x = tileIndex - tile_y*numTiles_x;
-    tile.region.lower.x = tile_x * TILE_SIZE;
-    tile.region.lower.y = tile_y * TILE_SIZE;
-    tile.region.upper.x = std::min(tile.region.lower.x+TILE_SIZE,fb->size.x);
-    tile.region.upper.y = std::min(tile.region.lower.y+TILE_SIZE,fb->size.y);
-    tile.fbSize = fb->size;
-    tile.rcp_fbSize = rcp(vec2f(tile.fbSize));
-    tile.generation = 0;
-    tile.children = 0;
-
-    const int spp = renderer->spp;
-    const int blocks = (fb->accumID > 0 || spp > 0) ? 1 :
-                       std::min(1 << -2 * spp, TILE_SIZE*TILE_SIZE);
-    const size_t numJobs = ((TILE_SIZE*TILE_SIZE)/
-                            RENDERTILE_PIXELS_PER_JOB + blocks-1)/blocks;
-
-    parallel_for(numJobs, [&](int taskIndex){
-      renderer->renderTile(perFrameData, tile, taskIndex);
-    });
-
-    fb->setTile(tile);
-  }
-
-
-  void InterleavedTiledLoadBalancer::RenderTask::finish() const
-  {
-    renderer->endFrame(perFrameData,channelFlags);
-    renderer = NULL;
-    fb = NULL;
-  }
-
   /*! render a frame via the tiled load balancer */
-  void InterleavedTiledLoadBalancer::renderFrame(Renderer *tiledRenderer,
+  std::string InterleavedTiledLoadBalancer::toString() const
+  {
+    return "ospray::InterleavedTiledLoadBalancer";
+  }
+
+  void InterleavedTiledLoadBalancer::renderFrame(Renderer *renderer,
                                                  FrameBuffer *fb,
                                                  const uint32 channelFlags)
   {
-    Assert(tiledRenderer);
+    Assert(renderer);
     Assert(fb);
 
-    void *perFrameData = tiledRenderer->beginFrame(fb);
+    void *perFrameData = renderer->beginFrame(fb);
 
-    RenderTask renderTask;
-    renderTask.fb = fb;
-    renderTask.perFrameData = perFrameData;
-    renderTask.renderer     = tiledRenderer;
-    renderTask.numTiles_x   = divRoundUp(fb->size.x,TILE_SIZE);
-    renderTask.numTiles_y   = divRoundUp(fb->size.y,TILE_SIZE);
-    size_t numTiles_total   = renderTask.numTiles_x * renderTask.numTiles_y;
+    int numTiles_x     = divRoundUp(fb->size.x,TILE_SIZE);
+    int numTiles_y     = divRoundUp(fb->size.y,TILE_SIZE);
+    int numTiles_total = numTiles_x * numTiles_y;
 
-    renderTask.numTiles_mine
-      = (numTiles_total / numDevices)
-      + (numTiles_total % numDevices > deviceID);
-    renderTask.channelFlags = channelFlags;
-    renderTask.deviceID     = deviceID;
-    renderTask.numDevices   = numDevices;
+    const int NTASKS = (numTiles_total / numDevices)
+                       + (numTiles_total % numDevices > deviceID);
 
-    const int NTASKS = renderTask.numTiles_mine;
-    parallel_for(NTASKS, [&](int taskIndex){renderTask.run(taskIndex);});
+    parallel_for(NTASKS, [&](int taskIndex){
+      int tileIndex = deviceID + numDevices * taskIndex;
 
-    renderTask.finish();
+      Tile tile;
+      const size_t tile_y = tileIndex / numTiles_x;
+      const size_t tile_x = tileIndex - tile_y*numTiles_x;
+      tile.region.lower.x = tile_x * TILE_SIZE;
+      tile.region.lower.y = tile_y * TILE_SIZE;
+      tile.region.upper.x = std::min(tile.region.lower.x+TILE_SIZE,fb->size.x);
+      tile.region.upper.y = std::min(tile.region.lower.y+TILE_SIZE,fb->size.y);
+      tile.fbSize = fb->size;
+      tile.rcp_fbSize = rcp(vec2f(tile.fbSize));
+      tile.generation = 0;
+      tile.children = 0;
+
+      const int spp = renderer->spp;
+      const int blocks = (fb->accumID > 0 || spp > 0) ? 1 :
+                         std::min(1 << -2 * spp, TILE_SIZE*TILE_SIZE);
+      const size_t numJobs = ((TILE_SIZE*TILE_SIZE)/
+                              RENDERTILE_PIXELS_PER_JOB + blocks-1)/blocks;
+
+      parallel_for(numJobs, [&](int taskIndex){
+        renderer->renderTile(perFrameData, tile, taskIndex);
+      });
+
+      fb->setTile(tile);
+    });
+
+    renderer->endFrame(perFrameData,channelFlags);
   }
 
 } // ::ospray
