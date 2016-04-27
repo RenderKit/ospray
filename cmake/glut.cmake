RenderKit/ospray

commit a218f6c4fc84b5b7857fa4a1f5867caf598cdef9

make GLUT CMake error more helpful, remove dead CMake code

cmake/glut.cmake
@@ -56,9 +56,7 @@ ELSEIF (WIN32)
   ENDIF()
 ELSE()
   FIND_PACKAGE(GLUT)
-  IF (GLUT_FOUND)
-    SET(GLUT_LIBRARIES glut GLU m)
-  ELSE()
+  IF (NOT GLUT_FOUND)
     FIND_PATH(GLUT_INCLUDE_DIR GL/glut.h 
       $ENV{TACC_FREEGLUT_INC}
       )
@@ -67,7 +65,16 @@ ELSE()
       )
     FIND_LIBRARY(GLUT_LIBRARIES NAMES libglut.so PATHS $ENV{TACC_FREEGLUT_LIB})
     IF (NOT GLUT_LIBRARIES)
-      MESSAGE(FATAL_ERROR "Could not find GLUT library, even after trying additional search dirs")
+      MESSAGE(FATAL_ERROR "Could not find GLUT library, even after trying"
+                          " additional search dirs."
+                          " Please disable the following to build without GLUT:"
+                          "\n"
+                          " OSPRAY_APPS_MODELVIEWER,"
+                          " OSPRAY_APPS_PARTICLEVIEWER,"
+                          " OSPRAY_APPS_QTVIEWER,"
+                          " OSPRAY_APPS_STREAMLINEVIEWER,"
+                          " OSPRAY_MODULE_TACHYON"
+                          "\n")
     ELSE()
       SET(GLUT_FOUND ON)
     ENDIF()

apps/qtViewer/CMakeLists.txt
@@ -16,9 +16,6 @@
 
 CONFIGURE_OSPRAY()
 
-#MESSAGE("EXTERNAL_EMBREE_DIR ${EXTERNAL_EMBREE_DIR}")
-#MESSAGE("OSPRAY_EMBREE_SOURCE_DIR ${OSPRAY_EMBREE_SOURCE_DIR}")
-
 INCLUDE_DIRECTORIES(${PROJECT_SOURCE_DIR}/ospray/include)
 INCLUDE_DIRECTORIES(${PROJECT_SOURCE_DIR}/)
 INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR})

commit d9be5233c7491d4df0b6b0272be99bc6ff257d14

revert CMake bug introduced by a218f6c4

cmake/glut.cmake
@@ -56,7 +56,9 @@ ELSEIF (WIN32)
   ENDIF()
 ELSE()
   FIND_PACKAGE(GLUT)
-  IF (NOT GLUT_FOUND)
+  IF (GLUT_FOUND)
+    SET(GLUT_LIBRARIES glut GLU m)
+  ELSE()
     FIND_PATH(GLUT_INCLUDE_DIR GL/glut.h 
       $ENV{TACC_FREEGLUT_INC}
       )