6e528416a838128d15b96e532c59f8cd4a65a307
Will Usher <willusher.life@gmail.com>
2016-06-14 13:22:39 -0500

    Updating loft office script to use the backplate and hdri light

--- loft_office_run.sh
+++ loft_office_run.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#SBATCH -J loft_office_bench
-#SBATCH -o loft_office_bench.txt
-#SBATCH -N 3
-#SBATCH -n 3
-#SBATCH -p development
-#SBATCH -t 00:10:00
+
+DATA_PATH=${WORK}/data/LoftOffice
 
 OSP_MPI="--osp:mpi"
 if [ $SLURM_NNODES -eq 1 ]; then
@@ -12,10 +8,13 @@ if [ $SLURM_NNODES -eq 1 ]; then
 	OSP_MPI=""
 fi
 
-# This camera position is sort of in the back-middle looking over the office interior
-CAM_VP="3.373260 -9.858215 6.385623"
-CAM_VU="0.0 1.0 0.0"
-CAM_VI="3.401789 -0.356800 3.917832"
+# this is the camera pos from the Embree scene script
+CAM_VP="7.63728 -9.89146 3.08021"
+CAM_VI="1.4654 -0.462097 3.01829"
+CAM_VU="0 -0.0034912 0.999994"
+
+# We take num workers samples per pixel so that's num nodes - 1 to account for the master
+((SPP = $SLURM_NNODES - 1))
 
 # Print the expanded list of workers hosts so we can easily paste into osp_separate_launch
 #scontrol show hostname $SLURM_NODELIST > stamp-worker-nodes.txt
@@ -23,8 +22,12 @@ CAM_VI="3.401789 -0.356800 3.917832"
 WORKER_HOSTS=`scontrol show hostname $SLURM_NODELIST | tr '\n' ',' | sed s/,$//`
 echo "Loft Office bench using hosts $WORKER_HOSTS"
 set -x
-ibrun ./ospModelViewer /work/03160/will/data/LoftOffice/LoftOffice.obj $OSP_MPI \
-	--renderer pathtracer --bench-out loft_office_spp_scale$SLURM_NNODES.ppm \
-	--max-depth 15 --spp $SLURM_NNODES --bench 25x25 --max-accum 1 --accum-reset --nowin --1k \
-	-vp $CAM_VP -vu $CAM_VU -vi $CAM_VI
+cd $WORK_DIR
+ibrun ./ospModelViewer ${DATA_PATH}/LoftOffice/LoftOffice.obj $OSP_MPI \
+	--renderer pathtracer --bench-out ${OUT_DIR}/${SLURM_JOB_NAME}.ppm \
+	--max-depth 15 --spp $SPP --bench 25x25 --max-accum 1 \
+  --accum-reset --nowin --1k \
+	-vp $CAM_VP -vu $CAM_VU -vi $CAM_VI \
+  --hdri-light 1 ${DATA_PATH}/LoftOffice/light_tent_by_lightmapltd.pfm \
+  --backplate ${DATA_PATH}/LoftOffice/background.ppm
 
--- loft_office_scaling.sh
+++ loft_office_scaling.sh
@@ -5,13 +5,18 @@
 # case is a test of 1 worker 1 master to compare with a single combined worker/master when
 # running non-distributed with a single node
 #node_counts=(1 2 3 5 9 17 33 65)
-node_counts=(1 5 17 65 129)
+# Use these node counts for the spp scaling
+node_counts=(1 5 17 65 117 129)
 queue="normal"
-job_time="00:05:00"
+job_time="00:15:00"
+
+export WORK_DIR=$WORK/ospray/build/rel/
+export OUT_DIR=`pwd`
+export RUN_SCRIPT=$WORK/ospray/loft_office_run.sh
 
 for nodes in ${node_counts[@]}; do
 	set -x
 	sbatch -J loft_office_spp_scale$nodes -o loft_office_spp_scale${nodes}.txt -N $nodes -n $nodes \
-		-p $queue -t $job_time ../../loft_office_run.sh
+		-p $queue -t $job_time $RUN_SCRIPT
 done
 
