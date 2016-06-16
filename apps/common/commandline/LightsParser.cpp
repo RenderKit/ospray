ec62956b2e33911b3073b9a7af6d6c45221222d6
Laura Lediaev <lediaev@sci.utah.edu>
2016-06-16 13:49:55 -0500

    update

--- apps/common/commandline/LightsParser.cpp
+++ apps/common/commandline/LightsParser.cpp
@@ -28,8 +28,8 @@ bool DefaultLightsParser::parse(int ac, const char **&av)
     } else if (arg == "--hdri-light") {
       // Syntax for HDRI light is the same as Embree:
         // --hdri-light <intensity> <image file>.(pfm|ppm)
-        OSPLight ospHdri = m_renderer.newLight("hdri");
-        ospSetString(ospHdri, "name", "hdri_test");
+        auto ospHdri = m_renderer.newLight("hdri");
+        ospHdri.set("name", "hdri light");//   ospSetString(ospHdri, "name", "hdri_test");
         if (1) {
         ospSet3f(ospHdri, "up", 0.f, 1.f, 0.f);
         ospSet3f(ospHdri, "dir", 1.f, 0.f, 0.0f);
