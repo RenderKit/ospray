0056d95d729a92e6c006772ec251a28c0eca09c5
Will Usher <willusher.life@gmail.com>
2016-06-16 15:49:49 -0500

    Use job name in loft office launch script

--- loft_office_scaling.sh
+++ loft_office_scaling.sh
@@ -4,11 +4,12 @@
 # Note that for distributed runs we have one extra node for the master though the 2 node
 # case is a test of 1 worker 1 master to compare with a single combined worker/master when
 # running non-distributed with a single node
-node_counts=(1 2 3 5 9 17 33 65)
+node_counts=(1 2 3 5 9 17 33 65 129 257)
 # Use these node counts for the spp scaling
 #node_counts=(1 5 17 65 129)
 queue="normal"
-job_time="00:15:00"
+job_time="01:15:00"
+job_name="loft_office_strong_64spp"
 
 export WORK_DIR=$WORK/ospray/build/rel/
 export OUT_DIR=`pwd`
@@ -16,7 +17,7 @@ export RUN_SCRIPT=$WORK/ospray/loft_office_run.sh
 
 for nodes in ${node_counts[@]}; do
 	set -x
-	sbatch -J loft_office_spp_scale$nodes -o loft_office_spp_scale${nodes}.txt -N $nodes -n $nodes \
+	sbatch -J ${job_name}$nodes -o ${job_name}${nodes}.txt -N $nodes -n $nodes \
 		-p $queue -t $job_time $RUN_SCRIPT
 done
 
