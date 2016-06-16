0c3eb8343a18de7b21272be781c05bc8a16f1467
Will Usher <willusher.life@gmail.com>
2016-06-16 15:50:54 -0500

    Shift run configs to be in environment
    The environment is copied at job launch but since the script is called
    by the VNC session it can't be changed until the job has actually run,
    otherwise a different config will be run than what we wanted.

--- volume_viewer_scaling.sh
+++ volume_viewer_scaling.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-#node_counts=(2 3 5 9 17 33 65 129)
-node_counts=(129)
-brick_counts=(3 5 9 17 33 65 129)
-
+#node_counts=(1 2 3 5 9 17 33 65 129 257)
+node_counts=(65 129)
 queue="normal"
-job_time="00:10:00"
-job_name=rm_brick_increase
+job_time="01:00:00"
+job_name=rm_data_scale_min
 
 # The script will run the volume run script from the specified work dir
 export WORK_DIR=$WORK/ospray/build/rel/
 export OUT_DIR=`pwd`
 export VOLUME_RUN_SCRIPT=$WORK/ospray/rm_run.sh
+#export VOLUME_RUN_SCRIPT=$WORK/ospray/dns_run.sh
 
-#for nodes in ${node_counts[@]}; do
-#	set -x
-#	sbatch -J ${job_name}$nodes -o ${job_name}${nodes}.txt -N $nodes -n $nodes \
-#		-p $queue -t $job_time $WORK/ospray/volume_viewer_bench.vnc -geometry 1600x1600
-#done
+export MIN_BRICK=1
+export DATA_SCALING=1
+export REPLICATED_DATA=0
 
-# Do the brick count increase runs with fixed number of nodes
-for bricks in ${brick_counts[@]}; do
+for nodes in ${node_counts[@]}; do
 	set -x
-  # TODO: Will slurm properly copy this environment variable per-submission? When does it copy
-  # environment variables?
-  export NUM_BRICKS=$bricks
-	sbatch -J ${job_name}$bricks -o ${job_name}${bricks}.txt -N ${node_counts[0]} -n ${node_counts[0]} \
+	sbatch -J ${job_name}$nodes -o ${job_name}${nodes}.txt -N $nodes -n $nodes \
 		-p $queue -t $job_time $WORK/ospray/volume_viewer_bench.vnc -geometry 1600x1600
 done
+
+# Do the brick count increase runs with fixed number of nodes
+#for bricks in ${brick_counts[@]}; do
+#	set -x
+  # TODO: Will slurm properly copy this environment variable per-submission? When does it copy
+  # environment variables?
+#  export RM_NUM_BRICKS=$bricks
+#	sbatch -J ${job_name}$bricks -o ${job_name}${bricks}.txt -N ${node_counts[0]} -n ${node_counts[0]} \
+#		-p $queue -t $job_time $WORK/ospray/volume_viewer_bench.vnc -geometry 1600x1600
+#done
 
