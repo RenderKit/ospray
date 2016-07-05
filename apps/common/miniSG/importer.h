7f49069796049871e6141caa1a0d9f8c554af2a2
Johannes Günther <johannes.guenther@intel.com>
2016-07-05 13:00:31 +0200

    Don't define '<' on vectors, fixing box::extend
    It's prone to bugs, because a definition of an ordering of vectors is a
    bit arbitrary and depends on the context (so, if really needed, define
    it locally). For example, in box::extend we certainly want the
    element-wise min/max and not the std::min/std::max made applicable by
    vec3f::operator<.

--- apps/common/miniSG/importer.h
+++ apps/common/miniSG/importer.h
@@ -32,23 +32,10 @@ namespace ospray {
       Model *model; /*!< current model we're importing a new mesh into */
       Mesh  *mesh;  /*!< current mesh we're importing */
 
-      /*! to tell the ID of any known position in the mesh's list of vertex positions */
-      std::map<vec3f,uint32_t> known_positions;
-      /*! to tell the ID of any known vtx normal in the mesh's list of vertex normals */
-      std::map<vec3f,uint32_t> known_normals;
-      /*! to tell the ID of any known texcoord in the mesh's list of texcoords */
-      std::map<vec2f,uint32_t> known_texcoords;
-      
       ImportHelper(Model &model, const std::string &name = "");
 
-      /*! find given vertex and return its ID, or add if it doesn't yet exist */
+      /*! add given vertex and return its index */
       uint32_t addVertex(const vec3f &position);
-      /*! find given vertex and return its ID, or add if it doesn't yet exist */
-      uint32_t addVertex(const vec3f &position, const vec2f &texcoord);
-      /*! find given vertex and return its ID, or add if it doesn't yet exist */
-      uint32_t addVertex(const vec3f &position, const vec3f &normal, const vec2f &texcoord);
-      /*! find given vertex and return its ID, or add if it doesn't yet exist */
-      uint32_t addVertex(const vec3f &position, const vec3f &normal);
       /*! add new triangle to the mesh. may discard the triangle if it is degenerated. */
       void addTriangle(const miniSG::Triangle &triangle);
 
