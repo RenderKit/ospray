7c997ade462839671273cb45ba137fca12bdc9a7
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2016-10-18 21:30:36 -0500

    add implementation of POSIX-only strncasecmp() function for Windows only

--- apps/qtViewer/sg/importer/ImportOBJ.cpp
+++ apps/qtViewer/sg/importer/ImportOBJ.cpp
@@ -29,6 +29,26 @@
 #include "sg/geometry/TriangleMesh.h"
 #include <cmath>
 
+#ifdef _WIN32
+#include <string.h>
+#include <ctype.h>
+
+int strncasecmp(const char *s1, const char *s2, size_t n)
+{
+  if (n == 0)
+    return 0;
+
+  while (n-- != 0 && tolower(*s1) == tolower(*s2)) {
+    if (n == 0 || *s1 == '\0' || *s2 == '\0')
+      break;
+    s1++;
+    s2++;
+  }
+
+  return tolower(*(unsigned char *) s1) - tolower(*(unsigned char *) s2);
+}
+#endif
+
 namespace ospray {
   namespace sg {
     using std::string;
