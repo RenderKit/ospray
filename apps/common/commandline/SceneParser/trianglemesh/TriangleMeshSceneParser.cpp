ae9a5e4514d54badf4e5e3d4f9a4d55a841216e8
Johannes Günther <johannes.guenther@intel.com>
2017-01-11 08:58:39 +0100

    Enforce ASCII encoding of source files

--- apps/common/commandline/SceneParser/trianglemesh/TriangleMeshSceneParser.cpp
+++ apps/common/commandline/SceneParser/trianglemesh/TriangleMeshSceneParser.cpp
@@ -174,7 +174,7 @@ cpp::Material TriangleMeshSceneParser::createMaterial(cpp::Renderer ren,
   float Tf_prime = 0.f;
 
   // workaround strange behavior of 3DMax exporter (fully transparent scenes)
-  // detect d==Tf==1-Tr, correct to d’=1, Tf’=1-d
+  // detect d==Tf==1-Tr, correct to d'=1, Tf'=1-d
   if (isOBJMaterial) {
     auto itTr = mat->params.find("Tr");
     auto end = mat->params.end();
