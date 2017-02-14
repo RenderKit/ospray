dd139813492ddec3c0e53a364e33e8e624208d20
Will Usher <willusher.life@gmail.com>
2017-02-14 12:47:33 -0700

    Fix GL2 texparam setting

--- apps/ViewerExample/widgets/transferFunction.cpp
+++ apps/ViewerExample/widgets/transferFunction.cpp
@@ -177,8 +177,8 @@ void TransferFunction::render(ospray::async_render_engine &renderEngine){
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, samples, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
     if (prev_binding) {
       glBindTexture(GL_TEXTURE_2D, prev_binding);
