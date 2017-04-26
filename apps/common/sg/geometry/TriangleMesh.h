ac742d8bbea1330aacd7f638a017294e8abaf4c1
CarsonBrownlee <carson.brownlee@intel.com>
2017-04-26 01:46:06 -0500

    merge with devel

--- apps/common/sg/geometry/TriangleMesh.h
+++ apps/common/sg/geometry/TriangleMesh.h
@@ -33,7 +33,7 @@ namespace ospray {
       /*! \brief returns a std::string with the c++ name of this class */
       std::string toString() const override;
 
-      box3f computeBounds() const;
+      box3f computeBounds() const override;
 
       void postCommit(RenderContext &ctx) override;
       void postRender(RenderContext& ctx) override;
