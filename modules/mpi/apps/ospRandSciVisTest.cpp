a2960cdb8d205b7bffcbc6012629322534029eab
Will Usher <will.usher@intel.com>
2017-05-19 15:34:25 -0500

    Kind of a wacky test for multiple boxes, but it works :)

--- modules/mpi/apps/ospRandSciVisTest.cpp
+++ modules/mpi/apps/ospRandSciVisTest.cpp
@@ -282,6 +282,20 @@ namespace ospRandSciVisTest {
     box3f worldBounds(vec3f(0), vec3f(1));
 
     std::vector<box3f> clipBoxes{volume.second};
+    bool setGap = false;
+    if (mpicommon::numGlobalRanks() % 2 == 0) {
+      setGap = mpicommon::globalRank() % 3 == 0;
+    } else  {
+      setGap = mpicommon::globalRank() % 2 == 0;
+    }
+    if (setGap) {
+      const float step = (clipBoxes[0].upper.x - clipBoxes[0].lower.x) / 4.0;
+      const vec3f low = clipBoxes[0].lower;
+      const vec3f hi = clipBoxes[0].upper;
+      clipBoxes[0].upper.x = low.x + step;
+      clipBoxes.push_back(box3f(vec3f(low.x + step * 3, low.y, low.z),
+                                vec3f(low.x + step * 4, hi.y, hi.z)));
+    }
     ospray::cpp::Data clipBoxData(clipBoxes.size() * 2, OSP_FLOAT3,
         clipBoxes.data());
     model.set("clipBoxes", clipBoxData);
