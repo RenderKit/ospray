5fdf91f3f7ea2de3e2c5cc8c0a54faa4625426c3
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-09-06 11:44:05 -0500

    use universal references for for_each Functors, only use one base version

--- components/ospcommon/array3D/for_each.h
+++ components/ospcommon/array3D/for_each.h
@@ -37,31 +37,12 @@ namespace ospcommon {
       return idx.x + size_t(dims.x) * (idx.y + size_t(dims.y) * idx.z);
     }
 
-    /*! a template that calls the given functor (typically a lambda) for
-      every vec3i(ix,iy,iz) with 0<=ix<xize.x, 0<=iy<size.y, and
-      0<=iz<size.z)
-
-      Example:
-
-      array3D::for_each(volume->size(),[&](const vec3i &idx){
-      domeSomeThing(volume,index);
-      });
-    */
-    template <typename Functor>
-    inline void for_each(const vec3i &size, const Functor &functor)
-    {
-      for (int iz = 0; iz < size.z; iz++)
-        for (int iy = 0; iy < size.y; iy++)
-          for (int ix = 0; ix < size.x; ix++)
-            functor(vec3i(ix, iy, iz));
-    }
-
     /*! iterate through all indices in [lower,upper), EXCLUSING the
         'upper' value */
     template <typename Functor>
     inline void for_each(const vec3i &lower,
                          const vec3i &upper,
-                         const Functor &functor)
+                         Functor &&functor)
     {
       for (int iz = lower.z; iz < upper.z; iz++)
         for (int iy = lower.y; iy < upper.y; iy++)
@@ -69,12 +50,28 @@ namespace ospcommon {
             functor(vec3i(ix, iy, iz));
     }
 
+    /*! a template that calls the given functor (typically a lambda) for
+      every vec3i(ix,iy,iz) with 0<=ix<xize.x, 0<=iy<size.y, and
+      0<=iz<size.z)
+
+      Example:
+
+      array3D::for_each(volume->size(),[&](const vec3i &idx){
+        doSomeThing(volume,index);
+      });
+    */
+    template <typename Functor>
+    inline void for_each(const vec3i &size, Functor &&functor)
+    {
+      for_each({0, 0, 0}, size, std::forward<Functor>(functor));
+    }
+
     /*! iterate through all indices in [lower,upper), EXCLUSING the
         'upper' value */
     template <typename Functor>
-    inline void for_each(const box3i &coords, const Functor &functor)
+    inline void for_each(const box3i &coords, Functor &&functor)
     {
-      for_each(coords.lower, coords.upper, functor);
+      for_each(coords.lower, coords.upper, std::forward<Functor>(functor));
     }
 
   }  // ::ospcommon::array3D
