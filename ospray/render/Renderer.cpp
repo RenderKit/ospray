68c5bd7add09cca81a6f1a1e1422f69e90d2058b
atafra <attila.t.afra@intel.com>
2017-11-04 21:32:41 +0200

    PathTracer uses adaptive epsilon if autoEpsilon is enabled

--- ospray/render/Renderer.cpp
+++ ospray/render/Renderer.cpp
@@ -31,6 +31,7 @@ namespace ospray {
 
   void Renderer::commit()
   {
+    autoEpsilon = getParam1i("autoEpsilon", true);
     epsilon = getParam1f("epsilon", 1e-6f);
     spp = getParam1i("spp", 1);
     const int32 maxDepth = getParam1i("maxDepth", 20);
@@ -62,6 +63,7 @@ namespace ospray {
       ispc::Renderer_set(getIE()
           , model ? model->getIE() : nullptr
           , camera ? camera->getIE() : nullptr
+          , autoEpsilon
           , epsilon
           , spp
           , maxDepth
