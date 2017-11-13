164ff2ed922526d65753b8f5b6ba838095fae20a
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2017-11-13 15:07:50 +0100

    Fix crash in MPI mode when creating lights w/o renderer

--- modules/mpi/common/OSPWork.cpp
+++ modules/mpi/common/OSPWork.cpp
@@ -320,6 +320,7 @@ namespace ospray {
       {
         Renderer *renderer = (Renderer*)rendererHandle.lookup();
         Light *light = nullptr;
+        if (renderer)
           light = renderer->createLight(type.c_str());
 
         // No renderer present or the renderer doesn't intercept this
