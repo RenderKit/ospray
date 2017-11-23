6b96ac3ec3bdb64cd57fd9a84b365f4b2085cbfa
Adam Perdeusz <adam.perdeusz@intel.com>
2017-11-23 09:58:05 +0100

    Reduce the number of the test cases, redundant tests removed.

--- tests/sources/ospray_test_volumetric.cpp
+++ tests/sources/ospray_test_volumetric.cpp
@@ -7,8 +7,8 @@ TEST_P(Sierpinski, simple) {
   PerformRenderTest();
 }
 
-INSTANTIATE_TEST_CASE_P(Volumetric, Sierpinski, ::testing::Combine(::testing::Values("scivis"), ::testing::Values(false), ::testing::Values(4, 5, 6, 7, 8, 9, 10)));
-INSTANTIATE_TEST_CASE_P(Isosurfaces, Sierpinski, ::testing::Combine(::testing::Values("scivis", "pathtracer"), ::testing::Values(true), ::testing::Values(4, 5, 6, 7, 8, 9, 10)));
+INSTANTIATE_TEST_CASE_P(Volumetric, Sierpinski, ::testing::Combine(::testing::Values("scivis"), ::testing::Values(false), ::testing::Values(4, 6, 9)));
+INSTANTIATE_TEST_CASE_P(Isosurfaces, Sierpinski, ::testing::Combine(::testing::Values("scivis", "pathtracer"), ::testing::Values(true), ::testing::Values( 5, 7,  9)));
 
 TEST_P(Torus, simple) {
   PerformRenderTest();
