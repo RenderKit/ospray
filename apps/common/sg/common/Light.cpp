6e87621ac7edc60cd7ba24be4524879e70559e16
Carson Brownlee <carson.brownlee@intel.com>
2018-03-06 12:34:22 -0500

    removing valid check for sg light

--- apps/common/sg/common/Light.cpp
+++ apps/common/sg/common/Light.cpp
@@ -69,7 +69,6 @@ namespace ospray {
 
       createChild("intensity", "float", 3.f,
                       NodeFlags::required |
-                      NodeFlags::valid_min_max |
                       NodeFlags::gui_slider).setMinMax(0.f,12.f);
 
       createChild("angularDiameter", "float", 0.53f,
