a371c9f590a8f23195d6b239be3055835fa7c59e
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2018-03-06 13:14:14 -0600

    let the renderer verify/commit on renderFrame() in async rendering

--- apps/exampleViewer/common/util/AsyncRenderEngine.cpp
+++ apps/exampleViewer/common/util/AsyncRenderEngine.cpp
@@ -51,18 +51,6 @@ namespace ospray {
         lastFTime = sg::TimeStamp();
       }
 
-      if (scenegraph->childrenLastModified() > lastRTime || !once) {
-        scenegraph->traverse("verify");
-        scenegraph->traverse("commit");
-
-        if (scenegraphDW) {
-          scenegraphDW->traverse("verify");
-          scenegraphDW->traverse("commit");
-        }
-
-        lastRTime = sg::TimeStamp();
-      }
-
       if (scenegraph->hasChild("animationcontroller"))
         scenegraph->child("animationcontroller").traverse("animate");
 
@@ -70,12 +58,12 @@ namespace ospray {
         pickResult = scenegraph->pick(pickPos.ref());
 
       fps.start();
-      scenegraph->renderFrame(sgFB, OSP_FB_COLOR | OSP_FB_ACCUM, false);
+      scenegraph->renderFrame(sgFB, OSP_FB_COLOR | OSP_FB_ACCUM, true);
 
       if (scenegraphDW) {
         auto dwFB =
             scenegraphDW->child("frameBuffer").nodeAs<sg::FrameBuffer>();
-        scenegraphDW->renderFrame(dwFB, OSP_FB_COLOR | OSP_FB_ACCUM, false);
+        scenegraphDW->renderFrame(dwFB, OSP_FB_COLOR | OSP_FB_ACCUM, true);
       }
 
       once = true;
