a7e513db0d07ca890854b08064b6f1f0df00ac77
Attila Afra <attila.t.afra@intel.com>
2018-04-12 02:20:24 +0300

    minor change to the default CarPaint params

--- ospray/render/pathtracer/materials/CarPaint.cpp
+++ ospray/render/pathtracer/materials/CarPaint.cpp
@@ -43,7 +43,7 @@ namespace ospray {
         MaterialParam1f normal = getMaterialParam1f("normal", 1.f);
 
         MaterialParam1f flakeScale = getMaterialParam1f("flakeScale", 100.f);
-        MaterialParam1f flakeDensity = getMaterialParam1f("flakeDensity", 1.f);
+        MaterialParam1f flakeDensity = getMaterialParam1f("flakeDensity", 0.f);
         MaterialParam1f flakeSpread = getMaterialParam1f("flakeSpread", 0.2f);
         MaterialParam1f flakeJitter = getMaterialParam1f("flakeJitter", 0.75f);
         MaterialParam1f flakeRoughness = getMaterialParam1f("flakeRoughness", 0.3f);
