a878469ce3d71bd65b703ed23917f55d3a73ab07
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2018-08-22 21:46:59 -0500

    WIP: adding volume texture sg node
    [ci skip]

---
+++ apps/common/sg/texture/VolumeTexture.h
@@ -0,0 +1,39 @@
+// ======================================================================== //
+// Copyright 2009-2018 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#pragma once
+
+// sg
+#include "Texture.h"
+
+namespace ospray {
+  namespace sg {
+
+    /*! \brief C++ wrapper for a 2D Texture */
+    struct OSPSG_INTERFACE VolumeTexture : public Texture
+    {
+      /*! constructor */
+      VolumeTexture() = default;
+      virtual ~VolumeTexture() override = default;
+
+      virtual void preCommit(RenderContext &ctx) override;
+
+      /*! \brief returns a std::string with the c++ name of this class */
+      std::string toString() const override;
+    };
+
+  } // ::ospray::sg
+} // ::ospray
