9e84f6ba159603906335f6a2cfebd9ea51555814
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2018-10-18 11:39:19 -0500

    add missing regression test images from build state, don't rebuild for test

--- scripts/tests/run_tests.bat
+++ scripts/tests/run_tests.bat
@@ -20,12 +20,6 @@ setlocal
 echo Running tests
 md failed
 
-cd build
-del CMakeCache.txt
-cmake -G "Visual Studio 15 2017 Win64" -DOSPRAY_ENABLE_TESTING=ON ..
-cmake --build . --config Release --target ospray_test_data -- /m /nologo
-cd ..
-
 set OSP_LIBS=build\Release
 set BASELINE_DIR=build\regression_tests\baseline\
 
