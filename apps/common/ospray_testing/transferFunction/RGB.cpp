96732211d0cded049e0e9b0f3581630feee9dfd0
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2018-12-06 14:43:30 -0600

    compile fix for Windows (RGB is defined as a macro somewhere...)

--- apps/common/ospray_testing/transferFunction/RGB.cpp
+++ apps/common/ospray_testing/transferFunction/RGB.cpp
@@ -24,22 +24,24 @@ using namespace ospcommon;
 namespace ospray {
   namespace testing {
 
-    struct RGB : public TransferFunction
+    // NOTE(jda) - Can't use the name 'RGB' because that is a MACRO defined on
+    //             Windows somewhere...
+    struct RedGreenBlue : public TransferFunction
     {
-      RGB();
-      ~RGB() override = default;
+      RedGreenBlue();
+      ~RedGreenBlue() override = default;
     };
 
     // Inlined definitions ////////////////////////////////////////////////////
 
-    RGB::RGB()
+    RedGreenBlue::RedGreenBlue()
     {
       colors.emplace_back(0, 0, 1);
       colors.emplace_back(0, 1, 0);
       colors.emplace_back(1, 0, 0);
     }
 
-    OSP_REGISTER_TESTING_TRANSFER_FUNCTION(RGB, rgb);
+    OSP_REGISTER_TESTING_TRANSFER_FUNCTION(RedGreenBlue, rgb);
 
   }  // namespace testing
 }  // namespace ospray
