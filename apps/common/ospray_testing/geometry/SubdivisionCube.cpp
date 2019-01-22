ae9a0c047ebeb04e6e56d23e3fc1710accaead48
Greg Johnson <greg.p.johnson@intel.com>
2019-01-22 10:58:41 -0600

    additional tutorial cleanups, all subdiv parameter types now consistent with documentation.

--- apps/common/ospray_testing/geometry/SubdivisionCube.cpp
+++ apps/common/ospray_testing/geometry/SubdivisionCube.cpp
@@ -40,6 +40,7 @@ namespace ospray {
     {
       box3f bounds(-1.f, 1.f);
 
+      // vertices of a cube
       std::vector<vec3f> vertices = {{-1.0f, -1.0f, -1.0f},
                                      {1.0f, -1.0f, -1.0f},
                                      {1.0f, -1.0f, 1.0f},
@@ -49,6 +50,7 @@ namespace ospray {
                                      {1.0f, 1.0f, 1.0f},
                                      {-1.0f, 1.0f, 1.0f}};
 
+      // color per vertex
       std::vector<vec4f> colors = {{0.0f, 0.0f, 0.0f, 1.f},
                                    {1.0f, 0.0f, 0.0f, 1.f},
                                    {1.0f, 0.0f, 1.0f, 1.f},
@@ -58,65 +60,81 @@ namespace ospray {
                                    {1.0f, 1.0f, 1.0f, 1.f},
                                    {0.0f, 1.0f, 1.0f, 1.f}};
 
+      // number of vertex indices per face
+      std::vector<unsigned int> faces = {4, 4, 4, 4, 4, 4};
+
+      // vertex indices for all faces
       std::vector<unsigned int> indices = {
           0, 4, 5, 1, 1, 5, 6, 2, 2, 6, 7, 3,
           0, 3, 7, 4, 4, 7, 6, 5, 0, 1, 2, 3,
       };
 
-      std::vector<unsigned int> faces = {4, 4, 4, 4, 4, 4};
-
-      std::vector<float> vertexCreaseWeights = {2, 2, 2, 2, 2, 2, 2, 2};
-
+      // vertex crease indices and weights
       std::vector<unsigned int> vertexCreaseIndices = {0, 1, 2, 3, 4, 5, 6, 7};
+      std::vector<float> vertexCreaseWeights        = {
+          2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f};
 
+      // edge crease indices and weights
+      std::vector<vec2i> edgeCreaseIndices = {{0, 1},
+                                              {1, 2},
+                                              {2, 3},
+                                              {3, 0},
+                                              {4, 5},
+                                              {5, 6},
+                                              {6, 7},
+                                              {7, 4},
+                                              {0, 4},
+                                              {1, 5},
+                                              {2, 6},
+                                              {3, 7}};
       std::vector<float> edgeCreaseWeights = {
-          2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+          2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f, 2.f};
 
-      std::vector<unsigned int> edgeCreaseIndices = {
-          0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6,
-          6, 7, 7, 4, 0, 4, 1, 5, 2, 6, 3, 7,
-      };
+      // global level of tessellation
+      float level = 5.f;
 
+      // create the OSPRay geometry and set all parameters
       OSPGeometry geometry = ospNewGeometry("subdivision");
 
-      OSPData verticesData = ospNewData(8, OSP_FLOAT3, vertices.data());
+      OSPData verticesData =
+          ospNewData(vertices.size(), OSP_FLOAT3, vertices.data());
       ospSetData(geometry, "vertex", verticesData);
       ospRelease(verticesData);
+
+      OSPData colorsData = ospNewData(colors.size(), OSP_FLOAT4, colors.data());
+      ospSetData(geometry, "color", colorsData);
+      ospRelease(colorsData);
+
+      OSPData facesData = ospNewData(faces.size(), OSP_UINT, faces.data());
+      ospSetData(geometry, "face", facesData);
+      ospRelease(facesData);
 
       OSPData indicesData =
           ospNewData(indices.size(), OSP_UINT, indices.data());
       ospSetData(geometry, "index", indicesData);
       ospRelease(indicesData);
 
-      OSPData facesData = ospNewData(faces.size(), OSP_UINT, faces.data());
-      ospSetData(geometry, "face", facesData);
-      ospRelease(facesData);
-
-      OSPData edgeCreaseIndicesData =
-          ospNewData(12, OSP_UINT2, edgeCreaseIndices.data());
-      ospSetData(geometry, "edgeCrease.index", edgeCreaseIndicesData);
-      ospRelease(edgeCreaseIndicesData);
-
-      OSPData edgeCreaseWeightsData =
-          ospNewData(12, OSP_FLOAT, edgeCreaseWeights.data());
-      ospSetData(geometry, "edgeCrease.weight", edgeCreaseWeightsData);
-      ospRelease(edgeCreaseWeightsData);
-
-      OSPData vertexCreaseIndicesData =
-          ospNewData(8, OSP_UINT, vertexCreaseIndices.data());
+      OSPData vertexCreaseIndicesData = ospNewData(
+          vertexCreaseIndices.size(), OSP_UINT, vertexCreaseIndices.data());
       ospSetData(geometry, "vertexCrease.index", vertexCreaseIndicesData);
       ospRelease(vertexCreaseIndicesData);
 
-      OSPData vertexCreaseWeightsData =
-          ospNewData(8, OSP_FLOAT, vertexCreaseWeights.data());
+      OSPData vertexCreaseWeightsData = ospNewData(
+          vertexCreaseWeights.size(), OSP_FLOAT, vertexCreaseWeights.data());
       ospSetData(geometry, "vertexCrease.weight", vertexCreaseWeightsData);
       ospRelease(vertexCreaseWeightsData);
 
-      OSPData colorsData = ospNewData(8, OSP_FLOAT4, colors.data());
-      ospSetData(geometry, "color", colorsData);
-      ospRelease(colorsData);
+      OSPData edgeCreaseIndicesData = ospNewData(
+          edgeCreaseIndices.size(), OSP_INT2, edgeCreaseIndices.data());
+      ospSetData(geometry, "edgeCrease.index", edgeCreaseIndicesData);
+      ospRelease(edgeCreaseIndicesData);
 
-      ospSet1f(geometry, "level", 5.0f);
+      OSPData edgeCreaseWeightsData = ospNewData(
+          edgeCreaseWeights.size(), OSP_FLOAT, edgeCreaseWeights.data());
+      ospSetData(geometry, "edgeCrease.weight", edgeCreaseWeightsData);
+      ospRelease(edgeCreaseWeightsData);
+
+      ospSet1f(geometry, "level", level);
 
       // create OBJ material and assign to geometry
       OSPMaterial objMaterial = ospNewMaterial2("scivis", "OBJMaterial");
@@ -124,6 +142,7 @@ namespace ospray {
       ospCommit(objMaterial);
 
       ospSetMaterial(geometry, objMaterial);
+      ospRelease(objMaterial);
 
       ospCommit(geometry);
 
