199d1944833e3528e628e63e37e38e0bf2ee1b41
Johannes Günther <johannes.guenther@intel.com>
2019-02-05 11:12:50 +0100

    Remove libmmd dependency

--- components/ospcommon/cmake/msvc.cmake
+++ components/ospcommon/cmake/msvc.cmake
@@ -34,4 +34,26 @@ if (USE_STATIC_RUNTIME)
   )
     string(REPLACE "/MD" "/MT" ${FLAG} ${${FLAG}})
   endforeach()
+else()
+  # remove libmmd dependency
+  if (OSPRAY_COMPILER_ICC)
+    # prevents sin(x),cos(x) -> sincos(x) optimization, which is only present in libmmd
+    foreach(FLAG
+      CMAKE_CXX_FLAGS_DEBUG
+      CMAKE_CXX_FLAGS_RELEASE
+      CMAKE_CXX_FLAGS_RELWITHDEBINFO
+      CMAKE_C_FLAGS_DEBUG
+      CMAKE_C_FLAGS_RELEASE
+      CMAKE_C_FLAGS_RELWITHDEBINFO
+    )
+      string(APPEND ${FLAG} " /Qfast-transcendentals-")
+    endforeach()
+    # use default math lib instead of libmmd[d]
+    string(APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG " /nodefaultlib:libmmdd.lib")
+    string(APPEND CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO " /nodefaultlib:libmmd.lib")
+    string(APPEND CMAKE_EXE_LINKER_FLAGS_RELEASE " /nodefaultlib:libmmd.lib")
+    string(APPEND CMAKE_SHARED_LINKER_FLAGS_DEBUG " /nodefaultlib:libmmdd.lib")
+    string(APPEND CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO " /nodefaultlib:libmmd.lib")
+    string(APPEND CMAKE_SHARED_LINKER_FLAGS_RELEASE " /nodefaultlib:libmmd.lib")
+  endif()
 endif()
