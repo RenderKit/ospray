bf0d962b0cf00d42113763251f2aa835f70cf34d
Will Usher <will@sci.utah.edu>
2019-02-12 17:55:57 -0700

    Add distributed sphere rendering tutorial

--- modules/mpi/tutorials/ospMPIDistributedTutorialSpheres.cpp
+++ modules/mpi/tutorials/ospMPIDistributedTutorialSpheres.cpp
@@ -27,6 +27,9 @@
 
 using namespace ospcommon;
 
+// Generate the rank's local spheres within its assigned grid cell
+OSPGeometry makeLocalSpheres(const int mpiRank, const int mpiWorldSize);
+
 int main(int argc, char **argv)
 {
   int mpiThreadCapability = 0;
@@ -43,6 +46,8 @@ int main(int argc, char **argv)
   int mpiWorldSize = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpiWorldSize);
+
+  std::cout << "OSRay rank " << mpiRank << "/" << mpiWorldSize << "\n";
 
   // load the MPI module, and select the MPI distributed device. Here we
   // do not call ospInit, as we want to explicitly pick the distributed
@@ -67,42 +72,9 @@ int main(int argc, char **argv)
 
   // all ranks specify the same rendering parameters, with the exception of
   // the data to be rendered, which is distributed among the ranks
-  // triangle mesh data
-  float vertex[] = { mpiRank, 0.0f, 3.5f, 0.f,
-                     mpiRank, 1.0f, 3.0f, 0.f,
-                     1.0f * (mpiRank + 1.f), 0.0f, 3.0f, 0.f,
-                     1.0f * (mpiRank + 1.f), 1.0f, 2.5f, 0.f };
-  float color[] =  { 0.0f, 0.0f, (mpiRank + 1.f) / mpiWorldSize, 1.0f,
-                     0.0f, 0.0f, (mpiRank + 1.f) / mpiWorldSize, 1.0f,
-                     0.0f, 0.0f, (mpiRank + 1.f) / mpiWorldSize, 1.0f,
-                     0.0f, 0.0f, (mpiRank + 1.f) / mpiWorldSize, 1.0f };
-  int32_t index[] = { 0, 1, 2,
-                      1, 2, 3 };
-
-  // create and setup model and mesh
-  OSPGeometry mesh = ospNewGeometry("triangles");
-  OSPData data = ospNewData(4, OSP_FLOAT3A, vertex, 0); // OSP_FLOAT3 format is also supported for vertex positions
-  ospCommit(data);
-  ospSetData(mesh, "vertex", data);
-  ospRelease(data); // we are done using this handle
-
-  data = ospNewData(4, OSP_FLOAT4, color, 0);
-  ospCommit(data);
-  ospSetData(mesh, "vertex.color", data);
-  ospRelease(data); // we are done using this handle
-
-  data = ospNewData(2, OSP_INT3, index, 0); // OSP_INT4 format is also supported for triangle indices
-  ospCommit(data);
-  ospSetData(mesh, "index", data);
-  ospRelease(data); // we are done using this handle
-
-  ospCommit(mesh);
-  ospAddGeometry(world, mesh);
-
-  // add in spheres geometry
-  //OSPTestingGeometry spheres = ospTestingNewGeometry("spheres", "pathtracer");
-  //ospAddGeometry(world, spheres);
-  //ospRelease(spheres);
+  OSPGeometry spheres = makeLocalSpheres(mpiRank, mpiWorldSize);
+  ospAddGeometry(world, spheres);
+  ospRelease(spheres);
 
   ospSet1i(world, "id", mpiRank);
   // commit the world model
@@ -112,11 +84,19 @@ int main(int argc, char **argv)
   OSPRenderer renderer = ospNewRenderer("mpi_raycast");
 
   // create and setup an ambient light
-  OSPLight light = ospNewLight3("ambient");
-  ospCommit(light);
-  OSPData lights = ospNewData(1, OSP_LIGHT, &light, 0);
-  ospCommit(lights);
-  ospSetObject(renderer, "lights", lights);
+  std::array<OSPLight, 2> lights = {
+    ospNewLight3("ambient"),
+    ospNewLight3("distant")
+  };
+  ospCommit(lights[0]);
+
+  ospSet3f(lights[1], "direction", -1.f, -1.f, 0.5f);
+  ospCommit(lights[1]);
+
+  OSPData lightData = ospNewData(lights.size(), OSP_LIGHT, lights.data(), 0);
+  ospCommit(lightData);
+  ospSetObject(renderer, "lights", lightData);
+  ospRelease(lightData);
 
   // create a GLFW OSPRay window: this object will create and manage the OSPRay
   // frame buffer and camera directly
@@ -157,3 +137,96 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+bool computeDivisor(int x, int &divisor)
+{
+  int upperBound = std::sqrt(x);
+  for (int i = 2; i <= upperBound; ++i) {
+    if (x % i == 0) {
+      divisor = i;
+      return true;
+    }
+  }
+  return false;
+}
+
+// Compute an X x Y x Z grid to have 'num' grid cells,
+// only gives a nice grid for numbers with even factors since
+// we don't search for factors of the number, we just try dividing by two
+vec3i computeGrid(int num)
+{
+  vec3i grid(1);
+  int axis = 0;
+  int divisor = 0;
+  while (computeDivisor(num, divisor)) {
+    grid[axis] *= divisor;
+    num /= divisor;
+    axis = (axis + 1) % 3;
+  }
+  if (num != 1) {
+    grid[axis] *= num;
+  }
+  return grid;
+}
+
+OSPGeometry makeLocalSpheres(const int mpiRank, const int mpiWorldSize) {
+  struct Sphere
+  {
+    vec3f org;
+  };
+
+  const float sphereRadius = 0.1;
+  std::vector<Sphere> spheres(10);
+
+  // To simulate loading a shared dataset all ranks generate the same
+  // sphere data.
+  std::random_device rd;
+  std::mt19937 rng(rd());
+
+  const vec3i grid = computeGrid(mpiWorldSize);
+  const vec3i brickId(mpiRank % grid.x,
+                      (mpiRank / grid.x) % grid.y,
+                      mpiRank / (grid.x * grid.y));
+
+  // The grid is over the [-1, 1] box
+  const vec3f brickSize = vec3f(2.0) / vec3f(grid);
+  const vec3f brickLower = brickSize * brickId - vec3f(1.f);
+  const vec3f brickUpper = brickSize * brickId - vec3f(1.f) + brickSize;
+
+  // Generate spheres within the box padded by the radius, so we don't need
+  // to worry about ghost bounds
+  std::uniform_real_distribution<float> distX(brickLower.x + sphereRadius,
+      brickUpper.x - sphereRadius);
+  std::uniform_real_distribution<float> distY(brickLower.y + sphereRadius,
+      brickUpper.y - sphereRadius);
+  std::uniform_real_distribution<float> distZ(brickLower.z + sphereRadius,
+      brickUpper.z - sphereRadius);
+
+  for (auto &s : spheres) {
+    s.org.x = distX(rng);
+    s.org.y = distY(rng);
+    s.org.z = distZ(rng);
+  }
+
+  OSPData sphereData = ospNewData(spheres.size() * sizeof(Sphere), OSP_UCHAR,
+                                  spheres.data());
+
+
+  vec3f color(0.f, 0.f, (mpiRank + 1.f) / mpiWorldSize);
+  OSPMaterial material = ospNewMaterial2("scivis", "OBJMaterial");
+  ospSet3fv(material, "Kd", &color.x);
+  ospSet3f(material, "Ks", 1.f, 1.f, 1.f);
+  ospCommit(material);
+
+  OSPGeometry sphereGeom = ospNewGeometry("spheres");
+  ospSet1i(sphereGeom, "bytes_per_sphere", int(sizeof(Sphere)));
+  ospSet1f(sphereGeom, "radius", sphereRadius);
+  ospSetData(sphereGeom, "spheres", sphereData);
+  ospSetMaterial(sphereGeom, material);
+  ospRelease(material);
+  ospRelease(sphereData);
+  ospCommit(sphereGeom);
+
+  return sphereGeom;
+}
+
