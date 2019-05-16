3ac1c42b75f3e10be4d88d1be9621e34b2229088
Alok Hota <alok.hota@intel.com>
2019-05-16 15:29:24 -0500

    add ospTutorialAMRVolume
    only shows a blank window

---
+++ apps/tutorials/ospTutorialAMRVolume.cpp
@@ -0,0 +1,90 @@
+// ======================================================================== //
+// Copyright 2018-2019 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include <iterator>
+#include <memory>
+#include <random>
+#include "GLFWOSPRayWindow.h"
+
+#include "ospcommon/library.h"
+#include "ospray_testing.h"
+
+#include <imgui.h>
+
+using namespace ospcommon;
+
+static const std::string renderer_type = "pathtracer";
+
+int main(int argc, const char **argv)
+{
+  // initialize OSPRay; OSPRay parses (and removes) its commandline parameters,
+  // e.g. "--osp:debug"
+  OSPError initError = ospInit(&argc, argv);
+
+  if (initError != OSP_NO_ERROR)
+    return initError;
+
+  // we must load the testing library explicitly on Windows to look up
+  // object creation functions
+  loadLibrary("ospray_testing");
+
+  // set an error callback to catch any OSPRay errors and exit the application
+  ospDeviceSetErrorFunc(
+      ospGetCurrentDevice(), [](OSPError error, const char *errorDetails) {
+        std::cerr << "OSPRay error: " << errorDetails << std::endl;
+        exit(error);
+      });
+
+  // create the world which will contain all of our geometries
+  OSPWorld world = ospNewWorld();
+
+  // add in AMR volume
+  box3f bounds(-1.f, 1.f);
+
+  // commit the world
+  ospCommit(world);
+
+  // create OSPRay renderer
+  OSPRenderer renderer = ospNewRenderer(renderer_type.c_str());
+
+  OSPData lightsData = ospTestingNewLights("ambient_only");
+  ospSetData(renderer, "lights", lightsData);
+  ospRelease(lightsData);
+
+  ospCommit(renderer);
+
+  // create a GLFW OSPRay window: this object will create and manage the OSPRay
+  // frame buffer and camera directly
+  auto glfwOSPRayWindow =
+      std::unique_ptr<GLFWOSPRayWindow>(new GLFWOSPRayWindow(
+          vec2i{1024, 768}, bounds, world, renderer));
+
+  glfwOSPRayWindow->registerImGuiCallback([&]() {
+    static int spp = 1;
+    if (ImGui::SliderInt("spp", &spp, 1, 64)) {
+      ospSet1i(renderer, "spp", spp);
+      glfwOSPRayWindow->addObjectToCommit(renderer);
+    }
+  });
+
+  // start the GLFW main loop, which will continuously render
+  glfwOSPRayWindow->mainLoop();
+
+  // cleanly shut OSPRay down
+  ospShutdown();
+
+  return 0;
+}
