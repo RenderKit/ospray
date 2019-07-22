f5aaafc668225335cb15e87eb80388d4112fdabb
Alok Hota <alok.hota@intel.com>
2019-07-22 14:45:47 -0500

    basic streamlines tutorial with random helixes

--- apps/ospray_testing/geometry/TestStreamlines.cpp
+++ apps/ospray_testing/geometry/TestStreamlines.cpp
@@ -18,6 +18,10 @@
 // ospcommon
 #include "ospcommon/box.h"
 
+#include <cmath>
+#include <random>
+#include <vector>
+
 using namespace ospcommon;
 
 namespace ospray {
@@ -35,12 +39,59 @@ namespace ospray {
     OSPTestingGeometry TestStreamlines::createGeometry(
         const std::string &renderer_type) const
     {
-      auto slGeom  = ospNewGeometry("streamlines");
+      auto slGeom = ospNewGeometry("streamlines");
+      std::vector<vec4f> points;
+      std::vector<int> indices;
+      std::vector<vec4f> colors;
+
+      std::random_device rd;
+      std::default_random_engine rng(rd());
+      std::uniform_real_distribution<float> radDist(0.5f, 1.5f);
+      std::uniform_real_distribution<float> stepDist(0.0001f, 0.01f);
+      std::uniform_int_distribution<int> dDist(0, 360);
+      std::uniform_real_distribution<float> freqDist(0.5f, 1.5f);
+      std::uniform_real_distribution<float> colorDist(0.1f, 1.f);
+
+      int offset = 0;
+
+      // create multiple lines
+      int numLines = 50;
+      for (int l = 0; l < numLines; l++) {
+        int dStart   = dDist(rng);
+        float radius = radDist(rng);
+        float h = 0, hStep = stepDist(rng);
+        float f = freqDist(rng);
+
+        vec4f c(colorDist(rng), colorDist(rng), colorDist(rng), 1.f);
+
+        for (int d = dStart; d < dStart + 360; d++, h += hStep) {
+          vec4f p;
+          p.x = radius * std::sin(d * M_PI / 180.f);
+          p.y = h;
+          p.z = radius * std::cos(d * M_PI / 180.f);
+          p.w = 0.01f * std::sin(f * (d * M_PI / 180.f)) + 0.01f;
+          points.push_back(p);
+          colors.push_back(c);
+          if (d < 359)
+            indices.push_back(offset + d);
+        }
+        offset += 360;
+      }
+
+      OSPData pointsData  = ospNewData(points.size(), OSP_VEC4F, points.data());
+      OSPData indicesData = ospNewData(indices.size(), OSP_INT, indices.data());
+      OSPData colorsData  = ospNewData(colors.size(), OSP_VEC4F, colors.data());
+      ospSetData(slGeom, "vertex.position", pointsData);
+      ospSetData(slGeom, "index", indicesData);
+      ospSetData(slGeom, "vertex.color", colorsData);
+      ospCommit(slGeom);
+
       auto slModel = ospNewGeometricModel(slGeom);
+      ospCommit(slModel);
 
       OSPGroup group = ospNewGroup();
       auto models    = ospNewData(1, OSP_OBJECT, &slModel);
-      ospSetData(group, "geometries", models);
+      ospSetData(group, "geometry", models);
       ospCommit(group);
       ospRelease(models);
 
@@ -61,6 +112,8 @@ namespace ospray {
 
       return retval;
     }
+
+    OSP_REGISTER_TESTING_GEOMETRY(TestStreamlines, streamlines);
 
   }  // namespace testing
 }  // namespace ospray
---
+++ apps/tutorials/ospTutorialStreamlines.cpp
@@ -0,0 +1,75 @@
+// ======================================================================== //
+// Copyright 2018-2019 Intel Corporation                                    //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+#include "GLFWOSPRayWindow.h"
+
+#include "ospcommon/library.h"
+#include "ospray_testing.h"
+
+#include "tutorial_util.h"
+
+#include <imgui.h>
+
+using namespace ospcommon;
+
+static std::string renderer_type = "scivis";
+
+int main(int argc, const char **argv)
+{
+  initializeOSPRay(argc, argv);
+
+  // create the world
+  OSPWorld world = ospNewWorld();
+
+  std::vector<OSPInstance> instanceHandles;
+
+  // generate streamlies
+  OSPTestingGeometry streamlines =
+      ospTestingNewGeometry("streamlines", renderer_type.c_str());
+  instanceHandles.push_back(streamlines.instance);
+  ospRelease(streamlines.geometry);
+  ospRelease(streamlines.model);
+
+  OSPData geomInstances =
+      ospNewData(instanceHandles.size(), OSP_OBJECT, instanceHandles.data());
+
+  ospSetData(world, "instance", geomInstances);
+  ospRelease(geomInstances);
+
+  ospCommit(world);
+
+  OSPRenderer renderer = ospNewRenderer(renderer_type.c_str());
+
+  OSPData lightsData = ospTestingNewLights("ambient_and_directional");
+  ospSetData(renderer, "light", lightsData);
+  ospRelease(lightsData);
+
+  ospCommit(renderer);
+
+  // create a GLFW OSPRay window: this object will create and manage the OSPRay
+  // frame buffer and camera directly
+  auto glfwOSPRayWindow =
+      std::unique_ptr<GLFWOSPRayWindow>(new GLFWOSPRayWindow(
+          vec2i{1024, 768}, box3f(vec3f(-2.f), vec3f(2.f)), world, renderer));
+
+  // start the GLFW main loop, which will continuously render
+  glfwOSPRayWindow->mainLoop();
+
+  // cleanly shut OSPRay down
+  ospShutdown();
+
+  return 0;
+}
