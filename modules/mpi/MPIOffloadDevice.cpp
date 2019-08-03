e545ffe9bd375b3b8ed4a55eb5cdeb32e6e44642
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-02 18:29:56 -0700

    more MPIOffload cleanups: remove unused mpi launch modes

--- modules/mpi/MPIOffloadDevice.cpp
+++ modules/mpi/MPIOffloadDevice.cpp
@@ -24,12 +24,6 @@
 // maml
 #include "maml/maml.h"
 
-// std
-#ifndef _WIN32
-#include <dlfcn.h>
-#include <unistd.h>  // for fork()
-#endif
-
 #ifdef OPEN_MPI
 #include <sched.h>
 #include <thread>
@@ -126,40 +120,11 @@ namespace ospray {
     // MPI initialization helper functions ////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////
 
-    /*! in this mode ("ospray on ranks" mode, or "ranks" mode), the
-        user has launched the app across all ranks using mpirun "<all
-        rank> <app>"; no new processes need to get launched.
-
-        Based on the 'startworkers' flag, this function can set up ospray in
-        one of two modes:
-
-        in "workers" mode (startworkes=true) all ranks > 0 become
-        workers, and will NOT return to the application; rank 0 is the
-        master that controls those workers but doesn't do any
-        rendeirng (we may at some point allow the master to join in
-        working as well, but currently this is not implemented). to
-        reach that mode we call this function with
-        'startworkers=true', which will make sure that, even though
-        all ranks _called_ mpiinit, only rank 0 will ever return to
-        the app, while all other ranks will automatically go to
-        running worker code, and never ever return from this function.
-
-        b) in "distribtued" mode the app itself is distributed, and
-        will use the ospray distributed api to control ospray in a
-        data-distributed mode. in this way, we'll call this function
-        with startWorkers=false, which will let all ranks return from
-        this function to do further work in the app.
-
-        For this function, we assume:
-
-        - all *all* MPI_COMM_WORLD processes are going into this function
-
-        - this fct is called from ospInit (with ranksBecomeWorkers=true) or
-          from ospdMpiInit (w/ ranksBecomeWorkers = false)
-    */
-    void createMPI_RanksBecomeWorkers(int *ac, const char **av)
+    void createMPI_RanksBecomeWorkers()
     {
-      mpi::init(ac, av, true);
+      int ac           = 2;
+      const char *av[] = {"ospray_mpi_worker", "--osp:mpi"};
+      mpi::init(&ac, av, true);
 
       postStatusMsg(OSPRAY_MPI_VERBOSE_LEVEL)
           << "#o: initMPI::OSPonRanks: " << world.rank << '/' << world.size;
@@ -177,203 +142,10 @@ namespace ospray {
 
         // now, all workers will enter their worker loop (ie, they will *not*
         // return)
-        mpi::runWorker();
+        runWorker();
         throw std::runtime_error("should never reach here!");
         /* no return here - 'runWorker' will never return */
       }
-    }
-
-    /*! in this mode ("separate worker group" mode)
-      - the user may or may not have launched MPI explicitly for his app
-      - the app may or may not be running distributed
-      - the ospray frontend (the part linked to the app) will wait for a remote
-        MPI group of workers to connect to this app
-      - the ospray frontend will store the port its waiting on connections for
-        in the filename passed to this function; or will print it to stdout if
-        this is nullptr
-    */
-    void createMPI_ListenForWorkers(int *ac, const char **av)
-    {
-      mpi::init(ac, av, true);
-
-      if (world.rank < 1) {
-        postStatusMsg(
-            "====================================================\n"
-            "initializing OSPRay MPI in 'listen for workers' mode\n"
-            "====================================================",
-            OSPRAY_MPI_VERBOSE_LEVEL);
-      }
-
-      app.comm = world.comm;
-      app.makeIntraComm();
-
-      char appPortName[MPI_MAX_PORT_NAME];
-
-      if (!IamTheMaster()) {
-        throw std::runtime_error(
-            "--osp:mpi-listen only makes sense with "
-            "a single rank...");
-      }
-
-      MPI_CALL(Open_port(MPI_INFO_NULL, appPortName));
-
-      socket_t mpiPortSocket = ospcommon::bind(3141);
-      socket_t clientSocket  = ospcommon::listen(mpiPortSocket);
-      size_t len             = strlen(appPortName);
-      ospcommon::write(clientSocket, &len, sizeof(len));
-      ospcommon::write(clientSocket, appPortName, len);
-      flush(clientSocket);
-
-      MPI_CALL(
-          Comm_accept(appPortName, MPI_INFO_NULL, 0, app.comm, &worker.comm));
-      ospcommon::close(clientSocket);
-      ospcommon::close(mpiPortSocket);
-
-      worker.makeInterComm();
-
-      mpi::Group mergedComm;
-      MPI_CALL(Intercomm_merge(worker.comm, 0, &mergedComm.comm));
-      mergedComm.makeIntraComm();
-      mpi::world.comm = mergedComm.comm;
-      mpi::world.makeIntraComm();
-
-      if (app.rank == 0) {
-        postStatusMsg(
-            "=====================================================\n"
-            "OSPRAY Worker ring connected\n"
-            "=====================================================",
-            OSPRAY_MPI_VERBOSE_LEVEL);
-      }
-
-      mpi::world.barrier();
-    }
-
-    void createMPI_connectToListener(int *ac,
-                                     const char **av,
-                                     const std::string &host)
-    {
-      mpi::init(ac, av, true);
-
-      if (world.rank < 1) {
-        postStatusMsg(
-            "=====================================================\n"
-            "initializing OSPRay MPI in 'connect to master' mode  \n"
-            "=====================================================",
-            OSPRAY_MPI_VERBOSE_LEVEL);
-      }
-
-      worker.comm = world.comm;
-      worker.makeIntraComm();
-
-      char appPortName[MPI_MAX_PORT_NAME];
-      if (worker.rank == 0) {
-        auto masterSocket = ospcommon::connect(host.c_str(), 3141);
-
-        size_t len;
-        ospcommon::read(masterSocket, &len, sizeof(len));
-        ospcommon::read(masterSocket, appPortName, len);
-        ospcommon::close(masterSocket);
-      }
-
-      MPI_CALL(
-          Comm_connect(appPortName, MPI_INFO_NULL, 0, worker.comm, &app.comm));
-      app.makeInterComm();
-
-      mpi::Group mergedComm;
-      MPI_CALL(Intercomm_merge(app.comm, 1, &mergedComm.comm));
-      mergedComm.makeIntraComm();
-      mpi::world.comm = mergedComm.comm;
-      mpi::world.makeIntraComm();
-
-      mpi::world.barrier();
-
-      postStatusMsg("starting worker...", OSPRAY_MPI_VERBOSE_LEVEL);
-      mpi::runWorker();
-    }
-
-    /*! in this mode ("separate worker group" mode)
-      - the user may or may not have launched MPI explicitly for his app
-      - the app may or may not be running distributed
-      - the ospray frontend (the part linked to the app) will use the specified
-        'launchCmd' to launch a _new_ MPI group of worker processes.
-      - the ospray frontend will assume the launched process to output
-        'OSPRAY_SERVICE_PORT' stdout, will parse that output for this string,
-        and create an mpi connection to this port to establish the service
-    */
-    void createMPI_LaunchWorkerGroup(int *ac,
-                                     const char **av,
-                                     const char *launchCommand)
-    {
-      mpi::init(ac, av, true);
-
-      Assert(launchCommand);
-
-      MPI_CALL(Comm_dup(world.comm, &app.comm));
-      app.makeIntraComm();
-
-      char appPortName[MPI_MAX_PORT_NAME];
-      if (app.rank == 0 || app.size == -1) {
-        postStatusMsg(OSPRAY_MPI_VERBOSE_LEVEL)
-            << "=======================================================\n"
-            << "initializing OSPRay MPI in 'launching new workers' mode"
-            << "=======================================================\n"
-            << "using launch script '" << launchCommand << "'";
-
-        MPI_CALL(Open_port(MPI_INFO_NULL, appPortName));
-
-        // fix port name: replace all '$'s by '%'s to allow using it on the
-        //                cmdline...
-        char *fixedPortName = strdup(appPortName);
-
-        postStatusMsg(OSPRAY_MPI_VERBOSE_LEVEL)
-            << "with port " << fixedPortName
-            << "\n=======================================================";
-
-        for (char *s = fixedPortName; *s; ++s)
-          if (*s == '$')
-            *s = '%';
-
-        char systemCommand[10000];
-        sprintf(systemCommand, "%s %s", launchCommand, fixedPortName);
-#ifdef _WIN32
-        throw std::runtime_error("OSPRay MPI: no fork() yet on Windows");
-#else
-        if (fork()) {
-          auto result = system(systemCommand);
-          (void)result;
-
-          postStatusMsg("OSPRay worker process has died - killing application");
-          exit(0);
-        }
-#endif
-      }
-
-      MPI_CALL(
-          Comm_accept(appPortName, MPI_INFO_NULL, 0, app.comm, &worker.comm));
-      worker.makeInterComm();
-
-      if (app.rank == 0 || app.size == -1) {
-        postStatusMsg(OSPRAY_MPI_VERBOSE_LEVEL)
-            << "OSPRay MPI Worker ring successfully connected.\n"
-            << "found " << worker.size << " workers."
-            << "\n=======================================================";
-      }
-
-      if (app.size > 1) {
-        if (app.rank == 1) {
-          postStatusMsg()
-              << "ospray:WARNING: you're trying to run an mpi-parallel app "
-              << "with ospray\n"
-              << "(only the root node is allowed to issue ospray api "
-              << "calls right now)\n"
-              << "=======================================================";
-        }
-        // Note: MAML has not started yet, so we don't run the collective
-        // through the messaging layer
-        MPI_CALL(Barrier(app.comm));
-      }
-
-      MPI_CALL(Barrier(app.comm));
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -395,38 +167,7 @@ namespace ospray {
 
       initialized = true;
 
-      int _ac           = 2;
-      const char *_av[] = {"ospray_mpi_worker", "--osp:mpi"};
-
-      std::string mode = getParam<std::string>("mpiMode", "mpi");
-
-      if (mode == "mpi") {
-        createMPI_RanksBecomeWorkers(&_ac, _av);
-      } else if (mode == "mpi-launch") {
-        std::string launchCommand = getParam<std::string>("launchCommand", "");
-
-        if (launchCommand.empty()) {
-          throw std::runtime_error(
-              "You must provide the launchCommand "
-              "parameter in mpi-launch mode!");
-        }
-
-        createMPI_LaunchWorkerGroup(&_ac, _av, launchCommand.c_str());
-      } else if (mode == "mpi-listen") {
-        createMPI_ListenForWorkers(&_ac, _av);
-      } else if (mode == "mpi-connect") {
-        std::string portName = getParam<std::string>("portName", "");
-
-        if (portName.empty()) {
-          throw std::runtime_error(
-              "You must provide the port name string "
-              "where the master is listening at!");
-        }
-
-        createMPI_connectToListener(&_ac, _av, portName);
-      } else {
-        throw std::runtime_error("Invalid MPI mode!");
-      }
+      createMPI_RanksBecomeWorkers();
 
       if (mpi::world.size != 1) {
         if (mpi::world.rank < 0) {
