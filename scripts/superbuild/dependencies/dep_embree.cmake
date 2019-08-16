8eb8471fd9df2e669247d6d204381f4a3cf5fc9a
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-16 15:01:26 -0500

    allow source and pre-built binary versions of OIDN and Embree in superbuild
    [ci skip]

--- scripts/superbuild/dependencies/dep_embree.cmake
+++ scripts/superbuild/dependencies/dep_embree.cmake
@@ -14,30 +14,52 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-ExternalProject_Add(embree
-  PREFIX embree
-  DOWNLOAD_DIR embree
-  STAMP_DIR embree/stamp
-  SOURCE_DIR embree/src
-  BINARY_DIR embree/build
-  INSTALL_DIR ${INSTALL_DIR_ABSOLUTE}/embree
-  URL "https://github.com/embree/embree/archive/v${BUILD_EMBREE_VERSION}.zip"
-  CMAKE_ARGS
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-    -DEMBREE_TUTORIALS=OFF
-    -DEMBREE_TBB_ROOT=${TBB_INSTALL_LOCATION}
-    -DEMBREE_ISPC_EXECUTABLE=${ISPC_PATH}
-    -DCMAKE_BUILD_TYPE=Release
-    -DBUILD_TESTING=OFF
-  BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
-  BUILD_ALWAYS OFF
-)
-
 set(EMBREE_PATH "${INSTALL_DIR_ABSOLUTE}/embree")
-if (NOT WIN32)
-  set(EMBREE_PATH "${EMBREE_PATH}/${CMAKE_INSTALL_LIBDIR}/cmake/embree-${BUILD_EMBREE_VERSION}")
-endif()
 
-ExternalProject_Add_StepDependencies(embree configure ispc tbb)
+if (BUILD_EMBREE_FROM_SOURCE)
+  ExternalProject_Add(embree
+    PREFIX embree
+    DOWNLOAD_DIR embree
+    STAMP_DIR embree/stamp
+    SOURCE_DIR embree/src
+    BINARY_DIR embree/build
+    INSTALL_DIR ${INSTALL_DIR_ABSOLUTE}/embree
+    URL "https://github.com/embree/embree/archive/v${BUILD_EMBREE_VERSION}.zip"
+    CMAKE_ARGS
+      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+      -DEMBREE_TUTORIALS=OFF
+      -DEMBREE_TBB_ROOT=${TBB_INSTALL_LOCATION}
+      -DEMBREE_ISPC_EXECUTABLE=${ISPC_PATH}
+      -DCMAKE_BUILD_TYPE=Release
+      -DBUILD_TESTING=OFF
+    BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
+    BUILD_ALWAYS OFF
+  )
+
+  if (NOT WIN32)
+    set(EMBREE_PATH "${EMBREE_PATH}/${CMAKE_INSTALL_LIBDIR}/cmake/embree-${BUILD_EMBREE_VERSION}")
+  endif()
+
+  ExternalProject_Add_StepDependencies(embree configure ispc tbb)
+else()
+  if (APPLE)
+    set(EMBREE_URL "https://github.com/embree/embree/releases/download/v${BUILD_EMBREE_VERSION}/embree-${BUILD_EMBREE_VERSION}.x86_64.macosx.tar.gz")
+  elseif (WIN32)
+    set(EMBREE_URL "https://github.com/embree/embree/releases/download/v${BUILD_EMBREE_VERSION}/embree-${BUILD_EMBREE_VERSION}.x64.vc14.windows.zip")
+  else()
+    set(EMBREE_URL "https://github.com/embree/embree/releases/download/v${BUILD_EMBREE_VERSION}/embree-${BUILD_EMBREE_VERSION}.x86_64.linux.tar.gz")
+  endif()
+
+  ExternalProject_Add(embree
+    PREFIX embree
+    DOWNLOAD_DIR embree
+    SOURCE_DIR ${INSTALL_DIR_ABSOLUTE}/embree
+    URL ${EMBREE_URL}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_ALWAYS OFF
+  )
+endif()
--- scripts/superbuild/dependencies/dep_oidn.cmake
+++ scripts/superbuild/dependencies/dep_oidn.cmake
@@ -14,29 +14,56 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
-ExternalProject_Add(oidn
-  PREFIX oidn
-  DOWNLOAD_DIR oidn
-  STAMP_DIR oidn/stamp
-  SOURCE_DIR oidn/src
-  BINARY_DIR oidn/build
-  INSTALL_DIR ${INSTALL_DIR_ABSOLUTE}/OpenImageDenoise
-  GIT_REPOSITORY https://github.com/OpenImageDenoise/oidn
-  GIT_TAG v${BUILD_OIDN_VERSION}
-  GIT_SHALLOW ON
-  CMAKE_ARGS
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-    -DCMAKE_BUILD_TYPE=Release
-    -DTBB_ROOT=${TBB_ROOT}
-  BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
-  BUILD_ALWAYS OFF
-)
-
 set(OIDN_PATH "${INSTALL_DIR_ABSOLUTE}/OpenImageDenoise")
-if (NOT WIN32)
-  set(OIDN_PATH "${OIDN_PATH}/${CMAKE_INSTALL_LIBDIR}/cmake/OpenImageDenoise")
+
+if (BUILD_OIDN_FROM_SOURCE)
+  ExternalProject_Add(oidn
+    PREFIX oidn
+    DOWNLOAD_DIR oidn
+    STAMP_DIR oidn/stamp
+    SOURCE_DIR oidn/src
+    BINARY_DIR oidn/build
+    INSTALL_DIR ${INSTALL_DIR_ABSOLUTE}/OpenImageDenoise
+    GIT_REPOSITORY https://github.com/OpenImageDenoise/oidn
+    GIT_TAG v${BUILD_OIDN_VERSION}
+    GIT_SHALLOW ON
+    CMAKE_ARGS
+      -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+      -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+      -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+      -DCMAKE_BUILD_TYPE=Release
+      -DTBB_ROOT=${TBB_ROOT}
+    BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
+    BUILD_ALWAYS OFF
+  )
+
+  if (NOT WIN32)
+    set(OIDN_PATH "${OIDN_PATH}/${CMAKE_INSTALL_LIBDIR}/cmake/OpenImageDenoise")
+  endif()
+
+  ExternalProject_Add_StepDependencies(oidn configure tbb ispc)
+else()
+  if (APPLE)
+    set(OIDN_URL "https://github.com/OpenImageDenoise/oidn/releases/download/v${BUILD_OIDN_VERSION}/oidn-${BUILD_OIDN_VERSION}.x86_64.macosx.tar.gz")
+  elseif (WIN32)
+    set(OIDN_URL "https://github.com/OpenImageDenoise/oidn/releases/download/v${BUILD_OIDN_VERSION}/oidn-${BUILD_OIDN_VERSION}.x64.vc14.windows.zip")
+  else()
+    set(OIDN_URL "https://github.com/OpenImageDenoise/oidn/releases/download/v${BUILD_OIDN_VERSION}/oidn-${BUILD_OIDN_VERSION}.x86_64.linux.tar.gz")
+  endif()
+
+  ExternalProject_Add(oidn
+    PREFIX oidn
+    DOWNLOAD_DIR oidn
+    SOURCE_DIR ${INSTALL_DIR_ABSOLUTE}/OpenImageDenoise
+    URL ${OIDN_URL}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_ALWAYS OFF
+  )
+
+  if (NOT WIN32)
+    set(OIDN_PATH "${OIDN_PATH}/lib/cmake/OpenImageDenoise")
+  endif()
 endif()
 
-ExternalProject_Add_StepDependencies(oidn configure tbb ispc)
