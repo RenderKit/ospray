bd684e150c5241692860158500569ce8f32d65c8
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-19 15:17:18 -0500

    build Embree/OIDN from source by default

--- scripts/build_gitlab/osx.sh
+++ scripts/build_gitlab/osx.sh
@@ -19,7 +19,6 @@ mkdir build
 cd build
 rm -rf *
 
-# NOTE(jda) - using Internal tasking system here temporarily to avoid installing TBB
 cmake \
 -D OSPRAY_ENABLE_TESTING=ON \
 -D OSPRAY_AUTO_DOWNLOAD_TEST_IMAGES=OFF \
