86a6028270024e55a2c0137ce84c4a5e20a9f9e0
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-27 21:04:49 -0500

    move ospTutorialCylinders geometry to ospray_testing, minor cleanups

--- apps/tutorials/ospTutorialCylinders.cpp
+++ apps/tutorials/ospTutorialCylinders.cpp
@@ -14,12 +14,8 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#include <iterator>
-#include <memory>
-#include <random>
 #include "GLFWOSPRayWindow.h"
 
-#include "ospcommon/library.h"
 #include "ospray_testing.h"
 
 #include "tutorial_util.h"
@@ -44,114 +40,13 @@ int main(int argc, const char **argv)
   OSPWorld world = ospNewWorld();
   std::vector<OSPInstance> instanceHandles;
 
-  // Create Cylinder struct
-  struct Cylinder
-  {
-    vec3f startVertex;
-    vec3f endVertex;
-    float radius;
-  };
+  OSPTestingGeometry cylinders =
+      ospTestingNewGeometry("cylinders", renderer_type.c_str());
+  instanceHandles.push_back(cylinders.instance);
+  ospRelease(cylinders.geometry);
+  ospRelease(cylinders.model);
 
-  // create random number distributions for cylinder start, end, radius, color
-  std::random_device rd;
-  std::mt19937 gen(rd());
-
-  std::uniform_real_distribution<float> startDelta(-0.015625f, 0.015625f);
-  std::uniform_real_distribution<float> endDelta(-0.125f, 0.125f);
-  std::uniform_real_distribution<float> radiusDistribution(0.001f, 0.002f);
-  std::uniform_real_distribution<float> colorDelta(-0.1f, 0.1f);
-
-  // Set up our patches of grass
-  vec2i numPatches = vec2i{9, 9};
-  int numCylindersPerPatch = 32;
-  int numCylinders = numPatches.x * numPatches.y * numCylindersPerPatch;
-
-  // populate the cylinders
-  std::vector<Cylinder> cylinders(numCylinders);
-  std::vector<vec4f> colors(numCylinders);
-
-  for (int pz = 0; pz < numPatches.y; pz++) {
-    for (int px = 0; px < numPatches.x; px++) {
-      for (int ps = 0; ps < numCylindersPerPatch; ps++) {
-        Cylinder& s 
-            = cylinders.at((pz*numPatches.x + px)*numCylindersPerPatch + ps);
-        s.startVertex.x = 
-            (4.0f/numPatches.x)*(px+0.5f) - 2.0f + startDelta(gen);
-        s.startVertex.y = -1.0f;
-        s.startVertex.z = 
-            (4.0f/numPatches.y)*(pz+0.5f) - 2.0f + startDelta(gen);
-
-        s.endVertex.x = 
-            (4.0f/numPatches.x)*(px+0.5f) - 2.0f + endDelta(gen);
-        s.endVertex.y = -0.5f + endDelta(gen);
-        s.endVertex.z = 
-            (4.0f/numPatches.y)*(pz+0.5f) - 2.0f + endDelta(gen);
-
-        s.radius = radiusDistribution(gen);
-      } 
-    }
-  }
-  
-  // Make all cylinders a greenish hue, with some luma variance
-  for (auto &c : colors) {
-    c.x = 0.2f + colorDelta(gen);
-    c.y = 0.6f + colorDelta(gen);
-    c.z = 0.1f + colorDelta(gen);
-    c.w = 1.0f;
-  }
-
-  // create a data object with all the cylinder information
-  OSPData cylindersData = 
-      ospNewData(numCylinders * sizeof(Cylinder), OSP_UCHAR, cylinders.data());
-
-  // create the cylinder geometry, and assign attributes
-  OSPGeometry cylindersGeometry = ospNewGeometry("cylinders");
-
-  ospSetData(cylindersGeometry, "cylinder", cylindersData);
-  ospSetInt(cylindersGeometry, "bytes_per_cylinder", int(sizeof(Cylinder)));
-  ospSetInt(
-      cylindersGeometry, "offset_v0", int(offsetof(Cylinder, startVertex)));
-  ospSetInt(
-      cylindersGeometry, "offset_v1", int(offsetof(Cylinder, endVertex)));
-  ospSetInt(
-      cylindersGeometry, "offset_radius", int(offsetof(Cylinder, radius)));
-
-  // commit the cylinders geometry
-  ospCommit(cylindersGeometry);
-
-  // Add the color data to the cylinders
-  OSPGeometricModel cylindersModel = ospNewGeometricModel(cylindersGeometry);
-  OSPData cylindersColor = ospNewData(numCylinders, OSP_VEC4F, colors.data());
-  ospSetData(cylindersModel, "prim.color", cylindersColor);
-
-  // create obj material and assign to cylinders model
-  OSPMaterial objMaterial =
-    ospNewMaterial(renderer_type.c_str(), "OBJMaterial");
-  //ospSetVec3f(objMaterial, "Ks", 0.8f, 0.8f, 0.8f);
-  ospCommit(objMaterial);
-
-  ospSetObject(cylindersModel, "material", objMaterial);
-
-  // release handles we no longer need
-  ospRelease(cylindersData);
-  ospRelease(cylindersColor);
-  ospRelease(objMaterial);
-
-  ospCommit(cylindersModel);
-
-  OSPGroup group = ospNewGroup();
-  auto models    = ospNewData(1, OSP_OBJECT, &cylindersModel);
-  ospSetData(group, "geometry", models);
-  ospCommit(group);
-  ospRelease(models);
-
-  // Codify cylinders into an instance
-  OSPInstance cylindersInstance = ospNewInstance(group);
-  ospCommit(cylindersInstance);
-
-  instanceHandles.push_back(cylindersInstance);
-  ospRelease(cylindersGeometry);
-  ospRelease(cylindersModel);
+  instanceHandles.push_back(cylinders.instance);
 
   // add in a ground plane geometry
   OSPInstance planeInstance = createGroundPlane(renderer_type, 2.0f);
@@ -183,7 +78,7 @@ int main(int argc, const char **argv)
   // frame buffer and camera directly
   auto glfwOSPRayWindow =
       std::unique_ptr<GLFWOSPRayWindow>(new GLFWOSPRayWindow(
-          vec2i{1024, 768}, box3f(vec3f(-1.f), vec3f(1.f)), world, renderer));
+          vec2i{1024, 768}, (box3f &)cylinders.bounds, world, renderer));
 
   // start the GLFW main loop, which will continuously render
   glfwOSPRayWindow->mainLoop();
