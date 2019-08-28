af289a1b3a86686d90335865ab324361d6745357
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-28 14:49:20 -0500

    remove glfw source from the repo, add glfw to superbuild

--- scripts/superbuild/dependencies/dep_glfw.cmake
+++ scripts/superbuild/dependencies/dep_glfw.cmake
@@ -14,15 +14,36 @@
 ## limitations under the License.                                           ##
 ## ======================================================================== ##
 
+set(COMPONENT_NAME glfw)
+
+set(COMPONENT_PATH ${INSTALL_DIR_ABSOLUTE})
+if (INSTALL_IN_SEPARATE_DIRECTORIES)
+  set(COMPONENT_PATH ${INSTALL_DIR_ABSOLUTE}/${COMPONENT_NAME})
+endif()
+
+ExternalProject_Add(${COMPONENT_NAME}
+  PREFIX ${COMPONENT_NAME}
+  DOWNLOAD_DIR ${COMPONENT_NAME}
+  STAMP_DIR ${COMPONENT_NAME}/stamp
+  SOURCE_DIR ${COMPONENT_NAME}/src
+  BINARY_DIR ${COMPONENT_NAME}/build
+  URL "https://github.com/glfw/glfw/archive/3.2.1.zip"
+  CMAKE_ARGS
+    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+    -DCMAKE_INSTALL_PREFIX:PATH=${COMPONENT_PATH}
+    -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
+    -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
+    -DCMAKE_INSTALL_DOCDIR=${CMAKE_INSTALL_DOCDIR}
+    -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
+    -DCMAKE_BUILD_TYPE=Release
+    -DGLFW_BUILD_DOCS=OFF
+    -DGLFW_BUILD_EXAMPLES=OFF
+    -DGLFW_BUILD_TESTS=OFF
+  BUILD_COMMAND ${DEFAULT_BUILD_COMMAND}
+  BUILD_ALWAYS OFF
+)
+
+set(GLFW_PATH "${COMPONENT_PATH}")
 if (NOT WIN32)
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+  set(GLFW_PATH "${GLFW_PATH}/${CMAKE_INSTALL_LIBDIR}/cmake/glfw3")
 endif()
-
-ospray_disable_compiler_warnings()
-
-if (OSPRAY_ENABLE_TESTING)
-  include(cmake/configure_gtest.cmake)
-endif()
-
-add_subdirectory(imgui)
-add_subdirectory(stb_image)
