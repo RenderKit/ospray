fdadb329a424009d8ab7cd19e2035a4e17027f03
Florian Reibold <florian.reibold@intel.com>
2019-09-03 18:25:52 +0200

    add perlin noise class in tutorial

--- apps/tutorials/ospTutorialVolumePathTracer.cpp
+++ apps/tutorials/ospTutorialVolumePathTracer.cpp
@@ -33,6 +33,188 @@ using namespace ospcommon;
 
 static std::string renderer_type = "pathtracer";
 
+class PerlinNoise
+{
+  static constexpr unsigned int tableSize = 12;
+  vec3f gradients[tableSize]; 
+  unsigned int permutationTable[tableSize * 2];
+  
+  inline unsigned int hash(const int x, const int y, const int z) const
+  {
+    return permutationTable[permutationTable[permutationTable[x] + y] + z];
+  }
+
+  static inline float fade(const float t)
+  {
+    return t * t * t * (t * (t * 6 - 15) + 10);
+  }
+
+  static inline float fadeDeriv(const float t)
+  {
+    return 30 * t * t * (t * (t - 2) + 1);
+  }
+
+  static float gradientDotV(uint8_t perm, float x, float y, float z)
+  {
+    switch (perm & 15) 
+    {
+      case 0:  return +x + y;
+      case 1:  return -x + y; 
+      case 2:  return +x - y;
+      case 3:  return -x - y;
+      case 4:  return +x + z;
+      case 5:  return -x + z;
+      case 6:  return +x - z;
+      case 7:  return -x - z;
+      case 8:  return +y + z;
+      case 9:  return -y + z;
+      case 10: return +y - z;
+      case 11: return -y - z;
+      case 12: return +y + x;
+      case 13: return -x + y;
+      case 14: return -y + z;
+      case 15: return -y - z;
+    }
+    return 0;
+  }
+
+ public:
+  PerlinNoise(const unsigned seed = 19937)
+  {
+    std::mt19937 generator(seed);
+    std::uniform_real_distribution<float> distribution;
+    auto randf = std::bind(distribution, generator);
+
+    for (unsigned i = 0; i < tableSize; ++i) 
+    {
+      float theta = acos(2 * randf() - 1);
+      float phi = 2 * randf() * M_PI;
+      float x = cos(phi) * sin(theta);
+      float y = sin(phi) * sin(theta);
+      float z = cos(theta);
+      gradients[i] = vec3f(x, y, z);
+      permutationTable[i] = i;
+    }
+
+    std::uniform_int_distribution<unsigned> distributionInt;
+    auto randi = std::bind(distributionInt, generator);
+
+    // create permutation table
+    for (unsigned i = 0; i < tableSize; ++i)
+      std::swap(permutationTable[i], permutationTable[randi() % tableSize]);
+
+    // extend the permutation table in the index range [256:512]
+    for (unsigned i = 0; i < tableSize; ++i) {
+      permutationTable[tableSize + i] = permutationTable[i];
+    }
+  }
+
+  vec3f deriv(vec3f p) const
+  {
+    p.x = fmodf(p.x * tableSize, tableSize);
+    p.y = fmodf(p.y * tableSize, tableSize);
+    p.z = fmodf(p.z * tableSize, tableSize);
+
+    int xi0 = ((int)std::floor(p.x)) % tableSize;
+    int yi0 = ((int)std::floor(p.y)) % tableSize;
+    int zi0 = ((int)std::floor(p.z)) % tableSize;
+
+    int xi1 = (xi0 + 1) % tableSize;
+    int yi1 = (yi0 + 1) % tableSize;
+    int zi1 = (zi0 + 1) % tableSize;
+
+    float tx = p.x - ((int)std::floor(p.x));
+    float ty = p.y - ((int)std::floor(p.y));
+    float tz = p.z - ((int)std::floor(p.z));
+
+    float u = fade(tx);
+    float v = fade(ty);
+    float w = fade(tz);
+
+    // generate vectors going from the grid points to p
+    float x0 = tx, x1 = tx - 1;
+    float y0 = ty, y1 = ty - 1;
+    float z0 = tz, z1 = tz - 1;
+
+    float a = gradientDotV(hash(xi0, yi0, zi0), x0, y0, z0);
+    float b = gradientDotV(hash(xi1, yi0, zi0), x1, y0, z0);
+    float c = gradientDotV(hash(xi0, yi1, zi0), x0, y1, z0);
+    float d = gradientDotV(hash(xi1, yi1, zi0), x1, y1, z0);
+    float e = gradientDotV(hash(xi0, yi0, zi1), x0, y0, z1);
+    float f = gradientDotV(hash(xi1, yi0, zi1), x1, y0, z1);
+    float g = gradientDotV(hash(xi0, yi1, zi1), x0, y1, z1);
+    float h = gradientDotV(hash(xi1, yi1, zi1), x1, y1, z1);
+
+    float du = fadeDeriv(tx);
+    float dv = fadeDeriv(ty);
+    float dw = fadeDeriv(tz);
+
+    float k1 = (b - a);
+    float k2 = (c - a);
+    float k3 = (e - a);
+    float k4 = (a + d - b - c);
+    float k5 = (a + f - b - e);
+    float k6 = (a + g - c - e);
+    float k7 = (b + c + e + h - a - d - f - g);
+
+    vec3f derivs;
+    derivs.x = du * (k1 + k4 * v + k5 * w + k7 * v * w);
+    derivs.y = dv * (k2 + k4 * u + k6 * w + k7 * v * w);
+    derivs.z = dw * (k3 + k5 * u + k6 * v + k7 * v * w);
+
+    return derivs;
+  } 
+  
+  float noise(vec3f p) const
+  {
+    p.x = fmodf(p.x * tableSize, tableSize);
+    p.y = fmodf(p.y * tableSize, tableSize);
+    p.z = fmodf(p.z * tableSize, tableSize);
+
+    int xi0 = ((int)std::floor(p.x)) % tableSize;
+    int yi0 = ((int)std::floor(p.y)) % tableSize;
+    int zi0 = ((int)std::floor(p.z)) % tableSize;
+
+    int xi1 = (xi0 + 1) % tableSize;
+    int yi1 = (yi0 + 1) % tableSize;
+    int zi1 = (zi0 + 1) % tableSize;
+
+    float tx = p.x - ((int)std::floor(p.x));
+    float ty = p.y - ((int)std::floor(p.y));
+    float tz = p.z - ((int)std::floor(p.z));
+
+    float u = fade(tx);
+    float v = fade(ty);
+    float w = fade(tz);
+
+    // generate vectors going from the grid points to p
+    float x0 = tx, x1 = tx - 1;
+    float y0 = ty, y1 = ty - 1;
+    float z0 = tz, z1 = tz - 1;
+
+    float a = gradientDotV(hash(xi0, yi0, zi0), x0, y0, z0);
+    float b = gradientDotV(hash(xi1, yi0, zi0), x1, y0, z0);
+    float c = gradientDotV(hash(xi0, yi1, zi0), x0, y1, z0);
+    float d = gradientDotV(hash(xi1, yi1, zi0), x1, y1, z0);
+    float e = gradientDotV(hash(xi0, yi0, zi1), x0, y0, z1);
+    float f = gradientDotV(hash(xi1, yi0, zi1), x1, y0, z1);
+    float g = gradientDotV(hash(xi0, yi1, zi1), x0, y1, z1);
+    float h = gradientDotV(hash(xi1, yi1, zi1), x1, y1, z1);
+
+    float k0 = a;
+    float k1 = (b - a);
+    float k2 = (c - a);
+    float k3 = (e - a);
+    float k4 = (a + d - b - c);
+    float k5 = (a + f - b - e);
+    float k6 = (a + g - c - e);
+    float k7 = (b + c + e + h - a - d - f - g);
+
+    return k0 + k1 * u + k2 * v + k3 * w + k4 * u * v + k5 * u * w +
+           k6 * v * w + k7 * u * v * w; 
+  } 
+};
+
 OSPGeometry PlaneGeometry(const vec4f& color, const AffineSpace3f& M)
 {
   const vec3f normal = xfmNormal(M, vec3f(0.f, 1.f, 0.f));
