cc7a255e07452237b80e50594e5e3758978ea417
Florian Reibold <florian.reibold@intel.com>
2019-09-04 16:36:20 +0200

    cleanup of perlin noise. use perlin's own implementation as reference

--- apps/tutorials/ospTutorialVolumePathTracer.cpp
+++ apps/tutorials/ospTutorialVolumePathTracer.cpp
@@ -33,187 +33,79 @@ using namespace ospcommon;
 
 static std::string renderer_type = "pathtracer";
 
-class PerlinNoise
+/* heavily based on Perlin's Java reference implementation of
+ * the improved perlin noise paper from Siggraph 2002 from here
+ * https://mrl.nyu.edu/~perlin/noise/
+**/
+class PerlinNoise 
 {
-  static constexpr unsigned int tableSize = 12;
-  vec3f gradients[tableSize]; 
-  unsigned int permutationTable[tableSize * 2];
-  
-  inline unsigned int hash(const int x, const int y, const int z) const
+  struct PerlinNoiseData
   {
-    return permutationTable[permutationTable[permutationTable[x] + y] + z];
-  }
-
-  static inline float fade(const float t)
-  {
-    return t * t * t * (t * (t * 6 - 15) + 10);
-  }
-
-  static inline float fadeDeriv(const float t)
-  {
-    return 30 * t * t * (t * (t - 2) + 1);
-  }
-
-  static float gradientDotV(uint8_t perm, float x, float y, float z)
-  {
-    switch (perm & 15) 
+    PerlinNoiseData()
     {
-      case 0:  return +x + y;
-      case 1:  return -x + y; 
-      case 2:  return +x - y;
-      case 3:  return -x - y;
-      case 4:  return +x + z;
-      case 5:  return -x + z;
-      case 6:  return +x - z;
-      case 7:  return -x - z;
-      case 8:  return +y + z;
-      case 9:  return -y + z;
-      case 10: return +y - z;
-      case 11: return -y - z;
-      case 12: return +y + x;
-      case 13: return -x + y;
-      case 14: return -y + z;
-      case 15: return -y - z;
+      const int permutation[256] = { 
+        151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,
+        37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,
+        57,177,33,88,237,149,56,87,174,20,125,136,171,168,68,175,74,165,71,134,139,48,27,
+        166,77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245, 40,244,
+        102,143,54,65,25,63,161,1,216,80,73,209,76,132,187,208,89,18,169,200,196,135,130,
+        116,188,159,86,164,100,109,198,173,186,3,64,52,217,226,250,124,123,5,202, 38,147,
+        118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189, 28, 42,223,183,170,213,
+        119,248,152,2,44,154,163,70,221,153,101,155,167,43,172,9,129,22,39,253,19,98,108,
+        110,79,113,224,232,178,185,112,104,218,246,97,228,251, 34,242,193,238,210,144,12,
+        191,179,162,241,81,51,145,235,249,14,239,107, 49,192,214, 31,181,199,106,157,184,
+        84,204,176,115,121,50,45,127,4,150,254,138,236,205,93,222,114,67, 29, 24, 72,243,
+        141,128,195,78,66,215,61,156,180
+      };
+      for (int i=0; i < 256 ; i++) p[256+i] = p[i] = permutation[i];
     }
-    return 0;
+    inline int operator[](size_t idx) const { return p[idx]; }
+    int p[512];
+  };
+
+  static PerlinNoiseData p;
+  static inline float smooth(float t) { return t * t * t * (t * (t * 6.f - 15.f) + 10.f); }
+  static inline float lerp  (float t, float a, float b) { return a + t * (b - a); }
+  static inline float grad(int hash, float x, float y, float z) {
+    const int h   = hash & 15;      
+    const float u = h < 8 ? x : y;
+    const float v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
   }
 
- public:
-  PerlinNoise(const unsigned seed = 19937)
+public:
+  static float noise(vec3f q, float frequency = 8.f)
   {
-    std::mt19937 generator(seed);
-    std::uniform_real_distribution<float> distribution;
-    auto randf = std::bind(distribution, generator);
+    float x = q.x * frequency;
+    float y = q.y * frequency;
+    float z = q.z * frequency;
+    const int X = (int)floor(x) & 255;
+    const int Y = (int)floor(y) & 255; 
+    const int Z = (int)floor(z) & 255;
+    x -= floor(x);
+    y -= floor(y);
+    z -= floor(z);
+    const float u = smooth(x);
+    const float v = smooth(y);
+    const float w = smooth(z);
+    const int  A = p[X] + Y;
+    const int  B = p[X + 1] + Y;
+    const int AA = p[A] + Z;
+    const int BA = p[B] + Z;
+    const int BB = p[B + 1] + Z; 
+    const int AB = p[A + 1] + Z;
 
-    for (unsigned i = 0; i < tableSize; ++i) 
-    {
-      float theta = acos(2 * randf() - 1);
-      float phi = 2 * randf() * M_PI;
-      float x = cos(phi) * sin(theta);
-      float y = sin(phi) * sin(theta);
-      float z = cos(theta);
-      gradients[i] = vec3f(x, y, z);
-      permutationTable[i] = i;
-    }
-
-    std::uniform_int_distribution<unsigned> distributionInt;
-    auto randi = std::bind(distributionInt, generator);
-
-    // create permutation table
-    for (unsigned i = 0; i < tableSize; ++i)
-      std::swap(permutationTable[i], permutationTable[randi() % tableSize]);
-
-    // extend the permutation table in the index range [256:512]
-    for (unsigned i = 0; i < tableSize; ++i) {
-      permutationTable[tableSize + i] = permutationTable[i];
-    }
+    return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), 
+                                   grad(p[BA], x - 1, y, z)),
+                           lerp(u, grad(p[AB], x, y - 1, z),  
+                                   grad(p[BB], x - 1, y - 1, z))),
+                   lerp(v, lerp(u, grad(p[AA + 1], x, y, z - 1),  
+                                   grad(p[BA + 1], x - 1, y, z - 1)),
+                           lerp(u, grad(p[AB + 1], x, y - 1, z - 1),
+                                   grad(p[BB + 1], x - 1, y - 1, z - 1))));
   }
-
-  vec3f deriv(vec3f p) const
-  {
-    p.x = fmodf(p.x * tableSize, tableSize);
-    p.y = fmodf(p.y * tableSize, tableSize);
-    p.z = fmodf(p.z * tableSize, tableSize);
-
-    int xi0 = ((int)std::floor(p.x)) % tableSize;
-    int yi0 = ((int)std::floor(p.y)) % tableSize;
-    int zi0 = ((int)std::floor(p.z)) % tableSize;
-
-    int xi1 = (xi0 + 1) % tableSize;
-    int yi1 = (yi0 + 1) % tableSize;
-    int zi1 = (zi0 + 1) % tableSize;
-
-    float tx = p.x - ((int)std::floor(p.x));
-    float ty = p.y - ((int)std::floor(p.y));
-    float tz = p.z - ((int)std::floor(p.z));
-
-    float u = fade(tx);
-    float v = fade(ty);
-    float w = fade(tz);
-
-    // generate vectors going from the grid points to p
-    float x0 = tx, x1 = tx - 1;
-    float y0 = ty, y1 = ty - 1;
-    float z0 = tz, z1 = tz - 1;
-
-    float a = gradientDotV(hash(xi0, yi0, zi0), x0, y0, z0);
-    float b = gradientDotV(hash(xi1, yi0, zi0), x1, y0, z0);
-    float c = gradientDotV(hash(xi0, yi1, zi0), x0, y1, z0);
-    float d = gradientDotV(hash(xi1, yi1, zi0), x1, y1, z0);
-    float e = gradientDotV(hash(xi0, yi0, zi1), x0, y0, z1);
-    float f = gradientDotV(hash(xi1, yi0, zi1), x1, y0, z1);
-    float g = gradientDotV(hash(xi0, yi1, zi1), x0, y1, z1);
-    float h = gradientDotV(hash(xi1, yi1, zi1), x1, y1, z1);
-
-    float du = fadeDeriv(tx);
-    float dv = fadeDeriv(ty);
-    float dw = fadeDeriv(tz);
-
-    float k1 = (b - a);
-    float k2 = (c - a);
-    float k3 = (e - a);
-    float k4 = (a + d - b - c);
-    float k5 = (a + f - b - e);
-    float k6 = (a + g - c - e);
-    float k7 = (b + c + e + h - a - d - f - g);
-
-    vec3f derivs;
-    derivs.x = du * (k1 + k4 * v + k5 * w + k7 * v * w);
-    derivs.y = dv * (k2 + k4 * u + k6 * w + k7 * v * w);
-    derivs.z = dw * (k3 + k5 * u + k6 * v + k7 * v * w);
-
-    return derivs;
-  } 
-  
-  float noise(vec3f p) const
-  {
-    p.x = fmodf(p.x * tableSize, tableSize);
-    p.y = fmodf(p.y * tableSize, tableSize);
-    p.z = fmodf(p.z * tableSize, tableSize);
-
-    int xi0 = ((int)std::floor(p.x)) % tableSize;
-    int yi0 = ((int)std::floor(p.y)) % tableSize;
-    int zi0 = ((int)std::floor(p.z)) % tableSize;
-
-    int xi1 = (xi0 + 1) % tableSize;
-    int yi1 = (yi0 + 1) % tableSize;
-    int zi1 = (zi0 + 1) % tableSize;
-
-    float tx = p.x - ((int)std::floor(p.x));
-    float ty = p.y - ((int)std::floor(p.y));
-    float tz = p.z - ((int)std::floor(p.z));
-
-    float u = fade(tx);
-    float v = fade(ty);
-    float w = fade(tz);
-
-    // generate vectors going from the grid points to p
-    float x0 = tx, x1 = tx - 1;
-    float y0 = ty, y1 = ty - 1;
-    float z0 = tz, z1 = tz - 1;
-
-    float a = gradientDotV(hash(xi0, yi0, zi0), x0, y0, z0);
-    float b = gradientDotV(hash(xi1, yi0, zi0), x1, y0, z0);
-    float c = gradientDotV(hash(xi0, yi1, zi0), x0, y1, z0);
-    float d = gradientDotV(hash(xi1, yi1, zi0), x1, y1, z0);
-    float e = gradientDotV(hash(xi0, yi0, zi1), x0, y0, z1);
-    float f = gradientDotV(hash(xi1, yi0, zi1), x1, y0, z1);
-    float g = gradientDotV(hash(xi0, yi1, zi1), x0, y1, z1);
-    float h = gradientDotV(hash(xi1, yi1, zi1), x1, y1, z1);
-
-    float k0 = a;
-    float k1 = (b - a);
-    float k2 = (c - a);
-    float k3 = (e - a);
-    float k4 = (a + d - b - c);
-    float k5 = (a + f - b - e);
-    float k6 = (a + g - c - e);
-    float k7 = (b + c + e + h - a - d - f - g);
-
-    return k0 + k1 * u + k2 * v + k3 * w + k4 * u * v + k5 * u * w +
-           k6 * v * w + k7 * u * v * w; 
-  } 
 };
+PerlinNoise::PerlinNoiseData PerlinNoise::p;
 
 OSPGeometry PlaneGeometry(const vec4f& color, const AffineSpace3f& M)
 {
@@ -274,61 +166,30 @@ int main(int argc, const char **argv)
   const float spacing = 3.f/(reduce_max(dims)-1);
   OSPVolume volume = ospNewVolume("shared_structured_volume");
 
-  PerlinNoise pnoise;
-
-  //auto turbulence_noise = [&pnoise](int octaves, vec3f p)
-  //{
-  //  float val = 0;
-  //  for (int o = 0; o < octaves; ++o)
-  //  {
-  //    float scale = (float)(1 << o);
-  //    val += pnoise.noise(scale*p)/scale;
-  //  }
-  //  return val;
-  //};
-  
-  auto turbulence_deriv = [&pnoise](int octaves, vec3f p)
+  auto turbulence = [](const vec3f& p, float base_freqency, int octaves)
   {
-    vec3f deriv(0.f);
+    float value = 0.f;
+    float scale = 1.f;
     for (int o = 0; o < octaves; ++o)
     {
-      float scale = (float)(1 << o);
-      deriv += pnoise.deriv(scale*p)/scale;
+      value += PerlinNoise::noise(scale*p, base_freqency)/scale;
+      scale *= 2.f;
     }
-    return deriv;
+    return value;
   };
 
   // generate volume data
   auto numVoxels = dims.product();
-  std::vector<float> voxels(numVoxels);
+  std::vector<float> voxels(numVoxels, 0);
   tasking::parallel_for(dims.z, [&](int64_t z) {
     for (int y = 0; y < dims.y; ++y) {
       for (int x = 0; x < dims.x; ++x) {
-        int64_t offset = dims.x * dims.y * z + dims.x * y + x;
-        vec3f p(((float)x)/dims.x, ((float)y)/dims.y, ((float)z)/dims.z);
-        #if 0
+        vec3f p = vec3f(x+0.5f, y+0.5f, z+0.5f)/dims;
         const float X = 2.f*((float)x)/dims.x - 1.f;
         const float Y = 2.f*((float)y)/dims.y - 1.f;
         const float Z = 2.f*((float)z)/dims.z - 1.f;
-        float d = std::sqrt(X * X + Y * Y + Z * Z);
-        if (d < 1.f) {
-          voxels[offset] = d * turbulence_noise(10, p) + (1.f - d) * 1.f;
-          voxels[offset] = d * turbulence_noise(10, p) + (1.f - d) * 1.f;
-        } else {
-          voxels[offset] = 0.f;
-        }
-        #else
-        vec3f deriv = 0.05f * turbulence_deriv(10, p);
-        const float X = (2.f*((float)x)/dims.x) - 1.f + deriv.x;
-        const float Y = (2.f*((float)y)/dims.y) - 1.f + deriv.y;
-        const float Z = (2.f*((float)z)/dims.z) - 1.f + deriv.z;
-        float d = std::sqrt(X * X + Y * Y + Z * Z);
-        if (d < 0.9f) {
-          voxels[offset] = 1.f;
-        } else {
-          voxels[offset] = 0.f;
-        }
-        #endif
+        float d = 1.2 * std::sqrt(X * X + Y * Y + Z * Z) + 0.2f * turbulence(p, 12.f, 12);
+        if (d < 1.f) voxels[dims.x * dims.y * z + dims.x * y + x] = 1.f;
       }
     }
   });
@@ -617,7 +478,7 @@ int main(int argc, const char **argv)
     }
     
     static float sigma_t = 1.0f;
-    if (ImGui::SliderFloat("sigma_t", &sigma_t, 0.f, 10.f)) {
+    if (ImGui::SliderFloat("sigma_t", &sigma_t, 0.f, 100.f)) {
       commitWorld = true;
       ospSetFloat(volumetricModels[0], "sigma_t", sigma_t);
       glfwOSPRayWindow->addObjectToCommit(volumetricModels[0]);
