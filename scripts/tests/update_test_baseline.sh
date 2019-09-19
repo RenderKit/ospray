d1824145a9a0cdc1cd749017e843faa225bf2d6e
Florian Reibold <florian.reibold@intel.com>
2019-09-19 17:54:12 +0200

    update update_test_baseline.sh to respect new test_image_data folder

--- scripts/tests/update_test_baseline.sh
+++ scripts/tests/update_test_baseline.sh
@@ -9,6 +9,6 @@
 #
 md5=`md5sum $2/$1.png | awk '{print $1 }'`
 echo $md5
-echo $md5 > $3/tests/baseline/$1.png.md5
+echo $md5 > $3/test_image_data/baseline/$1.png.md5
 rsync --rsync-path="sudo rsync" --chown=web:web $2/$1.png sdvis.org:/var/www/html/ospray/download/baseline/benchmark-data/MD5/$md5
 
