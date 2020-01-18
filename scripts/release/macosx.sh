0a105a817117d72550a4ea7658158a65491c54bd
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2020-01-18 07:04:53 -0600

    add missing oidn search path for macOS release script

--- scripts/release/macosx.sh
+++ scripts/release/macosx.sh
@@ -81,6 +81,7 @@ export ospcommon_DIR=$DEP_DIR
 export embree_DIR=$DEP_DIR
 export glfw3_DIR=$DEP_DIR
 export openvkl_DIR=$DEP_DIR
+export OpenImageDenoise_DIR=$DEP_DIR
 
 # set release and installer settings
 cmake -L \
