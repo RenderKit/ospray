f46490db7b1ab274ff8c117f6b56735fe10c86e1
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2020-01-23 06:10:36 -0600

    Revert "restore old name for denoise op"
    This reverts commit dff36da678b01e9b313af4a3883d71f92e5577e2.

--- modules/denoiser/DenoiseFrameOp.cpp
+++ modules/denoiser/DenoiseFrameOp.cpp
@@ -112,7 +112,6 @@ std::string DenoiseFrameOp::toString() const
 }
 
 OSP_REGISTER_IMAGE_OP(DenoiseFrameOp, denoiser);
-OSP_REGISTER_IMAGE_OP(DenoiseFrameOp, frame_denoise);
 
 } // namespace ospray
 
