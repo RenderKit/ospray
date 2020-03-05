f2bead73ce2bcd888859bbc95b1e8c70cb342c3d
Johannes Günther <johannes.guenther@intel.com>
2020-03-05 09:46:09 +0100

    Cleanup (CR year and header, formatting)

--- modules/mpi/ospray/common/DistributedWorld.cpp
+++ modules/mpi/ospray/common/DistributedWorld.cpp
@@ -1,18 +1,5 @@
-// ======================================================================== //
-// Copyright 2009-2019 Intel Corporation                                    //
-//                                                                          //
-// Licensed under the Apache License, Version 2.0 (the "License");          //
-// you may not use this file except in compliance with the License.         //
-// You may obtain a copy of the License at                                  //
-//                                                                          //
-//     http://www.apache.org/licenses/LICENSE-2.0                           //
-//                                                                          //
-// Unless required by applicable law or agreed to in writing, software      //
-// distributed under the License is distributed on an "AS IS" BASIS,        //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
-// See the License for the specific language governing permissions and      //
-// limitations under the License.                                           //
-// ======================================================================== //
+// Copyright 2009-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
 
 #include "DistributedWorld.h"
 #include <algorithm>
@@ -216,8 +203,8 @@ void DistributedWorld::exchangeRegions()
   if (logLevel() >= OSP_LOG_DEBUG) {
     for (int i = 0; i < mpiGroup.size; ++i) {
       if (i == mpiGroup.rank) {
-        postStatusMsg(OSP_LOG_DEBUG) << "Rank " << mpiGroup.rank
-                         << ": All regions in world {";
+        postStatusMsg(OSP_LOG_DEBUG)
+            << "Rank " << mpiGroup.rank << ": All regions in world {";
         for (const auto &b : allRegions) {
           postStatusMsg(OSP_LOG_DEBUG) << "\t" << b << ",";
         }
--- modules/mpi/ospray/common/Profiling.cpp
+++ modules/mpi/ospray/common/Profiling.cpp
@@ -1,18 +1,5 @@
-// ======================================================================== //
-// Copyright 2009-2018 Intel Corporation                                    //
-//                                                                          //
-// Licensed under the Apache License, Version 2.0 (the "License");          //
-// you may not use this file except in compliance with the License.         //
-// You may obtain a copy of the License at                                  //
-//                                                                          //
-//     http://www.apache.org/licenses/LICENSE-2.0                           //
-//                                                                          //
-// Unless required by applicable law or agreed to in writing, software      //
-// distributed under the License is distributed on an "AS IS" BASIS,        //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
-// See the License for the specific language governing permissions and      //
-// limitations under the License.                                           //
-// ======================================================================== //
+// Copyright 2009-2018 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
 
 #include "Profiling.h"
 #include <cstring>
@@ -49,8 +36,7 @@ void logProfilingData(
       - (start.usage.ru_utime.tv_sec + start.usage.ru_stime.tv_sec)
       + 1e-6f
           * (end.usage.ru_utime.tv_usec + end.usage.ru_stime.tv_usec
-                - (start.usage.ru_utime.tv_usec
-                      + start.usage.ru_stime.tv_usec));
+              - (start.usage.ru_utime.tv_usec + start.usage.ru_stime.tv_usec));
 
   const double elapsedWall =
       duration_cast<duration<double>>(end.time - start.time).count();
