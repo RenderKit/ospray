a7df6448676e8a23825172c48bbe75a4c785a72f
Johannes Günther <johannes.guenther@intel.com>
2020-03-19 10:30:42 +0100

    Better error checking during module initialization

--- modules/mpi/ospray/ModuleInit.cpp
+++ modules/mpi/ospray/ModuleInit.cpp
@@ -10,11 +10,20 @@ extern "C" OSPError ospray_module_init_mpi(
     int16_t versionMajor, int16_t versionMinor, int16_t /*versionPatch*/)
 {
   using namespace ospray;
-  // Run the ISPC module's initialization function as well to register local
-  // types
-  ospLoadModule("ispc");
-  api::Device::registerType<mpi::MPIDistributedDevice>("mpiDistributed");
-  api::Device::registerType<mpi::MPIOffloadDevice>("mpiOffload");
-  Renderer::registerType<mpi::DistributedRaycastRenderer>("mpiRaycast");
-  return moduleVersionCheck(versionMajor, versionMinor);
+
+  auto status = moduleVersionCheck(versionMajor, versionMinor);
+
+  if (status == OSP_NO_ERROR) {
+    // Run the ISPC module's initialization function as well to register local
+    // types
+    status = ospLoadModule("ispc");
+  }
+
+  if (status == OSP_NO_ERROR) {
+    api::Device::registerType<mpi::MPIDistributedDevice>("mpiDistributed");
+    api::Device::registerType<mpi::MPIOffloadDevice>("mpiOffload");
+    Renderer::registerType<mpi::DistributedRaycastRenderer>("mpiRaycast");
+  }
+
+  return status;
 }
