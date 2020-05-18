ddb396f3f84eafd6680fa332082a53f5473ec9bb
Krzysztof Raszkowski <krzysztof.raszkowski@intel.com>
2020-05-18 14:17:05 +0000

    ci: kw scan jobs adjustments

---
+++ scripts/build_gitlab/test-kw-critical.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+KW_CRITICAL_OUTPUT_PATH=/tmp/critical
+export KW_BUILD_NUMBER=$(cat $CI_PROJECT_DIR/kw_build_number)
+echo "Checking for critical issues in $KW_BUILD_NUMBER ..."
+no_proxy=$KW_SERVER_IP curl -f --data "action=search&project=$KW_PROJECT_NAME&query=build:'$KW_BUILD_NUMBER'%20severity:Critical%20status:Analyze,Fix&user=$KW_USER&ltoken=$KW_LTOKEN" http://$KW_SERVER_IP:$KW_SERVER_PORT/review/api -o $KW_CRITICAL_OUTPUT_PATH
+getCriticalCount() {
+        cat $KW_CRITICAL_OUTPUT_PATH | wc -l
+}
+if [ -f $KW_CRITICAL_OUTPUT_PATH ]; then
+        echo "Critical issues found - $(getCriticalCount) in $KW_BUILD_NUMBER";
+        cat $KW_CRITICAL_OUTPUT_PATH
+        exit 1;
+else
+        echo "No critical issues were found in $KW_BUILD_NUMBER"
+fi
