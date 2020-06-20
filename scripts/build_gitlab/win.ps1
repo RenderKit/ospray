3c4e4ebacddb88fe9e4d269fc49591f7b248683e
Johannes Günther <johannes.guenther@intel.com>
2020-06-20 19:00:06 +0200

    Add CI build+test for all (incl. dependencies) Debug

--- scripts/build_gitlab/win.ps1
+++ scripts/build_gitlab/win.ps1
@@ -10,8 +10,10 @@ cmake -L `
   -G $args[0] `
   -T $args[1] `
   -D BUILD_EMBREE_FROM_SOURCE=OFF `
+  -D CMAKE_BUILD_TYPE=${args[2]} `
+  -D DEPENDENCIES_BUILD_TYPE=${args[2]} `
   ../scripts/superbuild
 
-cmake --build . --config Release --target ALL_BUILD
+cmake --build . --config $args[2] --target ALL_BUILD
 
 exit $LASTEXITCODE
