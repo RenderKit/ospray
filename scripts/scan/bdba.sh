e46e338eaf0ba27e79385aa7f00417181b5811a1
Johannes Günther <johannes.guenther@intel.com>
2020-07-17 14:29:26 +0200

    BDBA scan all at once

--- scripts/scan/bdba.sh
+++ scripts/scan/bdba.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 
-file_path=$1
+files=$1
 
-#upload file
-upload_response=`https_proxy="" curl -k -H "Authorization: Bearer $BDBA_TOKEN" -H "Group: $BDBA_GROUP" -T $file_path "$BDBA_SERVER/api/upload/"`
-product_id=`echo "$upload_response" | /NAS/tools/jq-linux64 -r '.results.product_id'`
-if [ $product_id == "null" ]; then
-    echo "Cannot upload file"
-    exit 1
-fi
-report_url=`echo "$upload_response" | /NAS/tools/jq-linux64 -r '.results.report_url'`
+logfile="bdba.log"
+failed=0
+for file_path in $files ; do
+    echo -e "\n" >> $logfile
+    #upload file
+    upload_response=`https_proxy="" curl -k -H "Authorization: Bearer $BDBA_TOKEN" -H "Group: $BDBA_GROUP" -T $file_path "$BDBA_SERVER/api/upload/"`
+    product_id=`echo "$upload_response" | /NAS/tools/jq-linux64 -r '.results.product_id'`
+    if [ $product_id == "null" ]; then
+        echo "Cannot upload file $file_path" >> $logfile
+        failed=1
+        continue
+    fi
+    report_url=`echo "$upload_response" | /NAS/tools/jq-linux64 -r '.results.report_url'`
 
-echo "Scan upload completed - product id: $product_id ($report_url)"
+    echo "Scan upload of $file_path completed - product id: $product_id ($report_url)" >> $logfile
 
-set +e
-MAX_RETRY=600
+    set +e
+    MAX_RETRY=600
 
-RETRY_COUNTER="0"
-while [ $RETRY_COUNTER -lt $MAX_RETRY ]; do
-	response=`https_proxy="" curl -s -X GET -H "Authorization: Bearer $BDBA_TOKEN" -k $BDBA_SERVER/api/product/$product_id/`
-	CMD_RETURN_CODE=$?
+    RETRY_COUNTER="0"
+    while [ $RETRY_COUNTER -lt $MAX_RETRY ]; do
+        response=`https_proxy="" curl -s -X GET -H "Authorization: Bearer $BDBA_TOKEN" -k $BDBA_SERVER/api/product/$product_id/`
+        CMD_RETURN_CODE=$?
 
-	status=`echo "$response" | /NAS/tools/jq-linux64 -r '.results.status'`
-	verdict=`echo "$response" | /NAS/tools/jq-linux64 -r '.results.summary.verdict.short'`
- 
-	if [ $CMD_RETURN_CODE == 0 ] && [[ $status == "R" ]]; then
-	    echo $response | python -m json.tool
-	    echo "Verdict: $verdict"
-	    if [ $verdict == "Pass" ] || [ $verdict == "null" ]; then
-		    exit 0
-	    else
-		    echo "There is a problem - please check report $report_url"
-		    exit 1
-	    fi 
-	fi
-	RETRY_COUNTER=$[$RETRY_COUNTER+1]
-	echo "Scan not finished yet, [$RETRY_COUNTER/$MAX_RETRY] - checking again ... "
-	sleep 20
+        status=`echo "$response" | /NAS/tools/jq-linux64 -r '.results.status'`
+        verdict=`echo "$response" | /NAS/tools/jq-linux64 -r '.results.summary.verdict.short'`
+     
+        if [ $CMD_RETURN_CODE == 0 ] && [[ $status == "R" ]]; then
+            echo $response | python -m json.tool
+            echo "Verdict: $verdict" >> $logfile
+            if [ $verdict != "Pass" ] && [ $verdict != "null" ]; then
+                echo "There is a problem - please check report $report_url" >> $logfile
+                failed=1
+            fi 
+            break
+        fi
+        RETRY_COUNTER=$[$RETRY_COUNTER+1]
+        echo "Scan not finished yet, [$RETRY_COUNTER/$MAX_RETRY] - checking again ... "
+        sleep 20
+    done
+
+    set -e
+    if [ $RETRY_COUNTER -ge $MAX_RETRY ]; then
+        failed=62
+        continue
+    fi
 done
 
-set -e
-if [ $RETRY_COUNTER -ge $MAX_RETRY ]; then
-	exit 62
-fi
+cat $logfile
 
-exit 0
+exit $failed
