c6693aa378061649d4e75b5ac734c6478537ea45
Nan Zhang <nan.zhang1@ansys.com>
2021-05-20 01:51:20 -0400

    Added work progress

--- scripts/superbuild_ens/install_to_ensight.cmake
+++ scripts/superbuild_ens/install_to_ensight.cmake
@@ -1,10 +1,47 @@
 set(ENSIGHT_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../../../../..)
 set(APEX_DIR ${ENSIGHT_SRC_DIR}/apex221/machines/win64)
 
+#embree install commands
 set(EMBREE_INSTALL_DIR ${APEX_DIR}/intel_embree)
-set(EMBREE_FROM_DIR ${CMAKE_INSTALL_PREFIX}/embree)
 
 install(
-	FILES ${EMBREE_FROM_DIR}/bin/embree3.dll
+	FILES ${CMAKE_INSTALL_PREFIX}/embree/bin/embree3.dll
 	DESTINATION ${EMBREE_INSTALL_DIR}/bin
+)
+
+install(
+	DIRECTORY ${CMAKE_INSTALL_PREFIX}/embree/lib
+	DESTINATION ${EMBREE_INSTALL_DIR}/
+)
+
+install(
+	DIRECTORY ${CMAKE_BINARY_DIR}/embree/src/common
+	DESTINATION ${EMBREE_INSTALL_DIR}/
+)
+
+install(
+	DIRECTORY ${CMAKE_BINARY_DIR}/embree/src/include
+	DESTINATION ${EMBREE_INSTALL_DIR}/
+)
+
+
+#tbb install commands
+set(TBB_INSTALL_DIR ${APEX_DIR}/intel_tbb)
+
+install(
+	FILES ${CMAKE_INSTALL_PREFIX}/tbb/redist/intel64/vc14/tbb12.dll
+		  ${CMAKE_INSTALL_PREFIX}/tbb/redist/intel64/vc14/tbbmalloc.dll
+	DESTINATION ${TBB_INSTALL_DIR}/bin/
+)
+
+install(
+	FILES ${CMAKE_INSTALL_PREFIX}/tbb/lib/intel64/vc14/tbb.lib
+          ${CMAKE_INSTALL_PREFIX}/tbb/lib/intel64/vc14/tbb12.lib
+          ${CMAKE_INSTALL_PREFIX}/tbb/lib/intel64/vc14/tbbmalloc.lib	
+	DESTINATION ${TBB_INSTALL_DIR}/lib/
+)
+
+install(
+	DIRECTORY ${CMAKE_BINARY_DIR}/tbb/src/include
+	DESTINATION ${TBB_INSTALL_DIR}/
 )
