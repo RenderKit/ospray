e0b6b28718702f2a815f74eaba31e71503abd26f
Johannes Günther <johannes.guenther@intel.com>
2023-10-05 14:18:11 +0200

    Reorder and update GPU test filters

--- scripts/tests/run_gpu_tests.ps1
+++ scripts/tests/run_gpu_tests.ps1
@@ -27,37 +27,30 @@ $exitCode += $LastExitCode
 cmake --build . --config Release --target ospray_test_data
 $exitCode += $LastExitCode
 
-# Excluded tests on GPU
+### Excluded tests on GPU
+#########################
+# Clipping unsupported
 $test_filters="ClippingParallel.planes"
+$test_filters+=":TestScenesClipping/FromOsprayTesting.*"
+$test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/1"
+$test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/2"
 # Subdivision surfaces unsupported
 $test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/15"
 $test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/16"
 $test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/17"
-# Artifacts on PVC
-$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/21"
-$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/22"
-# Instancing test also makes use of motion blur, which is unsupported
-$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/24"
-$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/25"
-$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/26"
-# Clipping unsupported
-$test_filters+=":TestScenesClipping/FromOsprayTesting.*"
-# Different noise
-$test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/1"
-# Multiple volumes, unsupported
+$test_filters+=":Color/Interpolation.Interpolation/4"
+$test_filters+=":Color/Interpolation.Interpolation/5"
+$test_filters+=":Color/Interpolation.Interpolation/6"
+$test_filters+=":Color/Interpolation.Interpolation/7"
+$test_filters+=":Texcoord/Interpolation.Interpolation/2"
+$test_filters+=":Texcoord/Interpolation.Interpolation/3"
+# Multiple volumes unsupported
 $test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/3"
 $test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/4"
 $test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/5"
-# Line artifacts
-$test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/18"
-# Multiple volumes unsupported
 $test_filters+=":TestScenesVolumesStrictParams/FromOsprayTesting.*"
-# Clipping unsupported
-$test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/1"
-$test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/2"
-# Almost all working, some remaining bug on cell-valued unstructured volumes
-$test_filters+=":TestScenesVolumes/UnstructuredVolume.1"
-$test_filters+=":TestScenesVolumes/UnstructuredVolume.3"
+# Requires non-overlapping multiple volume support on GPU
+$test_filters+=":ObjectInstance/IDBuffer.*"
 # Motion blur unsupported
 $test_filters+=":TestMotionBlur/MotionBlurBoxes.*"
 $test_filters+=":CameraRollingShutter/MotionBlurBoxes.*"
@@ -66,21 +59,27 @@ $test_filters+=":Camera/MotionCamera.*"
 $test_filters+=":CameraOrtho/MotionCamera.*"
 $test_filters+=":CameraStereoRollingShutter/MotionCamera.*"
 $test_filters+=":LightMotionBlur/*"
-# Crashing
-$test_filters+=":Primitive/IDBuffer.*"
-# Requires non-overlapping multiple volume support on GPU
-$test_filters+=":ObjectInstance/IDBuffer.*"
-# Subdivision surfaces not supported on GPU
-$test_filters+=":Color/Interpolation.Interpolation/4"
-$test_filters+=":Color/Interpolation.Interpolation/5"
-$test_filters+=":Color/Interpolation.Interpolation/6"
-$test_filters+=":Color/Interpolation.Interpolation/7"
-$test_filters+=":Texcoord/Interpolation.Interpolation/2"
-$test_filters+=":Texcoord/Interpolation.Interpolation/3"
+# Instancing test also makes use of motion blur
+$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/24"
+$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/25"
+$test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/26"
 # Variance termination is not quite right
 $test_filters+=":TestScenesVariance/FromOsprayTestingVariance.testScenes/0"
 # 'mix' material not supported on GPU (difficult to implement without fn ptr)
 $test_filters+=":TestScenesPtMaterials/FromOsprayTesting.test_scenes/13"
+# Crashing FIXME
+$test_filters+=":Primitive/IDBuffer.*"
+
+# Different noise
+$test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/1"
+
+## Windows only (driver?)
+
+# Artifacts with dpcpp (icx is fine)
+$test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/7"
+$test_filters+=":TestScenesVolumes/UnstructuredVolume.simple/2"
+$test_filters+=":TestScenesVolumes/UnstructuredVolume.simple/3"
+
 
 $env:ONEAPI_DEVICE_SELECTOR="level_zero:*"
 
@@ -97,20 +96,23 @@ $env:OSPRAY_ALLOW_DEVICE_MEMORY = "0"
 
 if ( $testMultiDevice ) {
   md failed-multidevice
-  $Env:OSPRAY_NUM_SUBDEVICES = 2
-  $test_filters = "DebugOp/ImageOp.ImageOp/0" # post-processing not enabled on multidevice
-  ospTestSuite.exe --osp:load-modules=multidevice_cpu --osp:device=multidevice --gtest_output=xml:tests-multidevice.xml --baseline-dir=regression_test_baseline\ --failed-dir=failed-multidevice --gtest_filter="-$test_filters"
+  $env:OSPRAY_NUM_SUBDEVICES = "2"
+  # post-processing not enabled on multidevice
+  $test_filters_md=$test_filters
+  $test_filters_md+=":DenoiserOp.DenoiserOp"
+  $test_filters_md+=":DebugOp/ImageOp.ImageOp/0"
+  ospTestSuite.exe --osp:load-modules=multidevice_cpu --osp:device=multidevice --gtest_output=xml:tests-multidevice.xml --baseline-dir=regression_test_baseline\ --failed-dir=failed-multidevice --gtest_filter="-$test_filters_md"
   $exitCode += $LastExitCode
 }
 
 if ( $testMPI ) {
-  md failed-mpi
-  $test_filters += ":TestScenesVariance/*"
-  mpiexec.exe -n 2 ospTestSuite.exe --osp:load-modules=mpi_offload --osp:device=mpiOffload --gtest_output=xml:tests-mpi.xml --baseline-dir=regression_test_baseline\ --failed-dir=failed-mpi --gtest_filter="-$test_filters"
+  md failed-mpi-gpu
+  $env:OSPRAY_MPI_DISTRIBUTED_GPU = "1"
+  mpiexec.exe -n 2 ospTestSuite.exe --osp:load-modules=mpi_offload --osp:device=mpiOffload --gtest_output=xml:tests-mpi.xml --baseline-dir=regression_test_baseline\ --failed-dir=failed-mpi-gpu --gtest_filter="-$test_filters"
   $exitCode += $LastExitCode
 
-  md failed-mpi-data-parallel
-  mpiexec.exe -n 2 ospMPIDistribTestSuite.exe --gtest_output=xml:tests-mpi.xml --baseline-dir=regression_test_baseline\ --failed-dir=failed-mpi-data-parallel
+  md failed-mpi-gpu-data-parallel
+  mpiexec.exe -n 2 ospMPIDistribTestSuite.exe --gtest_output=xml:tests-mpi.xml --baseline-dir=regression_test_baseline\ --failed-dir=failed-mpi-gpu-data-parallel
   $exitCode += $LastExitCode
 }
 
--- scripts/tests/run_gpu_tests.sh
+++ scripts/tests/run_gpu_tests.sh
@@ -41,37 +41,30 @@ cmake -D OSPRAY_TEST_ISA=AVX512SKX "${SOURCEDIR}/test_image_data"
 make -j 4 ospray_test_data
 let exitCode+=$?
 
-# Excluded tests on GPU
+### Excluded tests on GPU
+#########################
+# Clipping unsupported
 test_filters="ClippingParallel.planes"
+test_filters+=":TestScenesClipping/FromOsprayTesting.*"
+test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/1"
+test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/2"
 # Subdivision surfaces unsupported
 test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/15"
 test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/16"
 test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/17"
-# Artifacts on PVC
-test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/21"
-test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/22"
-# Instancing test also makes use of motion blur, which is unsupported
-test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/24"
-test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/25"
-test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/26"
-# Clipping unsupported
-test_filters+=":TestScenesClipping/FromOsprayTesting.*"
-# Different noise
-test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/1"
-# Multiple volumes, unsupported
+test_filters+=":Color/Interpolation.Interpolation/4"
+test_filters+=":Color/Interpolation.Interpolation/5"
+test_filters+=":Color/Interpolation.Interpolation/6"
+test_filters+=":Color/Interpolation.Interpolation/7"
+test_filters+=":Texcoord/Interpolation.Interpolation/2"
+test_filters+=":Texcoord/Interpolation.Interpolation/3"
+# Multiple volumes unsupported
 test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/3"
 test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/4"
 test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/5"
-# Line artifacts
-test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/18"
-# Multiple volumes unsupported
 test_filters+=":TestScenesVolumesStrictParams/FromOsprayTesting.*"
-# Clipping unsupported
-test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/1"
-test_filters+=":TestScenesMaxDepth/FromOsprayTestingMaxDepth.test_scenes/2"
-# Almost all working, some remaining bug on cell-valued unstructured volumes
-test_filters+=":TestScenesVolumes/UnstructuredVolume.1"
-test_filters+=":TestScenesVolumes/UnstructuredVolume.3"
+# Requires non-overlapping multiple volume support on GPU
+test_filters+=":ObjectInstance/IDBuffer.*"
 # Motion blur unsupported
 test_filters+=":TestMotionBlur/MotionBlurBoxes.*"
 test_filters+=":CameraRollingShutter/MotionBlurBoxes.*"
@@ -80,21 +73,32 @@ test_filters+=":Camera/MotionCamera.*"
 test_filters+=":CameraOrtho/MotionCamera.*"
 test_filters+=":CameraStereoRollingShutter/MotionCamera.*"
 test_filters+=":LightMotionBlur/*"
-# Crashing
-test_filters+=":Primitive/IDBuffer.*"
-# Requires non-overlapping multiple volume support on GPU
-test_filters+=":ObjectInstance/IDBuffer.*"
-# Subdivision surfaces not supported on GPU
-test_filters+=":Color/Interpolation.Interpolation/4"
-test_filters+=":Color/Interpolation.Interpolation/5"
-test_filters+=":Color/Interpolation.Interpolation/6"
-test_filters+=":Color/Interpolation.Interpolation/7"
-test_filters+=":Texcoord/Interpolation.Interpolation/2"
-test_filters+=":Texcoord/Interpolation.Interpolation/3"
+# Instancing test also makes use of motion blur
+test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/24"
+test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/25"
+test_filters+=":TestScenesGeometry/FromOsprayTesting.test_scenes/26"
 # Variance termination is not quite right
 test_filters+=":TestScenesVariance/FromOsprayTestingVariance.testScenes/0"
 # 'mix' material not supported on GPU (difficult to implement without fn ptr)
 test_filters+=":TestScenesPtMaterials/FromOsprayTesting.test_scenes/13"
+# Crashing FIXME
+test_filters+=":Primitive/IDBuffer.*"
+
+# Different noise
+test_filters+=":TestScenesVolumes/FromOsprayTesting.test_scenes/1"
+
+## Linux only (driver?)
+
+# Artifacts
+test_filters+=":TestScenesPtMaterials/FromOsprayTesting.test_scenes/12"
+test_filters+=":Renderers/TextureVolumeTransform.simple/0"
+test_filters+=":Appearance/Texture2D.filter/*"
+
+# Artifacts on PVC only (DG2 is fine)
+test_filters+=":Texcoord/Interpolation.Interpolation/0"
+test_filters+=":Texcoord/Interpolation.Interpolation/1"
+test_filters+=":Appearance/Texture2DTransform.simple/0"
+
 
 export ONEAPI_DEVICE_SELECTOR=level_zero:*
 
