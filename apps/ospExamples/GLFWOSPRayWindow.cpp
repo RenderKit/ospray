0ee3e726f722c8befbbf4e452c48703e8ed6ec6e
Johannes Günther <johannes.guenther@intel.com>
2024-11-25 13:06:15 +0100

    Fix initialization of OpenGL on OSX, closes #601

--- apps/ospExamples/GLFWOSPRayWindow.cpp
+++ apps/ospExamples/GLFWOSPRayWindow.cpp
@@ -164,7 +164,7 @@ GLFWOSPRayWindow::GLFWOSPRayWindow(
 
   glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-#ifdef __APPLE_
+#ifdef __APPLE__
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   const char *glslVersion = "#version 150";
